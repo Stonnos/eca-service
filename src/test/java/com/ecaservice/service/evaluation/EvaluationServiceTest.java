9c4bd3ea7e58d4339fb1bf2968ed0d94daba1edc
Roman Batygin <r.batygin@intabia.ru>
2018-03-02 17:51:13 +0700

    refactoring EvaluationServiceTest

--- src/test/java/com/ecaservice/service/evaluation/EvaluationServiceTest.java
+++ src/test/java/com/ecaservice/service/evaluation/EvaluationServiceTest.java
@@ -5,29 +5,45 @@ import com.ecaservice.config.CrossValidationConfig;
 import com.ecaservice.model.InputData;
 import com.ecaservice.model.evaluation.ClassificationResult;
 import com.ecaservice.model.evaluation.EvaluationMethod;
+import eca.core.evaluation.Evaluation;
 import eca.metrics.KNearestNeighbours;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import weka.classifiers.Classifier;
 import weka.core.Instances;
 
+import javax.inject.Inject;
 import java.util.Collections;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doThrow;
 
 /**
  * Unit tests that checks EvaluationService functionality {@see EvaluationService}.
  *
  * @author Roman Batygin
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringRunner.class)
+@PrepareForTest({Evaluation.class, EvaluationService.class})
+@EnableConfigurationProperties
+@TestPropertySource("classpath:application.properties")
+@Import(CrossValidationConfig.class)
 public class EvaluationServiceTest {
 
-    @Mock
+    @Inject
     private CrossValidationConfig config;
 
     private EvaluationService evaluationService;
@@ -35,9 +51,6 @@ public class EvaluationServiceTest {
 
     @Before
     public void setUp() throws Exception {
-        when(config.getSeed()).thenReturn(TestHelperUtils.SEED);
-        when(config.getNumFolds()).thenReturn(TestHelperUtils.NUM_FOLDS);
-        when(config.getNumTests()).thenReturn(TestHelperUtils.NUM_TESTS);
         testInstances = TestHelperUtils.loadInstances();
         evaluationService = new EvaluationService(config);
     }
@@ -101,8 +114,11 @@ public class EvaluationServiceTest {
     }
 
     @Test
-    public void testClassificationResultWithError() {
-        when(config.getSeed()).thenThrow(Exception.class);
+    public void testClassificationResultWithError() throws Exception {
+        Evaluation evaluation = PowerMockito.mock(Evaluation.class);
+        PowerMockito.whenNew(Evaluation.class).withAnyArguments().thenReturn(evaluation);
+        doThrow(new Exception()).when(evaluation).kCrossValidateModel(any(Classifier.class), any(Instances.class),
+                anyInt(), anyInt(), any(Random.class));
         InputData inputData = new InputData(new KNearestNeighbours(), testInstances);
         ClassificationResult result = evaluationService.evaluateModel(inputData, EvaluationMethod.CROSS_VALIDATION,
                 TestHelperUtils.createEvaluationOptionsMap(TestHelperUtils.NUM_FOLDS, TestHelperUtils.NUM_TESTS));
