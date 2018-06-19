c7db45bb163eae7480c4c8e5505994f19368ce7c
r.batygin <r.batygin@intabia.ru>
2018-06-19 11:13:44 +0700

    Some refactoring

--- src/main/java/com/ecaservice/model/entity/ClassifierOptionsRequestEntity.java
+++ src/main/java/com/ecaservice/model/entity/ClassifierOptionsRequestEntity.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -35,6 +37,7 @@ public class ClassifierOptionsRequestEntity {
     /**
      * Request source
      */
+    @Enumerated(EnumType.STRING)
     private ClassifierOptionsRequestSource source;
 
     /**
