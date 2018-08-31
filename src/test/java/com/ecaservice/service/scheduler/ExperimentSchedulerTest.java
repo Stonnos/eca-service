406752410090f9a3b83b427c24e6c9bd1e250f86
Roman93 <bat1238@yandex.ru>
2018-08-31 20:58:55 +0700

    Fixed test

--- src/test/java/com/ecaservice/service/scheduler/ExperimentSchedulerTest.java
+++ src/test/java/com/ecaservice/service/scheduler/ExperimentSchedulerTest.java
@@ -4,6 +4,7 @@ import com.ecaservice.TestHelperUtils;
 import com.ecaservice.config.ExperimentConfig;
 import com.ecaservice.model.entity.Experiment;
 import com.ecaservice.model.experiment.ExperimentStatus;
+import com.ecaservice.repository.EmailRequestRepository;
 import com.ecaservice.repository.ErsRequestRepository;
 import com.ecaservice.repository.ExperimentRepository;
 import com.ecaservice.service.ers.ErsRequestService;
@@ -43,6 +44,8 @@ public class ExperimentSchedulerTest {
     private ExperimentRepository experimentRepository;
     @Inject
     private ErsRequestRepository ersRequestRepository;
+    @Inject
+    private EmailRequestRepository emailRequestRepository;
     @Mock
     private ExperimentService experimentService;
     @Mock
@@ -66,6 +69,7 @@ public class ExperimentSchedulerTest {
     @After
     public void after() {
         ersRequestRepository.deleteAll();
+        emailRequestRepository.deleteAll();
         experimentRepository.deleteAll();
     }
 
