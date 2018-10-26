4e1eac83892fa486817211b7268f39b839af0918
romanbatygin <romanbatygin@axmor.com>
2018-10-26 12:19:57 +0700

    Added eca-web module configuration

---
+++ eca-web/src/main/java/com/ecaservice/web/EcaWebApplication.java
@@ -0,0 +1,29 @@
+package com.ecaservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Main class.
+ *
+ * @author Roman Batygin
+ */
+@SpringBootApplication
+public class EcaWebApplication extends SpringBootServletInitializer {
+
+    /**
+     * Runs application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EcaWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EcaWebApplication.class);
+    }
+}
