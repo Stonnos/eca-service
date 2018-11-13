8a5b65e22e475e0b9c85cc5d8738d35bf473e554
romanbatygin <romanbatygin@axmor.com>
2018-11-13 14:05:13 +0700

    1. Fixed auth-interceptor 2. Fixed login form design

--- eca-web/src/main/resources/src/app/auth/interceptors/auth-interceptor.ts
+++ eca-web/src/main/resources/src/app/auth/interceptors/auth-interceptor.ts
@@ -6,23 +6,28 @@ import {
   HttpRequest
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable } from "rxjs/internal/Observable";
-import { tap } from "rxjs/internal/operators";
+import { catchError } from "rxjs/internal/operators";
+import { Router } from "@angular/router";
+import { EMPTY } from "rxjs/internal/observable/empty";
+import { throwError } from "rxjs/internal/observable/throwError";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private cookieService: CookieService) {
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(tap((error: any) => {
-      if (error instanceof HttpErrorResponse) {
-        if (error.status === 401) {
-          this.router.navigate(['/login']);
-        }
+  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      if (err instanceof HttpErrorResponse && err.status === 401) {
+        this.cookieService.delete('access_token');
+        this.router.navigate(['/login']);
+        return EMPTY;
       }
+      const error = err.error.message || err.statusText;
+      return throwError(error);
     }));
   }
 }
