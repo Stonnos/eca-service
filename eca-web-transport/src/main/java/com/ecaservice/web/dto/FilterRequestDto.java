6cb89ba0bdec31ad84eac74fd080ec2be3e2a5df
romanbatygin <romanbatygin@axmor.com>
2018-11-15 19:27:47 +0700

    Fixed swagger annotations

--- eca-web-transport/src/main/java/com/ecaservice/web/dto/FilterRequestDto.java
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/FilterRequestDto.java
@@ -1,6 +1,5 @@
 package com.ecaservice.web.dto;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -14,24 +13,20 @@ public class FilterRequestDto {
     /**
      * Column name
      */
-    @ApiModelProperty(notes = "Field name")
     private String name;
 
     /**
      * Column value
      */
-    @ApiModelProperty(notes = "Field value")
     private String value;
 
     /**
      * Filter type {@link FilterType}
      */
-    @ApiModelProperty(notes = "Filter type")
     private FilterType filterType;
 
     /**
      * Match mode type {@link MatchMode}
      */
-    @ApiModelProperty(notes = "Filter match mode")
     private MatchMode matchMode;
 }
--- eca-web-transport/src/main/java/com/ecaservice/web/dto/PageRequestDto.java
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/PageRequestDto.java
@@ -1,6 +1,5 @@
 package com.ecaservice.web.dto;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -16,30 +15,25 @@ public class PageRequestDto {
     /**
      * Page number
      */
-    @ApiModelProperty(notes = "Page number")
     private int page;
 
     /**
      * Page size
      */
-    @ApiModelProperty(notes = "Page size")
     private int size;
 
     /**
      * Sort field
      */
-    @ApiModelProperty(notes = "Sort field name")
     private String sortField;
 
     /**
      * Is ascending sort?
      */
-    @ApiModelProperty(notes = "Is ascending sort order?")
     private boolean ascending;
 
     /**
      * Filters list
      */
-    @ApiModelProperty(notes = "Filters list")
     private List<FilterRequestDto> filters;
 }
