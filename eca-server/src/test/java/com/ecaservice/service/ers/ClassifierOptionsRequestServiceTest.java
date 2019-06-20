392caba3260c080c87a81749d3fecbbc393b37c2
romanbatygin <romanbatygin@axmor.com>
2019-06-20 14:34:49 +0700

    Implements unit test for global filtering by classifierOptionsRequest

--- eca-server/src/test/java/com/ecaservice/service/ers/ClassifierOptionsRequestServiceTest.java
+++ eca-server/src/test/java/com/ecaservice/service/ers/ClassifierOptionsRequestServiceTest.java
@@ -1,8 +1,11 @@
 package com.ecaservice.service.ers;
 
+import com.ecaservice.TestHelperUtils;
 import com.ecaservice.config.CommonConfig;
+import com.ecaservice.dto.evaluation.ResponseStatus;
 import com.ecaservice.model.entity.ClassifierOptionsRequestModel;
 import com.ecaservice.model.entity.ClassifierOptionsRequestModel_;
+import com.ecaservice.model.entity.FilterTemplateType;
 import com.ecaservice.repository.ClassifierOptionsRequestModelRepository;
 import com.ecaservice.service.AbstractJpaTest;
 import com.ecaservice.service.filter.GlobalFilterService;
@@ -10,33 +13,86 @@ import com.ecaservice.web.dto.model.FilterFieldType;
 import com.ecaservice.web.dto.model.FilterRequestDto;
 import com.ecaservice.web.dto.model.MatchMode;
 import com.ecaservice.web.dto.model.PageRequestDto;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 
 import javax.inject.Inject;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for checking {@link ClassifierOptionsRequestService} functionality.
  *
  * @author Roman Batygin
  */
-@Import({ClassifierOptionsRequestService.class, CommonConfig.class, GlobalFilterService.class})
+@Import(CommonConfig.class)
 public class ClassifierOptionsRequestServiceTest extends AbstractJpaTest {
 
     @Inject
-    private ClassifierOptionsRequestService classifierOptionsRequestService;
-    @Inject
     private ClassifierOptionsRequestModelRepository classifierOptionsRequestModelRepository;
+    @Inject
+    private CommonConfig commonConfig;
+    @Mock
+    private GlobalFilterService globalFilterService;
+
+
+    private ClassifierOptionsRequestService classifierOptionsRequestService;
+
+    @Override
+    public void init() {
+        classifierOptionsRequestService = new ClassifierOptionsRequestService(commonConfig, globalFilterService,
+                classifierOptionsRequestModelRepository);
+    }
 
     @Override
     public void deleteAll() {
         classifierOptionsRequestModelRepository.deleteAll();
+    }
+
+    /**
+     * Tests global filtering relationName "gla" search query and response status equals to SUCCESS.
+     */
+    @Test
+    public void testGlobalFilter() {
+        ClassifierOptionsRequestModel requestModel =
+                TestHelperUtils.createClassifierOptionsRequestModel(StringUtils.EMPTY, LocalDateTime.now(),
+                        ResponseStatus.ERROR, Collections.emptyList());
+        requestModel.setRelationName("relation1");
+        ClassifierOptionsRequestModel requestModel1 =
+                TestHelperUtils.createClassifierOptionsRequestModel(StringUtils.EMPTY, LocalDateTime.now(),
+                        ResponseStatus.SUCCESS, Collections.emptyList());
+        requestModel1.setRelationName("relation2");
+        ClassifierOptionsRequestModel requestModel2 =
+                TestHelperUtils.createClassifierOptionsRequestModel(StringUtils.EMPTY, LocalDateTime.now(),
+                        ResponseStatus.SUCCESS, Collections.emptyList());
+        requestModel2.setRelationName("glass");
+        ClassifierOptionsRequestModel requestModel3 =
+                TestHelperUtils.createClassifierOptionsRequestModel(StringUtils.EMPTY, LocalDateTime.now(),
+                        ResponseStatus.RESULTS_NOT_FOUND, Collections.emptyList());
+        requestModel3.setRelationName("glass");
+        classifierOptionsRequestModelRepository.saveAll(
+                Arrays.asList(requestModel, requestModel1, requestModel2, requestModel3));
+        PageRequestDto pageRequestDto =
+                new PageRequestDto(0, 10, ClassifierOptionsRequestModel_.REQUEST_DATE, false, "gla", newArrayList());
+        pageRequestDto.getFilters().add(
+                new FilterRequestDto(ClassifierOptionsRequestModel_.RESPONSE_STATUS, ResponseStatus.SUCCESS.name(),
+                        FilterFieldType.REFERENCE, MatchMode.EQUALS));
+        when(globalFilterService.getGlobalFilterFields(FilterTemplateType.CLASSIFIER_OPTIONS_REQUEST)).thenReturn(
+                Arrays.asList(ClassifierOptionsRequestModel_.RELATION_NAME, ClassifierOptionsRequestModel_.REQUEST_ID));
+        Page<ClassifierOptionsRequestModel> classifierOptionsRequestModelPage =
+                classifierOptionsRequestService.getNextPage(pageRequestDto);
+        assertThat(classifierOptionsRequestModelPage).isNotNull();
+        assertThat(classifierOptionsRequestModelPage.getTotalElements()).isOne();
     }
 
     @Test
