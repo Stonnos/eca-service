48847e263004dd852c9e6669cf91ef1429b74e83
romanbatygin <romanbatygin@axmor.com>
2019-07-08 18:35:45 +0700

    Changed experiment types labels

--- eca-server/src/main/java/com/ecaservice/model/dictionary/ExperimentTypeDictionary.java
+++ eca-server/src/main/java/com/ecaservice/model/dictionary/ExperimentTypeDictionary.java
@@ -11,13 +11,12 @@ public class ExperimentTypeDictionary {
     }
 
     public static final String NEURAL_NETWORKS_NAME = "Нейронные сети";
-    public static final String HETEROGENEOUS_ENSEMBLE_NAME = "Неоднородный ансамблевый алгоритм";
-    public static final String MODIFIED_HETEROGENEOUS_ENSEMBLE_NAME =
-            "Модифицированный неоднородный ансамблевый алгоритм";
+    public static final String HETEROGENEOUS_ENSEMBLE_NAME = "Неоднородный ансамбль";
+    public static final String MODIFIED_HETEROGENEOUS_ENSEMBLE_NAME = "Мод. неоднородный ансамбль";
     public static final String ADA_BOOST_NAME = "Алгоритм AdaBoost";
     public static final String STACKING_NAME = "Алгоритм Stacking";
-    public static final String KNN_NAME = "Алгоритм k - взвешенных ближайших соседей";
-    public static final String RANDOM_FORESTS_NAME = "Случайные леса";
     public static final String STACKING_CV_NAME = "Алгоритм Stacking CV";
+    public static final String KNN_NAME = "Алгоритм KNN";
+    public static final String RANDOM_FORESTS_NAME = "Случайные леса";
     public static final String DECISION_TREE_NAME = "Деревья решений";
 }
