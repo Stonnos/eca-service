384d46fe4b9ee905810f3c547ed60698faaa3afe
Roman93 <bat1238@yandex.ru>
2019-07-22 01:10:55 +0700

    Removed test method

--- eca-server/src/main/java/com/ecaservice/controller/api/EcaController.java
+++ eca-server/src/main/java/com/ecaservice/controller/api/EcaController.java
@@ -23,7 +23,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -117,13 +116,6 @@ public class EcaController {
                 evaluationResponse.getStatus());
         sendEvaluationResultsToErs(evaluationResponse);
         return evaluationResponse;
-    }
-
-
-    @PreAuthorize("#oauth2.hasScope('eca')")
-    @GetMapping(value = "/hello")
-    public String hello() {
-        return "4343";
     }
 
     /**
