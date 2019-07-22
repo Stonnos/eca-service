fce0555f96902afcda67bfc276705322ad4dffa3
Roman93 <bat1238@yandex.ru>
2019-07-22 23:41:55 +0700

    Changed experiment token generation algorithm

--- eca-server/src/main/java/com/ecaservice/util/ExperimentUtils.java
+++ eca-server/src/main/java/com/ecaservice/util/ExperimentUtils.java
@@ -6,6 +6,7 @@ import org.springframework.util.Base64Utils;
 import org.springframework.util.DigestUtils;
 
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -35,19 +36,20 @@ public class ExperimentUtils {
     }
 
     /**
-     * Generate unique token for experiment by algorithm:
-     * 1. Creates salt in format uuid:creation_date_millis
+     * Generates unique token for experiment by algorithm:
+     * 1. Creates salt in format experiment_uuid:experiment_creation_date_millis
      * 2. Gets md5_salt = MD5(salt)
-     * 3. Creates string to encode: stringToEncode = md5_salt:start_date_millis
-     * 4. Gets results = base64(stringToEncode)
+     * 3. Creates string in format md5_salt:token_creation_date_millis
+     * 4. Gets results = base64(md5_salt:token_creation_date_millis)
      *
      * @param experiment - experiment entity
      * @return experiment token
      */
     public static String generateToken(Experiment experiment) {
+        LocalDateTime tokenCreationDate = LocalDateTime.now();
         String salt = String.format(SALT_FORMAT, experiment.getUuid(), toMillis(experiment.getCreationDate()));
         String md5Salt = DigestUtils.md5DigestAsHex(salt.getBytes(Charsets.UTF_8));
-        String stringToEncode = String.format(SALT_FORMAT, md5Salt, toMillis(experiment.getStartDate()));
+        String stringToEncode = String.format(SALT_FORMAT, md5Salt, toMillis(tokenCreationDate));
         return Base64Utils.encodeToString(stringToEncode.getBytes(Charsets.UTF_8));
     }
 }
