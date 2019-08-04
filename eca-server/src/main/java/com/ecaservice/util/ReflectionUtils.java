fa1be3b650b7a7d44eec33db932c5b01b1c81c41
Roman93 <bat1238@yandex.ru>
2019-08-04 20:33:04 +0700

    Added composite values support in ReflectionUtils

--- eca-server/src/main/java/com/ecaservice/util/ReflectionUtils.java
+++ eca-server/src/main/java/com/ecaservice/util/ReflectionUtils.java
@@ -3,6 +3,8 @@ package com.ecaservice.util;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 
+import static com.ecaservice.util.Utils.splitByPointSeparator;
+
 /**
  * Reflection utility class.
  *
@@ -15,30 +17,38 @@ public class ReflectionUtils {
     }
 
     /**
-     * Gets getter method name by field property.
+     * Gets getter method return type by field property. Note: Property can be composite, for example:
+     * <pre>
+     *  entity.value1
+     * </pre>
+     * If the field is composite, then the last property value is taken. In our case the last value is value1.
      *
      * @param fieldName - field name
      * @param clazz     - entity class
      * @return getter name
      * @throws IntrospectionException in case of errors
-     */
-    public static String getGetterName(String fieldName, Class<?> clazz) throws IntrospectionException {
-        PropertyDescriptor propertyDescriptor = new PropertyDescriptor(fieldName, clazz);
-        return propertyDescriptor.getReadMethod().getName();
-    }
-
-    /**
-     * Gets getter return type by field property.
-     *
-     * @param fieldName - field name
-     * @param clazz     - entity class
-     * @return getter return type
-     * @throws IntrospectionException in case on errors
      * @throws NoSuchMethodException  in case if method is not exists
      */
     public static Class<?> getGetterReturnType(String fieldName, Class<?> clazz)
             throws IntrospectionException, NoSuchMethodException {
-        String getter = getGetterName(fieldName, clazz);
-        return clazz.getMethod(getter).getReturnType();
+        String[] fieldLevels = splitByPointSeparator(fieldName);
+        String targetField = getTargetField(fieldLevels);
+        Class<?> targetClazz = getTargetClazz(fieldLevels, clazz);
+        String getter = getGetterForField(targetField, targetClazz);
+        return targetClazz.getMethod(getter).getReturnType();
+    }
+
+    private static Class<?> getTargetClazz(String[] fieldLevels, Class<?> clazz)
+            throws IntrospectionException, NoSuchMethodException {
+        return fieldLevels.length > 1 ? getGetterReturnType(fieldLevels[0], clazz) : clazz;
+    }
+
+    private static String getTargetField(String[] fieldLevels) {
+        return fieldLevels.length > 1 ? fieldLevels[1] : fieldLevels[0];
+    }
+
+    private static String getGetterForField(String fieldName, Class<?> clazz) throws IntrospectionException {
+        PropertyDescriptor propertyDescriptor = new PropertyDescriptor(fieldName, clazz);
+        return propertyDescriptor.getReadMethod().getName();
     }
 }
