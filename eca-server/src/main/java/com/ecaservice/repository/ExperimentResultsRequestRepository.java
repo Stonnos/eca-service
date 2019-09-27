432527365015727bc03fbe5edacd3335e982d9b7
Roman93 <bat1238@yandex.ru>
2019-09-27 16:32:00 +0700

    Removed unused method

--- eca-server/src/main/java/com/ecaservice/repository/ExperimentResultsRequestRepository.java
+++ eca-server/src/main/java/com/ecaservice/repository/ExperimentResultsRequestRepository.java
@@ -1,12 +1,9 @@
 package com.ecaservice.repository;
 
 import com.ecaservice.model.entity.ErsResponseStatus;
-import com.ecaservice.model.entity.Experiment;
 import com.ecaservice.model.entity.ExperimentResultsEntity;
 import com.ecaservice.model.entity.ExperimentResultsRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 /**
  * Repository to manage with {@link ExperimentResultsRequest} persistence entity.
@@ -14,15 +11,6 @@ import java.util.List;
  * @author Roman Batygin
  */
 public interface ExperimentResultsRequestRepository extends JpaRepository<ExperimentResultsRequest, Long> {
-
-    /**
-     * Gets experiment results requests for specified experiment.
-     *
-     * @param experiment - experiment
-     * @return experiment results requests list
-     */
-    List<ExperimentResultsRequest> findAllByExperiment(Experiment experiment);
-
     /**
      * Finds experiment results requests with specified status.
      *
