3a306e267ace48aa54fb5993a95a541da93dc384
Roman93 <bat1238@yandex.ru>
2020-01-17 01:00:17 +0700

    Removed unused import

--- eca-server/src/main/java/com/ecaservice/filter/AbstractFilter.java
+++ eca-server/src/main/java/com/ecaservice/filter/AbstractFilter.java
@@ -20,7 +20,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
