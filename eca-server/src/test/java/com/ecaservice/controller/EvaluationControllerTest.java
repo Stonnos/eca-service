c57f0adfbb12915f9a092ba70cfa743866690ddc
Roman93 <bat1238@yandex.ru>
2020-01-21 02:44:00 +0700

    Implements token service

--- eca-server/src/test/java/com/ecaservice/controller/EvaluationControllerTest.java
+++ eca-server/src/test/java/com/ecaservice/controller/EvaluationControllerTest.java
@@ -1,10 +1,7 @@
 package com.ecaservice.controller;
 
 import com.ecaservice.TestHelperUtils;
-import com.ecaservice.config.oauth2.MethodSecurityConfiguration;
-import com.ecaservice.configuation.Oauth2ResourceServerConfiguration;
-import com.ecaservice.configuation.Oauth2ServerConfiguration;
-import com.ecaservice.configuation.Oauth2ServerSecurityConfiguration;
+import com.ecaservice.token.TokenService;
 import com.ecaservice.configuation.annotation.Oauth2TestConfiguration;
 import com.ecaservice.controller.web.EvaluationController;
 import com.ecaservice.mapping.ClassifierInfoMapperImpl;
@@ -20,7 +17,6 @@ import com.ecaservice.web.dto.model.EvaluationLogDetailsDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -28,20 +24,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.Base64Utils;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import javax.inject.Inject;
-import java.nio.charset.StandardCharsets;
 
 import static com.ecaservice.TestHelperUtils.TEST_UUID;
 import static com.ecaservice.controller.ResponseBodyMatcher.responseBody;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -50,7 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = EvaluationController.class)
 @Oauth2TestConfiguration
-@Import({EvaluationLogMapperImpl.class, InstancesInfoMapperImpl.class,
+@Import({TokenService.class, EvaluationLogMapperImpl.class, InstancesInfoMapperImpl.class,
         ClassifierInputOptionsMapperImpl.class, ClassifierInfoMapperImpl.class})
 public class EvaluationControllerTest {
 
@@ -62,13 +51,15 @@ public class EvaluationControllerTest {
     @Inject
     private EvaluationLogMapper evaluationLogMapper;
     @Inject
+    private TokenService tokenService;
+    @Inject
     private MockMvc mockMvc;
 
     private String token;
 
     @Before
     public void init() throws Exception {
-        token = obtainAccessToken("admin", "secret");
+        token = tokenService.obtainAccessToken();
     }
 
     @Test
@@ -98,27 +89,5 @@ public class EvaluationControllerTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(responseBody().containsBody(evaluationLogDetailsDto));
-    }
-
-    private String obtainAccessToken(String username, String password) throws Exception {
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "password");
-        params.add("username", username);
-        params.add("password", password);
-
-        String credentials = Base64Utils.encodeToString("client:secret".getBytes(StandardCharsets.UTF_8));
-
-        ResultActions result = mockMvc.perform(post("/oauth/token")
-                .params(params)
-                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
-                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
-
-        String resultString = result.andReturn().getResponse().getContentAsString();
-
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        return jsonParser.parseMap(resultString).get("access_token").toString();
     }
 }
