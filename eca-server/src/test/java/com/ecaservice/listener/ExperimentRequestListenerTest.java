3f0d41377b0e878359e96139455e59591e6f5831
Roman93 <bat1238@yandex.ru>
2020-01-21 20:01:07 +0700

    Unit tests for ExperimentRequestListener

---
+++ eca-server/src/test/java/com/ecaservice/listener/ExperimentRequestListenerTest.java
@@ -0,0 +1,84 @@
+package com.ecaservice.listener;
+
+import com.ecaservice.TestHelperUtils;
+import com.ecaservice.dto.EcaResponse;
+import com.ecaservice.dto.ExperimentRequest;
+import com.ecaservice.exception.experiment.ExperimentException;
+import com.ecaservice.mapping.EcaResponseMapper;
+import com.ecaservice.model.TechnicalStatus;
+import com.ecaservice.model.entity.Experiment;
+import com.ecaservice.service.experiment.ExperimentRequestService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for checking {@link ExperimentRequestListener} functionality.
+ *
+ * @author Roman Batygin
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ExperimentRequestListenerTest {
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+    @Mock
+    private ExperimentRequestService experimentRequestService;
+    @Mock
+    private EcaResponseMapper ecaResponseMapper;
+
+    @InjectMocks
+    private ExperimentRequestListener experimentRequestListener;
+
+    @Captor
+    private ArgumentCaptor<String> replyToCaptor;
+    @Captor
+    private ArgumentCaptor<EcaResponse> ecaResponseArgumentCaptor;
+
+    @Test
+    public void testHandleMessage() {
+        ExperimentRequest evaluationRequest = TestHelperUtils.createExperimentRequest();
+        Message message = Mockito.mock(Message.class);
+        when(experimentRequestService.createExperimentRequest(evaluationRequest)).thenReturn(
+                TestHelperUtils.createExperiment(UUID.randomUUID().toString()));
+        when(ecaResponseMapper.map(any(Experiment.class))).thenReturn(new EcaResponse());
+        MessageProperties messageProperties = TestHelperUtils.buildMessageProperties();
+        when(message.getMessageProperties()).thenReturn(messageProperties);
+        experimentRequestListener.handleMessage(evaluationRequest, message);
+        verify(rabbitTemplate).convertAndSend(replyToCaptor.capture(), any(EcaResponse.class),
+                any(MessagePostProcessor.class));
+        Assertions.assertThat(replyToCaptor.getValue()).isEqualTo(messageProperties.getReplyTo());
+    }
+
+    @Test
+    public void testHandleErrorMessage() {
+        ExperimentRequest evaluationRequest = TestHelperUtils.createExperimentRequest();
+        Message message = Mockito.mock(Message.class);
+        when(experimentRequestService.createExperimentRequest(evaluationRequest)).thenThrow(
+                new ExperimentException("error"));
+        MessageProperties messageProperties = TestHelperUtils.buildMessageProperties();
+        when(message.getMessageProperties()).thenReturn(messageProperties);
+        experimentRequestListener.handleMessage(evaluationRequest, message);
+        verify(rabbitTemplate).convertAndSend(replyToCaptor.capture(), ecaResponseArgumentCaptor.capture(),
+                any(MessagePostProcessor.class));
+        Assertions.assertThat(replyToCaptor.getValue()).isEqualTo(messageProperties.getReplyTo());
+        Assertions.assertThat(ecaResponseArgumentCaptor.getValue()).isNotNull();
+        Assertions.assertThat(ecaResponseArgumentCaptor.getValue().getStatus()).isEqualTo(TechnicalStatus.ERROR);
+    }
+}
