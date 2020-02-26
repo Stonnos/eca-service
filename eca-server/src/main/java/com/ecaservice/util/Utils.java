a7acf4a9776dbaa347a0d0e0bc7ced73c3c2fc9c
Roman93 <bat1238@yandex.ru>
2020-02-26 21:40:22 +0700

    Refactoring Utils class

--- eca-server/src/main/java/com/ecaservice/util/Utils.java
+++ eca-server/src/main/java/com/ecaservice/util/Utils.java
@@ -230,14 +230,11 @@ public class Utils {
      */
     public static RequestStatusStatisticsDto toRequestStatusesStatistics(Map<RequestStatus, Long> statusStatisticsMap) {
         RequestStatusStatisticsDto requestStatusStatisticsDto = new RequestStatusStatisticsDto();
-        requestStatusStatisticsDto.setNewRequestsCount(
-                Optional.ofNullable(statusStatisticsMap.get(RequestStatus.NEW)).orElse(0L));
+        requestStatusStatisticsDto.setNewRequestsCount(statusStatisticsMap.getOrDefault(RequestStatus.NEW, 0L));
         requestStatusStatisticsDto.setFinishedRequestsCount(
-                Optional.ofNullable(statusStatisticsMap.get(RequestStatus.FINISHED)).orElse(0L));
-        requestStatusStatisticsDto.setTimeoutRequestsCount(
-                Optional.ofNullable(statusStatisticsMap.get(RequestStatus.TIMEOUT)).orElse(0L));
-        requestStatusStatisticsDto.setErrorRequestsCount(
-                Optional.ofNullable(statusStatisticsMap.get(RequestStatus.ERROR)).orElse(0L));
+                statusStatisticsMap.getOrDefault(RequestStatus.FINISHED, 0L));
+        requestStatusStatisticsDto.setTimeoutRequestsCount(statusStatisticsMap.getOrDefault(RequestStatus.TIMEOUT, 0L));
+        requestStatusStatisticsDto.setErrorRequestsCount(statusStatisticsMap.getOrDefault(RequestStatus.ERROR, 0L));
         requestStatusStatisticsDto.setTotalCount(
                 statusStatisticsMap.values().stream().mapToLong(Long::longValue).sum());
         return requestStatusStatisticsDto;
