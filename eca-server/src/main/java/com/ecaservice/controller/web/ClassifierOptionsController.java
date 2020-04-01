c843227a8afc3fd5245d74bb628a8678b28e1bae
rbatygin <r.batygin@lantan.info>
2020-04-01 16:05:30 +0700

    Added endpoints for classifier options crud operations

--- eca-server/src/main/java/com/ecaservice/controller/web/ClassifierOptionsController.java
+++ eca-server/src/main/java/com/ecaservice/controller/web/ClassifierOptionsController.java
@@ -2,17 +2,24 @@ package com.ecaservice.controller.web;
 
 import com.ecaservice.mapping.ClassifierOptionsDatabaseModelMapper;
 import com.ecaservice.model.entity.ClassifierOptionsDatabaseModel;
+import com.ecaservice.model.options.ClassifierOptions;
 import com.ecaservice.service.classifiers.ClassifierOptionsService;
 import com.ecaservice.web.dto.model.ClassifierOptionsDto;
 import com.ecaservice.web.dto.model.PageDto;
 import com.ecaservice.web.dto.model.PageRequestDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -25,7 +32,7 @@ import java.util.List;
  */
 @Api(tags = "Experiment classifiers configs API for web application")
 @RestController
-@RequestMapping("/experiment/classifiers-config")
+@RequestMapping("/experiment/classifier-options")
 @RequiredArgsConstructor
 public class ClassifierOptionsController {
 
@@ -42,8 +49,8 @@ public class ClassifierOptionsController {
             value = "Finds active classifiers options configs",
             notes = "Finds active classifiers options configs"
     )
-    @GetMapping(value = "/list")
-    public List<ClassifierOptionsDto> configs() {
+    @GetMapping(value = "/active-options")
+    public List<ClassifierOptionsDto> getActiveClassifiersOptions() {
         List<ClassifierOptionsDatabaseModel> classifierOptionsDatabaseModels =
                 classifierOptionsService.getActiveClassifiersOptions();
         return classifierOptionsDatabaseModelMapper.map(classifierOptionsDatabaseModels);
@@ -69,4 +76,33 @@ public class ClassifierOptionsController {
         return PageDto.of(classifierOptionsDtoList, pageRequestDto.getPage(),
                 classifierOptionsDatabaseModels.getTotalElements());
     }
+
+    @PreAuthorize("#oauth2.hasScope('web')")
+    @ApiOperation(
+            value = "",
+            notes = ""
+    )
+    @PostMapping(value = "/save")
+    public void save(@ApiParam(value = "Configuration id", required = true) @RequestParam long configurationId,
+                     @RequestBody ClassifierOptions classifierOptions) {
+        classifierOptionsService.saveClassifierOptions(configurationId, classifierOptions);
+    }
+
+    /**
+     * Deletes classifier options specified id.
+     *
+     * @param id - classifier options id
+     */
+    @PreAuthorize("#oauth2.hasScope('web')")
+    @ApiOperation(
+            value = "Classifier options id",
+            notes = "Classifier options id"
+    )
+    @DeleteMapping(value = "/delete")
+    public void delete(
+            @ApiParam(value = "Classifier options id", example = "1", required = true) @PathVariable Long id) {
+        classifierOptionsService.deleteOptions(id);
+    }
+
+
 }
