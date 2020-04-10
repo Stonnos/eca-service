4b08bd3b4de6d8094f199db2bf4cc8f99b2f2d01
rbatygin <r.batygin@lantan.info>
2020-04-10 15:38:44 +0700

    Implements configurations menu bar

--- eca-web/src/main/resources/src/app/classifiers-configurations/components/classifiers-configurations.component.ts
+++ eca-web/src/main/resources/src/app/classifiers-configurations/components/classifiers-configurations.component.ts
@@ -3,7 +3,7 @@ import {
   ClassifiersConfigurationDto, PageDto,
   PageRequestDto
 } from "../../../../../../../target/generated-sources/typescript/eca-web-dto";
-import { MessageService } from "primeng/api";
+import { MenuItem, MessageService } from "primeng/api";
 import { BaseListComponent } from "../../common/lists/base-list.component";
 import { Observable } from "rxjs/internal/Observable";
 import { ClassifiersConfigurationFields } from "../../common/util/field-names";
@@ -17,6 +17,11 @@ import { ClassifiersConfigurationsService } from "../services/classifiers-config
 })
 export class ClassifiersConfigurationsComponent extends BaseListComponent<ClassifiersConfigurationDto> implements OnInit {
 
+  public setActiveMenuItem: MenuItem;
+  public deleteMenuItem: MenuItem;
+  public optionsMenu: MenuItem[] = [];
+  public selectedConfiguration: ClassifiersConfigurationDto;
+
   public constructor(private injector: Injector,
                      private classifierOptionsService: ClassifiersConfigurationsService) {
     super(injector.get(MessageService), injector.get(FieldService));
@@ -27,10 +32,17 @@ export class ClassifiersConfigurationsComponent extends BaseListComponent<Classi
   }
 
   public ngOnInit() {
+    this.initMenu();
   }
 
   public getNextPageAsObservable(pageRequest: PageRequestDto): Observable<PageDto<ClassifiersConfigurationDto>> {
     return this.classifierOptionsService.getClassifiersConfigurations(pageRequest);
+  }
+
+  public onMouseClickMenu(event: any, item: ClassifiersConfigurationDto): void {
+    this.selectedConfiguration = item;
+    this.setActiveMenuItem.visible = !item.active;
+    this.deleteMenuItem.visible = !item.buildIn && !item.active;
   }
 
   private initColumns() {
@@ -39,6 +51,39 @@ export class ClassifiersConfigurationsComponent extends BaseListComponent<Classi
       { name: ClassifiersConfigurationFields.CREATED, label: "Дата создания" },
       { name: ClassifiersConfigurationFields.UPDATED, label: "Дата обновления" },
       { name: ClassifiersConfigurationFields.CLASSIFIERS_OPTIONS_COUNT, label: "Кол-во настроек классификаторов" },
+    ];
+  }
+
+  private initMenu() {
+    this.setActiveMenuItem = {
+      label: 'Сделать активной',
+      icon: 'pi pi-tag',
+      command: () => {
+        console.log('Activate ' + this.selectedConfiguration.name);
+      }
+    };
+    this.deleteMenuItem = {
+      label: 'Удалить',
+      icon: 'pi pi-fw pi-trash',
+      command: () => {
+        console.log('Delete' + this.selectedConfiguration.name);
+      }
+    };
+    this.optionsMenu = [
+      {
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Переименовать',
+            icon: 'pi pi-fw pi-pencil',
+            command: () => {
+              console.log('Renamed');
+            }
+          },
+          this.setActiveMenuItem,
+          this.deleteMenuItem
+        ]
+      }
     ];
   }
 }
