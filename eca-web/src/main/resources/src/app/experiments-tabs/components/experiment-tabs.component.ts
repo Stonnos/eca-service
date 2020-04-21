c55e179519fef4b3fa8bc13123b061c6a159775d
rbatygin <r.batygin@lantan.info>
2020-04-21 14:32:49 +0700

    Save experiment tab index in localStorage

--- eca-web/src/main/resources/src/app/experiments-tabs/components/experiment-tabs.component.ts
+++ eca-web/src/main/resources/src/app/experiments-tabs/components/experiment-tabs.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentTabsComponent implements OnInit {
 
+  private static readonly EXPERIMENT_ACTIVE_TAB_KEY: string = 'experimentTabActiveIndex';
+
+  public activeTabIndex: number = 0;
+
   public ngOnInit() {
+    this.updateActiveIndex();
+  }
+
+  public onChangeTab(event): void {
+    localStorage.setItem(ExperimentTabsComponent.EXPERIMENT_ACTIVE_TAB_KEY, event.index);
+  }
+
+  public updateActiveIndex(): void {
+    const tabIndex = localStorage.getItem(ExperimentTabsComponent.EXPERIMENT_ACTIVE_TAB_KEY);
+    if (tabIndex) {
+      this.activeTabIndex = parseInt(tabIndex);
+    }
   }
 }
