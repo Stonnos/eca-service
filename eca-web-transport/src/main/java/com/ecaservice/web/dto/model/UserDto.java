97c43ff672448e3d51ceeaeedc04eb9ba188c193
rbatygin <r.batygin@lantan.info>
2020-05-20 17:38:49 +0700

    Added creation date field in UserDto

--- eca-web-transport/src/main/java/com/ecaservice/web/dto/model/UserDto.java
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/model/UserDto.java
@@ -1,10 +1,16 @@
 package com.ecaservice.web.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.ecaservice.web.dto.util.FieldConstraints.DATE_TIME_PATTERN;
 
 /**
  * User dto model.
@@ -32,6 +38,14 @@ public class UserDto {
      */
     @ApiModelProperty(value = "User first name")
     private String firstName;
+
+    /**
+     * User creation date
+     */
+    @ApiModelProperty(value = "User creation date")
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime creationDate;
 
     /**
      * Roles list
