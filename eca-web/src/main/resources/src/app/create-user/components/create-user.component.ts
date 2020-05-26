78ba5ca371d4f79667cd648fb8185b88a1bd64a6
rbatygin <r.batygin@lantan.info>
2020-05-26 17:43:17 +0700

    User login and email validation on web client

--- eca-web/src/main/resources/src/app/create-user/components/create-user.component.ts
+++ eca-web/src/main/resources/src/app/create-user/components/create-user.component.ts
@@ -9,6 +9,9 @@ import { CreateUserModel } from "../model/create-user.model";
 })
 export class CreateUserComponent extends BaseCreateDialogComponent<CreateUserModel> implements OnInit {
 
+  public loginRegex: string = '[a-z0-9]*';
+  public emailRegex: string = '[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})';
+
   public ngOnInit(): void {
   }
 
