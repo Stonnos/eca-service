c670e2ba5fc6f7847e352355c3fe433ebe91a7a3
rbatygin <r.batygin@lantan.info>
2020-06-01 13:46:16 +0700

    Refactoring PasswordService

--- eca-oauth/src/main/java/com/ecaservice/oauth/service/PasswordService.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/service/PasswordService.java
@@ -8,6 +8,7 @@ import org.passay.PasswordGenerator;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -24,17 +25,26 @@ public class PasswordService {
     private final PasswordGenerator passwordGenerator;
     private final PasswordConfig passwordConfig;
 
+    private List<CharacterRule> rules = newArrayList();
+
+    /**
+     * Initialize password generator rules.
+     */
+    @PostConstruct
+    public void initializeRules() {
+        rules = newArrayList();
+        addRuleIfSpecified(rules, passwordConfig.getUseDigits(), EnglishCharacterData.Digit);
+        addRuleIfSpecified(rules, passwordConfig.getUseLowerCaseSymbols(), EnglishCharacterData.LowerCase);
+        addRuleIfSpecified(rules, passwordConfig.getUseUpperCaseSymbols(), EnglishCharacterData.UpperCase);
+        Assert.notEmpty(rules, "Password generator rules must be not empty! Specified at least one rule in configs");
+    }
+
     /**
      * Generates random password.
      *
      * @return random password
      */
     public String generatePassword() {
-        List<CharacterRule> rules = newArrayList();
-        addRuleIfSpecified(rules, passwordConfig.getUseDigits(), EnglishCharacterData.Digit);
-        addRuleIfSpecified(rules, passwordConfig.getUseLowerCaseSymbols(), EnglishCharacterData.LowerCase);
-        addRuleIfSpecified(rules, passwordConfig.getUseUpperCaseSymbols(), EnglishCharacterData.UpperCase);
-        Assert.notEmpty(rules, "Password generator rules must be not empty! Specified at least one rule in configs");
         return passwordGenerator.generatePassword(passwordConfig.getLength(), rules);
     }
 
