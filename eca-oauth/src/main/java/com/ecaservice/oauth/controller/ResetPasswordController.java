564e81af58000dcd1ae3f8686c90a44051d86021
rbatygin <r.batygin@lantan.info>
2020-06-02 13:42:29 +0700

    Added forgotPassword API method

--- eca-oauth/src/main/java/com/ecaservice/oauth/controller/ResetPasswordController.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/controller/ResetPasswordController.java
@@ -1,10 +1,17 @@
 package com.ecaservice.oauth.controller;
 
+import com.ecaservice.oauth.dto.ForgotPasswordRequest;
+import com.ecaservice.oauth.service.ResetPasswordService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Implements reset password REST API.
@@ -17,4 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/password")
 @RequiredArgsConstructor
 public class ResetPasswordController {
+
+    private final ResetPasswordService resetPasswordService;
+
+    /**
+     * Creates forgot password request.
+     *
+     * @param forgotPasswordRequest - forgot password request
+     */
+    @ApiOperation(
+            value = "Creates forgot password request",
+            notes = "Creates forgot password request"
+    )
+    @PostMapping(value = "/forgot")
+    public void forgotPassword(@Valid @RequestBody ForgotPasswordRequest forgotPasswordRequest) {
+        log.info("Received forgot password request {}", forgotPasswordRequest);
+        resetPasswordService.getOrSaveResetPasswordRequest(forgotPasswordRequest);
+    }
 }
