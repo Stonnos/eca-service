e7fccf7c57f85c0c184a46f132b1b0654fdab47c
rbatygin <r.batygin@lantan.info>
2020-06-02 12:41:06 +0700

    Added forgot-password.request.ts

--- eca-web/src/main/resources/src/app/change-password/components/forgot-password/forgot-password.component.ts
+++ eca-web/src/main/resources/src/app/change-password/components/forgot-password/forgot-password.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MessageService } from "primeng/api";
-import {BaseCreateDialogComponent} from "../../../common/dialog/base-create-dialog.component";
-import {ValidationService} from "../../../common/services/validation.service";
+import { ValidationService } from "../../../common/services/validation.service";
+import { BaseForm } from "../../../common/form/base-form";
+import { NgForm } from "@angular/forms";
+import { ForgotPasswordRequest } from "../model/forgot-password.request";
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent extends BaseCreateDialogComponent<string> implements OnInit {
+export class ForgotPasswordComponent implements BaseForm, OnInit {
 
+  public submitted: boolean = false;
   public loading: boolean = false;
+  public sent: boolean = false;
+
+  @ViewChild(NgForm, { static: true })
+  public form: NgForm;
+
+  public forgotPasswordRequest: ForgotPasswordRequest = new ForgotPasswordRequest();
 
   public constructor(private messageService: MessageService,
                      private validationService: ValidationService) {
-    super();
   }
 
   public ngOnInit(): void {
+  }
+
+  public clear(): void {
+    this.submitted = false;
+  }
+
+  public isValid(): boolean {
+    return this.form.valid;
+  }
+
+  public submit(): void {
+    this.submitted = true;
+    if (this.isValid()) {
+      this.sent = true;
+    }
   }
 }
