1bc0471017ee3261bb82f3fd5dd3ffa968be73f4
rbatygin <r.batygin@lantan.info>
2020-06-02 16:55:38 +0700

    Added ResetPasswordRequestCreatedEvent

---
+++ eca-oauth/src/main/java/com/ecaservice/oauth/event/model/ResetPasswordRequestCreatedEvent.java
@@ -0,0 +1,31 @@
+package com.ecaservice.oauth.event.model;
+
+import com.ecaservice.oauth.entity.ResetPasswordRequestEntity;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Reset password request created event.
+ *
+ * @author Roman Batygin
+ */
+public class ResetPasswordRequestCreatedEvent extends ApplicationEvent {
+
+    /**
+     * User entity
+     */
+    @Getter
+    private final ResetPasswordRequestEntity resetPasswordRequestEntity;
+
+    /**
+     * Create a new {@code ResetPasswordRequestCreatedEvent}.
+     *
+     * @param source                     - the object on which the event initially occurred or with which the event is
+     *                                   associated (never {@code null})
+     * @param resetPasswordRequestEntity - reset password request created entity
+     */
+    public ResetPasswordRequestCreatedEvent(Object source, ResetPasswordRequestEntity resetPasswordRequestEntity) {
+        super(source);
+        this.resetPasswordRequestEntity = resetPasswordRequestEntity;
+    }
+}
