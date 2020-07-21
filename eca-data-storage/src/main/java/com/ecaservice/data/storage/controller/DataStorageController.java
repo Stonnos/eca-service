8af6f86260f079617892a8cf1d4a1bca3efe4259
Батыгин Роман Игоревич <batygin@self.team>
2020-07-21 10:54:31 +0700

    Implements ConstraintViolationExceptionHandler

--- eca-data-storage/src/main/java/com/ecaservice/data/storage/controller/DataStorageController.java
+++ eca-data-storage/src/main/java/com/ecaservice/data/storage/controller/DataStorageController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * Data storage API for web application.
@@ -30,6 +31,9 @@ import javax.validation.constraints.NotBlank;
 @RequestMapping("/instances")
 @RequiredArgsConstructor
 public class DataStorageController {
+
+    private static final String TABLE_NAME_REGEX = "^[0-9a-z_]+$";
+    private static final int MAX_TABLE_NAME_LENGTH = 30;
 
     private final StorageService storageService;
 
@@ -47,7 +51,10 @@ public class DataStorageController {
     @PostMapping(value = "/save")
     public CreateInstancesResultDto saveInstances(
             @ApiParam(value = "Training data file", required = true) @RequestParam MultipartFile trainingData,
-            @ApiParam(value = "Table name", required = true) @NotBlank @RequestParam String tableName) {
+            @ApiParam(value = "Table name", required = true)
+            @Pattern(regexp = TABLE_NAME_REGEX)
+            @Size(max = MAX_TABLE_NAME_LENGTH)
+            @RequestParam String tableName) {
         log.info("Received request for saving instances '{}' into table [{}]",
                 trainingData.getOriginalFilename(), tableName);
         CreateInstancesResultDto createInstancesResultDto = new CreateInstancesResultDto();
