026f94e77a6b2c2f6951932cf358d184db0bf92f
Батыгин Роман Игоревич <batygin@self.team>
2020-07-22 16:57:48 +0700

    Unit test for InstancesMapper

---
+++ eca-data-storage/src/test/java/com/ecaservice/data/storage/mapping/InstancesMapperTest.java
@@ -0,0 +1,42 @@
+package com.ecaservice.data.storage.mapping;
+
+import com.ecaservice.data.storage.entity.InstancesEntity;
+import com.ecaservice.web.dto.model.InstancesDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.inject.Inject;
+
+import static com.ecaservice.data.storage.TestHelperUtils.createInstancesEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for checking {@link InstancesMapper} functionality.
+ *
+ * @author Roman Batygin
+ */
+@ExtendWith(SpringExtension.class)
+@Import(InstancesMapperImpl.class)
+class InstancesMapperTest {
+
+    private static final long ID = 1L;
+
+    @Inject
+    private InstancesMapper instancesMapper;
+
+    @Test
+    void testMapInstancesEntity() {
+        InstancesEntity instancesEntity = createInstancesEntity();
+        instancesEntity.setId(ID);
+        InstancesDto instancesDto = instancesMapper.map(instancesEntity);
+        assertThat(instancesDto).isNotNull();
+        assertThat(instancesDto.getTableName()).isEqualTo(instancesEntity.getTableName());
+        assertThat(instancesDto.getNumInstances()).isEqualTo(instancesEntity.getNumInstances());
+        assertThat(instancesDto.getNumAttributes()).isEqualTo(instancesEntity.getNumAttributes());
+        assertThat(instancesDto.getCreated()).isEqualTo(instancesEntity.getCreated());
+        assertThat(instancesDto.getCreatedBy()).isEqualTo(instancesEntity.getCreatedBy());
+        assertThat(instancesDto.getId()).isEqualTo(instancesEntity.getId());
+    }
+}
