9d9a0cd1f8ab43424b9cb6c8cab5c585d7d7f4db
Батыгин Роман Игоревич <batygin@self.team>
2020-07-22 18:07:19 +0700

    Tests for delete and rename methods

--- eca-data-storage/src/test/java/com/ecaservice/data/storage/service/StorageServiceTest.java
+++ eca-data-storage/src/test/java/com/ecaservice/data/storage/service/StorageServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.inject.Inject;
 import java.io.File;
 
+import static com.ecaservice.data.storage.TestHelperUtils.createInstancesEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -27,6 +28,7 @@ class StorageServiceTest extends AbstractJpaTest {
 
     private static final String DATA_PATH = "german_credit.xls";
     private static final String TEST_TABLE = "test_table";
+    private static final String NEW_TABLE_NAME = "new_table_name";
 
     @Inject
     private StorageService storageService;
@@ -49,7 +51,28 @@ class StorageServiceTest extends AbstractJpaTest {
         InstancesEntity expected = storageService.saveData(fileResource, TEST_TABLE);
         InstancesEntity actual = instancesRepository.findById(expected.getId()).orElse(null);
         assertThat(actual).isNotNull();
-        assertThat(actual.getId()).isEqualTo(expected.getId());
         assertThat(actual.getTableName()).isEqualTo(TEST_TABLE);
+    }
+
+    @Test
+    void testRenameData() {
+        InstancesEntity instancesEntity = createAndSaveInstancesEntity();
+        storageService.renameData(instancesEntity.getId(), NEW_TABLE_NAME);
+        InstancesEntity actual = instancesRepository.findById(instancesEntity.getId()).orElse(null);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getTableName()).isEqualTo(NEW_TABLE_NAME);
+    }
+
+    @Test
+    void testDeleteData() {
+        InstancesEntity instancesEntity = createAndSaveInstancesEntity();
+        storageService.deleteData(instancesEntity.getId());
+        assertThat(instancesRepository.existsById(instancesEntity.getId())).isFalse();
+    }
+
+    private InstancesEntity createAndSaveInstancesEntity() {
+        InstancesEntity instancesEntity = createInstancesEntity();
+        instancesEntity.setTableName(TEST_TABLE);
+        return instancesRepository.save(instancesEntity);
     }
 }
