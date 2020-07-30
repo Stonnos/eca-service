51f59a816db01524a532e652c5d8334cb843dd6c
Батыгин Роман Игоревич <batygin@self.team>
2020-07-30 12:32:56 +0700

    Fix dsUrl for production in eca-web

--- eca-web/src/main/resources/src/environments/environment.prod.ts
+++ eca-web/src/main/resources/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   serverUrl: `http://${window.location.hostname}:${window.location.port}/eca-server`,
   oauthUrl: `http://${window.location.hostname}:${window.location.port}/eca-oauth`,
+  dsUrl: `http://${window.location.hostname}:${window.location.port}/eca-ds`,
   clientId: "eca_web",
   secret: "web_secret"
 };
