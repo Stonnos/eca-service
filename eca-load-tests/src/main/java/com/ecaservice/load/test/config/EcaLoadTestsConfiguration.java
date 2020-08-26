c8758502c5acf856b9c71857bb6ecd55991d9b55
Батыгин Роман Игоревич <batygin@self.team>
2020-08-26 13:02:16 +0700

    Enables caching

--- eca-load-tests/src/main/java/com/ecaservice/load/test/config/EcaLoadTestsConfiguration.java
+++ eca-load-tests/src/main/java/com/ecaservice/load/test/config/EcaLoadTestsConfiguration.java
@@ -2,10 +2,10 @@ package com.ecaservice.load.test.config;
 
 import com.ecaservice.classifier.options.config.ClassifiersOptionsConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,7 +16,7 @@ import java.util.concurrent.Executors;
  * @author Roman Batygin
  */
 @Configuration
-@EnableScheduling
+@EnableCaching
 @EnableConfigurationProperties(EcaLoadTestsConfig.class)
 @Import(ClassifiersOptionsConfiguration.class)
 public class EcaLoadTestsConfiguration {
