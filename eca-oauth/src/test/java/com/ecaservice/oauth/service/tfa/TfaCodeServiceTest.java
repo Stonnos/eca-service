4d9d9c1c695f3bf1af9391bcb29493c01a2e43a9
Батыгин Роман Игоревич <batygin@self.team>
2020-09-16 11:24:38 +0700

    Unit tests for TfaCodeService

---
+++ eca-oauth/src/test/java/com/ecaservice/oauth/service/tfa/TfaCodeServiceTest.java
@@ -0,0 +1,57 @@
+package com.ecaservice.oauth.service.tfa;
+
+import com.ecaservice.oauth.config.TfaConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Unit tests for {@link TfaCodeService} class.
+ *
+ * @author Roman Batygin
+ */
+@ExtendWith(SpringExtension.class)
+@EnableConfigurationProperties
+@TestPropertySource("classpath:application.properties")
+@Import({TfaCodeService.class, TfaConfig.class})
+class TfaCodeServiceTest {
+
+    @Inject
+    private TfaConfig tfaConfig;
+    @Inject
+    private TfaCodeService tfaCodeService;
+
+    @Mock
+    private OAuth2Authentication oAuth2Authentication;
+
+    @Test
+    void testCreateAuthorizationCode() {
+        String code = tfaCodeService.createAuthorizationCode(oAuth2Authentication);
+        assertThat(code).hasSize(tfaConfig.getCodeLength());
+    }
+
+    @Test
+    void testConsumeAuthorizationCode() {
+        String code = tfaCodeService.createAuthorizationCode(oAuth2Authentication);
+        OAuth2Authentication authentication = tfaCodeService.consumeAuthorizationCode(code);
+        assertThat(authentication).isNotNull();
+    }
+
+    @Test
+    void testConsumeInvalidAuthorizationCode() {
+        String code = UUID.randomUUID().toString();
+        assertThrows(InvalidGrantException.class, () -> tfaCodeService.consumeAuthorizationCode(code));
+    }
+}
