b15aff3847a349d74ece695499567b53374ee7e5
Батыгин Роман Игоревич <batygin@self.team>
2020-09-17 12:55:44 +0700

    Fix code generation for multi thread environment

--- eca-oauth/src/main/java/com/ecaservice/oauth/service/tfa/TfaCodeService.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/service/tfa/TfaCodeService.java
@@ -11,6 +11,8 @@ import org.springframework.security.oauth2.provider.code.AuthorizationCodeServic
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,6 +27,8 @@ public class TfaCodeService implements AuthorizationCodeServices {
     private final TfaConfig tfaConfig;
 
     private Cache<String, OAuth2Authentication> codesCache;
+
+    private Map<String, Object> monitorsMap = new ConcurrentHashMap<>();
 
     private RandomValueStringGenerator generator = new RandomValueStringGenerator();
 
@@ -42,7 +46,14 @@ public class TfaCodeService implements AuthorizationCodeServices {
     @Override
     public String createAuthorizationCode(OAuth2Authentication authentication) {
         String code = generator.generate();
-        codesCache.put(code, authentication);
+        String user = authentication.getName();
+        monitorsMap.putIfAbsent(user, new Object());
+        synchronized (monitorsMap.get(user)) {
+            //Invalidate previous code
+            invalidatePreviousCode(user);
+            codesCache.put(code, authentication);
+        }
+        monitorsMap.remove(user);
         return code;
     }
 
@@ -54,5 +65,14 @@ public class TfaCodeService implements AuthorizationCodeServices {
         }
         codesCache.invalidate(code);
         return authentication;
+    }
+
+    private void invalidatePreviousCode(String user) {
+        codesCache.asMap().entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().getName().equals(user))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .ifPresent(oldCode -> codesCache.invalidate(oldCode));
     }
 }
