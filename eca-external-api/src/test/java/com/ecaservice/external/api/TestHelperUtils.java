654c9e66ae1ed96f2ddc1e0e670fba5fb13f640a
Батыгин Роман Игоревич <batygin@self.team>
2020-11-13 17:52:38 +0700

    Unit tests for DataScheduler

--- eca-external-api/src/test/java/com/ecaservice/external/api/TestHelperUtils.java
+++ eca-external-api/src/test/java/com/ecaservice/external/api/TestHelperUtils.java
@@ -2,8 +2,10 @@ package com.ecaservice.external.api;
 
 import com.ecaservice.external.api.dto.EvaluationRequestDto;
 import com.ecaservice.external.api.entity.EvaluationRequestEntity;
+import com.ecaservice.external.api.entity.InstancesEntity;
 import com.ecaservice.external.api.entity.RequestStageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import eca.core.evaluation.EvaluationMethod;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
@@ -25,6 +27,7 @@ public class TestHelperUtils {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String REPLY_TO = "reply-to";
+    private static final String ABSOLUTE_PATH = "data.csv";
 
     /**
      * Creates evaluation request dto.
@@ -59,8 +62,38 @@ public class TestHelperUtils {
     public static EvaluationRequestEntity createEvaluationRequestEntity(String correlationId) {
         EvaluationRequestEntity evaluationRequestEntity = new EvaluationRequestEntity();
         evaluationRequestEntity.setRequestStage(RequestStageType.NOT_SEND);
+        evaluationRequestEntity.setEvaluationMethod(EvaluationMethod.TRAINING_DATA);
         evaluationRequestEntity.setCreationDate(LocalDateTime.now());
         evaluationRequestEntity.setCorrelationId(correlationId);
         return evaluationRequestEntity;
+    }
+
+    /**
+     * Creates evaluation request entity.
+     *
+     * @param requestStageType - request stage type
+     * @param endDate          - end date
+     * @return evaluation request entity
+     */
+    public static EvaluationRequestEntity createEvaluationRequestEntity(RequestStageType requestStageType,
+                                                                        LocalDateTime endDate) {
+        EvaluationRequestEntity evaluationRequestEntity = createEvaluationRequestEntity(UUID.randomUUID().toString());
+        evaluationRequestEntity.setRequestStage(requestStageType);
+        evaluationRequestEntity.setEndDate(endDate);
+        return evaluationRequestEntity;
+    }
+
+    /**
+     * Creates instances entity.
+     *
+     * @param creationDate - creation date
+     * @return instances entity
+     */
+    public static InstancesEntity createInstancesEntity(LocalDateTime creationDate) {
+        InstancesEntity instancesEntity = new InstancesEntity();
+        instancesEntity.setUuid(UUID.randomUUID().toString());
+        instancesEntity.setAbsolutePath(ABSOLUTE_PATH);
+        instancesEntity.setCreationDate(creationDate);
+        return instancesEntity;
     }
 }
---
+++ eca-external-api/src/test/java/com/ecaservice/external/api/scheduler/DataSchedulerTest.java
@@ -0,0 +1,75 @@
+package com.ecaservice.external.api.scheduler;
+
+import com.ecaservice.external.api.AbstractJpaTest;
+import com.ecaservice.external.api.config.ExternalApiConfig;
+import com.ecaservice.external.api.entity.RequestStageType;
+import com.ecaservice.external.api.repository.EcaRequestRepository;
+import com.ecaservice.external.api.repository.InstancesRepository;
+import com.ecaservice.external.api.service.FileDataService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import javax.inject.Inject;
+import java.time.LocalDateTime;
+
+import static com.ecaservice.external.api.TestHelperUtils.createEvaluationRequestEntity;
+import static com.ecaservice.external.api.TestHelperUtils.createInstancesEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for checking {@link DataScheduler} functionality.
+ *
+ * @author Roman Batygin
+ */
+@Import({ExternalApiConfig.class, DataScheduler.class})
+class DataSchedulerTest extends AbstractJpaTest {
+
+    @MockBean
+    private FileDataService fileDataService;
+
+    @Inject
+    private ExternalApiConfig externalApiConfig;
+    @Inject
+    private InstancesRepository instancesRepository;
+    @Inject
+    private EcaRequestRepository ecaRequestRepository;
+
+    @Inject
+    private DataScheduler dataScheduler;
+
+    @Override
+    public void deleteAll() {
+        instancesRepository.deleteAll();
+        ecaRequestRepository.deleteAll();
+    }
+
+    @Test
+    void testClearClassifiers() {
+        LocalDateTime dateTime = LocalDateTime.now().minusDays(externalApiConfig.getNumberOfDaysForStorage() + 1);
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.REQUEST_SENT, null));
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.ERROR, LocalDateTime.now()));
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.ERROR, dateTime));
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.COMPLETED, dateTime));
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.COMPLETED, LocalDateTime.now()));
+        ecaRequestRepository.save(createEvaluationRequestEntity(RequestStageType.EXCEEDED, dateTime));
+        dataScheduler.clearClassifiers();
+        verify(fileDataService, atLeastOnce()).delete(any());
+    }
+
+    @Test
+    void testClearData() {
+        LocalDateTime dateTime = LocalDateTime.now().minusDays(externalApiConfig.getNumberOfDaysForStorage() + 1);
+        instancesRepository.save(createInstancesEntity(LocalDateTime.now()));
+        instancesRepository.save(createInstancesEntity(dateTime));
+        when(fileDataService.delete(anyString())).thenReturn(true);
+        dataScheduler.clearData();
+        verify(fileDataService, atLeastOnce()).delete(anyString());
+        assertThat(instancesRepository.count()).isOne();
+    }
+}
