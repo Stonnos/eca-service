6b49e2fee3cf4229f4ab0d8305377983a4453729
Батыгин Роман Игоревич <batygin@self.team>
2020-11-13 18:27:09 +0700

    Unit tests for EcaResponseHandler

--- eca-external-api/src/test/java/com/ecaservice/external/api/TestHelperUtils.java
+++ eca-external-api/src/test/java/com/ecaservice/external/api/TestHelperUtils.java
@@ -1,18 +1,29 @@
 package com.ecaservice.external.api;
 
+import com.ecaservice.base.model.EvaluationResponse;
+import com.ecaservice.base.model.TechnicalStatus;
 import com.ecaservice.external.api.dto.EvaluationRequestDto;
 import com.ecaservice.external.api.entity.EvaluationRequestEntity;
 import com.ecaservice.external.api.entity.InstancesEntity;
 import com.ecaservice.external.api.entity.RequestStageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import eca.core.evaluation.Evaluation;
 import eca.core.evaluation.EvaluationMethod;
+import eca.core.evaluation.EvaluationResults;
+import eca.core.evaluation.EvaluationService;
+import eca.data.file.resource.FileResource;
+import eca.data.file.xls.XLSLoader;
+import eca.trees.CART;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.springframework.amqp.core.MessageProperties;
+import weka.core.Instances;
 
+import java.io.File;
 import java.io.InputStream;
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -24,10 +35,38 @@ import java.util.UUID;
 public class TestHelperUtils {
 
     private static final String TEST_REQUEST_JSON = "test-request.json";
+    private static final String DATA_PATH = "data/iris.xls";
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String REPLY_TO = "reply-to";
     private static final String ABSOLUTE_PATH = "data.csv";
+
+    /**
+     * Generates the test data set.
+     *
+     * @return created training data
+     */
+    @SneakyThrows
+    public static Instances loadInstances() {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        XLSLoader dataLoader = new XLSLoader();
+        dataLoader.setSource(new FileResource(new File(classLoader.getResource(DATA_PATH).getFile())));
+        return dataLoader.loadInstances();
+    }
+
+    /**
+     * Evaluation classifier and returns its evaluation results.
+     *
+     * @return evaluation results
+     */
+    @SneakyThrows
+    public static EvaluationResults getEvaluationResults() {
+        CART cart = new CART();
+        Instances testInstances = loadInstances();
+        Evaluation evaluation = EvaluationService.evaluateModel(cart, testInstances,
+                EvaluationMethod.TRAINING_DATA, 0, 0, null);
+        return new EvaluationResults(cart, evaluation);
+    }
 
     /**
      * Creates evaluation request dto.
@@ -95,5 +134,30 @@ public class TestHelperUtils {
         instancesEntity.setAbsolutePath(ABSOLUTE_PATH);
         instancesEntity.setCreationDate(creationDate);
         return instancesEntity;
+    }
+
+    /**
+     * Creates evaluation response with success status.
+     *
+     * @return evaluation response
+     */
+    public static EvaluationResponse successEvaluationResponse() {
+        EvaluationResponse evaluationResponse = new EvaluationResponse();
+        evaluationResponse.setEvaluationResults(getEvaluationResults());
+        evaluationResponse.setRequestId(UUID.randomUUID().toString());
+        evaluationResponse.setStatus(TechnicalStatus.SUCCESS);
+        return evaluationResponse;
+    }
+
+    /**
+     * Creates evaluation response with error status.
+     *
+     * @return evaluation response
+     */
+    public static EvaluationResponse errorEvaluationResponse() {
+        EvaluationResponse evaluationResponse = new EvaluationResponse();
+        evaluationResponse.setRequestId(UUID.randomUUID().toString());
+        evaluationResponse.setStatus(TechnicalStatus.ERROR);
+        return evaluationResponse;
     }
 }
