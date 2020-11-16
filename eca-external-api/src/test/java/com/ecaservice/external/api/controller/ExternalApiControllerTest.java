43bf5c2438a364d72a30b38d37211587e8e1d7bf
Батыгин Роман Игоревич <batygin@self.team>
2020-11-16 19:38:25 +0700

    Unit tests for ExternalApiController#downloadModel

--- eca-external-api/src/test/java/com/ecaservice/external/api/controller/ExternalApiControllerTest.java
+++ eca-external-api/src/test/java/com/ecaservice/external/api/controller/ExternalApiControllerTest.java
@@ -1,29 +1,23 @@
 package com.ecaservice.external.api.controller;
 
-import com.ecaservice.base.model.ExperimentRequest;
-import com.ecaservice.base.model.ExperimentType;
 import com.ecaservice.external.api.config.ExternalApiConfig;
 import com.ecaservice.external.api.dto.InstancesDto;
+import com.ecaservice.external.api.entity.EvaluationRequestEntity;
 import com.ecaservice.external.api.entity.InstancesEntity;
 import com.ecaservice.external.api.mapping.EcaRequestMapper;
 import com.ecaservice.external.api.repository.EcaRequestRepository;
 import com.ecaservice.external.api.repository.EvaluationRequestRepository;
-import com.ecaservice.external.api.service.EcaResponseHandler;
 import com.ecaservice.external.api.service.EvaluationApiService;
 import com.ecaservice.external.api.service.InstancesService;
 import com.ecaservice.external.api.service.MessageCorrelationService;
 import com.ecaservice.external.api.service.RequestStageHandler;
 import com.ecaservice.oauth2.test.controller.AbstractControllerTest;
-import com.ecaservice.user.model.UserDetailsImpl;
-import eca.core.evaluation.EvaluationMethod;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -31,13 +25,9 @@ import java.util.UUID;
 import static com.ecaservice.external.api.TestHelperUtils.createEvaluationRequestEntity;
 import static com.ecaservice.external.api.TestHelperUtils.createInstancesEntity;
 import static com.ecaservice.external.api.TestHelperUtils.createInstancesMockMultipartFile;
-import static com.ecaservice.external.api.TestHelperUtils.errorEvaluationResponse;
-import static com.ecaservice.external.api.TestHelperUtils.successEvaluationResponse;
 import static com.ecaservice.external.api.util.Constants.DATA_URL_PREFIX;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,6 +42,7 @@ class ExternalApiControllerTest extends AbstractControllerTest {
 
     private static final String BASE_URL = "/";
     private static final String UPLOAD_DATA_URL = BASE_URL + "uploads-train-data";
+    private static final String DOWNLOAD_MODEL_URL = BASE_URL + "download-model/{requestId}";
     private static final String INVALID_DATA = "data.png";
 
     @MockBean
@@ -102,5 +93,30 @@ class ExternalApiControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(objectMapper.writeValueAsString(expected)));
+    }
+
+    @Test
+    void testDownloadModelUnauthorized() throws Exception {
+        mockMvc.perform(get(DOWNLOAD_MODEL_URL, UUID.randomUUID().toString())).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void testDownloadModelForNotExistingRequest() throws Exception {
+        String requestId = UUID.randomUUID().toString();
+        when(evaluationRequestRepository.findByCorrelationId(requestId)).thenReturn(null);
+        mockMvc.perform(get(DOWNLOAD_MODEL_URL, requestId)
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDownloadModelWithNotExistingFile() throws Exception {
+        EvaluationRequestEntity evaluationRequestEntity = createEvaluationRequestEntity(UUID.randomUUID().toString());
+        evaluationRequestEntity.setClassifierAbsolutePath(null);
+        when(evaluationRequestRepository.findByCorrelationId(evaluationRequestEntity.getCorrelationId())).thenReturn(
+                evaluationRequestEntity);
+        mockMvc.perform(get(DOWNLOAD_MODEL_URL, evaluationRequestEntity.getCorrelationId())
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken()))
+                .andExpect(status().isNotFound());
     }
 }
