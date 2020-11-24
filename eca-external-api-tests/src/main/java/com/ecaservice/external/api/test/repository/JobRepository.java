b0a4d309dd568e1ea848adf85badd36bf8193ba2
Батыгин Роман Игоревич <batygin@self.team>
2020-11-24 13:08:07 +0700

    Implements AutoTestsScheduler

--- eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/repository/JobRepository.java
+++ eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/repository/JobRepository.java
@@ -2,7 +2,9 @@ package com.ecaservice.external.api.test.repository;
 
 import com.ecaservice.external.api.test.entity.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,6 +13,14 @@ import java.util.Optional;
  * @author Roman Batygin
  */
 public interface JobRepository extends JpaRepository<JobEntity, Long> {
+
+    /**
+     * Finds new auto tests jobs.
+     *
+     * @return jobs list
+     */
+    @Query("select job from JobEntity job where job.executionStatus = 'NEW' order by job.created")
+    List<JobEntity> findNewJobs();
 
     /**
      * Finds job entity by uuid.
