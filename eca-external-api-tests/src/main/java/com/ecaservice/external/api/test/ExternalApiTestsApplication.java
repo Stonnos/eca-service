11630f9c845021b9b2c1d4a3d1d4f65337f0edf4
Батыгин Роман Игоревич <batygin@self.team>
2020-12-25 16:51:05 +0700

    #ES-58 - camunda configuration

--- eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/ExternalApiTestsApplication.java
+++ eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/ExternalApiTestsApplication.java
@@ -1,5 +1,6 @@
 package com.ecaservice.external.api.test;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author Roman Batygin
  */
 @SpringBootApplication
+@EnableProcessApplication
 @EnableFeignClients
 public class ExternalApiTestsApplication extends SpringBootServletInitializer {
 
