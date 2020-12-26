f25c806e2ced8f58d441e6d6347f07a086d01e59
Батыгин Роман Игоревич <batygin@self.team>
2020-12-26 15:18:55 +0700

    #ES-58 - refactoring

--- eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/bpm/service/task/EvaluationRequestHandler.java
+++ eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/bpm/service/task/EvaluationRequestHandler.java
@@ -4,14 +4,9 @@ import com.ecaservice.external.api.dto.EvaluationResponseDto;
 import com.ecaservice.external.api.dto.RequestStatus;
 import com.ecaservice.external.api.dto.ResponseDto;
 import com.ecaservice.external.api.test.bpm.model.TaskType;
-import com.ecaservice.external.api.test.entity.AutoTestEntity;
-import com.ecaservice.external.api.test.exception.EntityNotFoundException;
 import com.ecaservice.external.api.test.exception.ProcessEvaluationException;
 import com.ecaservice.external.api.test.model.TestDataModel;
-import com.ecaservice.external.api.test.repository.AutoTestRepository;
 import com.ecaservice.external.api.test.service.api.ExternalApiClient;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Component;
@@ -21,7 +16,6 @@ import static com.ecaservice.external.api.test.bpm.CamundaVariables.AUTO_TEST_ID
 import static com.ecaservice.external.api.test.bpm.CamundaVariables.EVALUATION_RESPONSE;
 import static com.ecaservice.external.api.test.bpm.CamundaVariables.TEST_DATA_MODEL;
 import static com.ecaservice.external.api.test.util.CamundaUtils.getVariable;
-import static com.ecaservice.external.api.test.util.Utils.getValueSafe;
 
 /**
  * Implements handler to send evaluation request.
@@ -33,27 +27,19 @@ import static com.ecaservice.external.api.test.util.Utils.getValueSafe;
 public class EvaluationRequestHandler extends ExternalApiTaskHandler {
 
     private final ExternalApiClient externalApiClient;
-    private final ObjectMapper objectMapper;
-    private final AutoTestRepository autoTestRepository;
 
     /**
      * Constructor with spring dependency injection.
      *
-     * @param externalApiClient  - external api client bean
-     * @param objectMapper       - object mapper bean
-     * @param autoTestRepository - auto test repository bean
+     * @param externalApiClient - external api client bean
      */
-    public EvaluationRequestHandler(ExternalApiClient externalApiClient,
-                                    ObjectMapper objectMapper,
-                                    AutoTestRepository autoTestRepository) {
+    public EvaluationRequestHandler(ExternalApiClient externalApiClient) {
         super(TaskType.EVALUATION_REQUEST);
         this.externalApiClient = externalApiClient;
-        this.objectMapper = objectMapper;
-        this.autoTestRepository = autoTestRepository;
     }
 
     @Override
-    protected void internalHandle(DelegateExecution execution) throws JsonProcessingException {
+    protected void internalHandle(DelegateExecution execution) {
         Long autoTestId = getVariable(execution, AUTO_TEST_ID, Long.class);
         TestDataModel testDataModel = getVariable(execution, TEST_DATA_MODEL, TestDataModel.class);
         log.debug("Starting to send evaluation request for test [{}]", autoTestId);
@@ -64,17 +50,6 @@ public class EvaluationRequestHandler extends ExternalApiTaskHandler {
         }
         Assert.notNull(response.getPayload(),
                 String.format("Expected not null evaluation response for auto test [%d]", autoTestId));
-        saveResponse(autoTestId, response);
         execution.setVariable(EVALUATION_RESPONSE, response);
-    }
-
-    private void saveResponse(Long autoTestId, ResponseDto<EvaluationResponseDto> response)
-            throws JsonProcessingException {
-        AutoTestEntity autoTestEntity = autoTestRepository.findById(autoTestId)
-                .orElseThrow(() -> new EntityNotFoundException(AutoTestEntity.class, autoTestId));
-        autoTestEntity.setResponse(objectMapper.writeValueAsString(response));
-        String requestId = getValueSafe(response, EvaluationResponseDto::getRequestId);
-        autoTestEntity.setRequestId(requestId);
-        autoTestRepository.save(autoTestEntity);
     }
 }
--- eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/bpm/service/task/EvaluationResponseComparisonHandler.java
+++ eca-external-api-tests/src/main/java/com/ecaservice/external/api/test/bpm/service/task/EvaluationResponseComparisonHandler.java
@@ -9,6 +9,8 @@ import com.ecaservice.external.api.test.entity.MatchResult;
 import com.ecaservice.external.api.test.model.TestDataModel;
 import com.ecaservice.external.api.test.repository.AutoTestRepository;
 import com.ecaservice.external.api.test.service.TestResultsMatcher;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Component;
 
@@ -28,38 +30,55 @@ public class EvaluationResponseComparisonHandler extends ComparisonTaskHandler {
     private static final String DOWNLOAD_URL_FORMAT = "%s/download-model/%s";
 
     private final ExternalApiTestsConfig externalApiTestsConfig;
+    private final ObjectMapper objectMapper;
 
     /**
      * Constructor with parameters.
      *
      * @param autoTestRepository     - auto test repository bean
      * @param externalApiTestsConfig - external api config bean
+     * @param objectMapper           - object mapper bean
      */
     public EvaluationResponseComparisonHandler(AutoTestRepository autoTestRepository,
-                                               ExternalApiTestsConfig externalApiTestsConfig) {
+                                               ExternalApiTestsConfig externalApiTestsConfig,
+                                               ObjectMapper objectMapper) {
         super(TaskType.COMPARE_EVALUATION_RESPONSE_RESULT, autoTestRepository);
         this.externalApiTestsConfig = externalApiTestsConfig;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     protected void compareAndMatchFields(DelegateExecution execution,
                                          AutoTestEntity autoTestEntity,
-                                         TestResultsMatcher matcher) {
+                                         TestResultsMatcher matcher) throws JsonProcessingException {
         TestDataModel testDataModel = getVariable(execution, TEST_DATA_MODEL, TestDataModel.class);
         ResponseDto<EvaluationResponseDto> responseDto = getVariable(execution, EVALUATION_RESPONSE, ResponseDto.class);
-        EvaluationResponseDto evaluationResponseDto = responseDto.getPayload();
+        saveResponse(autoTestEntity, responseDto);
         //Compare and match evaluation response status
         compareAndMatchRequestStatus(autoTestEntity, testDataModel.getExpectedResponse().getRequestStatus(),
                 responseDto.getRequestStatus(), matcher);
         //Compare and match model url
+        compareAndMatchModelUrl(responseDto, autoTestEntity, matcher);
+    }
+
+    private void compareAndMatchModelUrl(ResponseDto<EvaluationResponseDto> responseDto,
+                                         AutoTestEntity autoTestEntity,
+                                         TestResultsMatcher matcher) {
         String actualModelUrl = getValueSafe(responseDto, EvaluationResponseDto::getModelUrl);
         String expectedModelUrl =
                 String.format(DOWNLOAD_URL_FORMAT, externalApiTestsConfig.getDownloadBaseUrl(),
-                        evaluationResponseDto.getRequestId());
+                        responseDto.getPayload().getRequestId());
         autoTestEntity.setExpectedModelUrl(expectedModelUrl);
         autoTestEntity.setActualModelUrl(actualModelUrl);
         MatchResult modelUrlMatchResult = matcher.compareAndMatch(expectedModelUrl, actualModelUrl);
         autoTestEntity.setModelUrlMatchResult(modelUrlMatchResult);
+    }
+
+    private void saveResponse(AutoTestEntity autoTestEntity, ResponseDto<EvaluationResponseDto> response)
+            throws JsonProcessingException {
+        autoTestEntity.setResponse(objectMapper.writeValueAsString(response));
+        String requestId = getValueSafe(response, EvaluationResponseDto::getRequestId);
+        autoTestEntity.setRequestId(requestId);
     }
 }
