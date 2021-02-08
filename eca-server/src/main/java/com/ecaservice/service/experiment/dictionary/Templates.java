dbfb1481f12cb581f5bc8e7e16caa11ce5af580b
Батыгин Роман Игоревич <batygin@self.team>
2021-02-08 13:35:56 +0700

    #ES-64 - refactoring EmailTemplateVisitor

---
+++ eca-server/src/main/java/com/ecaservice/service/experiment/dictionary/Templates.java
@@ -0,0 +1,32 @@
+package com.ecaservice.service.experiment.dictionary;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Email template codes.
+ *
+ * @author Roman Batygin
+ */
+@UtilityClass
+public class Templates {
+
+    /**
+     * New experiment template
+     */
+    public static final String NEW_EXPERIMENT = "NEW_EXPERIMENT";
+
+    /**
+     * Finished experiment template
+     */
+    public static final String FINISHED_EXPERIMENT = "FINISHED_EXPERIMENT";
+
+    /**
+     * Timeout experiment template
+     */
+    public static final String TIMEOUT_EXPERIMENT = "TIMEOUT_EXPERIMENT";
+
+    /**
+     * Error experiment template
+     */
+    public static final String ERROR_EXPERIMENT = "ERROR_EXPERIMENT";
+}
