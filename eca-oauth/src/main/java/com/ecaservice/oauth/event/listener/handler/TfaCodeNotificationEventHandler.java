2a9f8d67a10a39a7da1099427e299a6b04133caa
Батыгин Роман Игоревич <batygin@self.team>
2021-02-24 14:54:02 +0700

    #ES-78 - implements TfaCodeNotificationEventHandler

---
+++ eca-oauth/src/main/java/com/ecaservice/oauth/event/listener/handler/TfaCodeNotificationEventHandler.java
@@ -0,0 +1,38 @@
+package com.ecaservice.oauth.event.listener.handler;
+
+import com.ecaservice.oauth.event.model.TfaCodeNotificationEvent;
+import com.ecaservice.oauth.service.mail.dictionary.Templates;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static com.ecaservice.oauth.service.mail.dictionary.TemplateVariablesDictionary.TFA_CODE;
+
+/**
+ * Tfa code notification event handler.
+ *
+ * @author Roman Batygin
+ */
+@Slf4j
+@Component
+public class TfaCodeNotificationEventHandler extends AbstractNotificationEventHandler<TfaCodeNotificationEvent> {
+
+    /**
+     * Creates tfa code notification event handler.
+     */
+    public TfaCodeNotificationEventHandler() {
+        super(TfaCodeNotificationEvent.class, Templates.TFA_CODE);
+    }
+
+    @Override
+    Map<String, String> createVariables(TfaCodeNotificationEvent event) {
+        return Collections.singletonMap(TFA_CODE, event.getTfaCode());
+    }
+
+    @Override
+    String getReceiver(TfaCodeNotificationEvent event) {
+        return event.getUserEntity().getEmail();
+    }
+}
---
+++ eca-oauth/src/main/java/com/ecaservice/oauth/event/model/TfaCodeNotificationEvent.java
@@ -0,0 +1,38 @@
+package com.ecaservice.oauth.event.model;
+
+import com.ecaservice.oauth.entity.UserEntity;
+import lombok.Getter;
+
+/**
+ * Tfa code notification event.
+ *
+ * @author Roman Batygin
+ */
+public class TfaCodeNotificationEvent extends AbstractNotificationEvent {
+
+    /**
+     * User entity
+     */
+    @Getter
+    private final UserEntity userEntity;
+
+    /**
+     * Tfa code
+     */
+    @Getter
+    private final String tfaCode;
+
+    /**
+     * Create a new {@code TfaCodeNotificationEvent}.
+     *
+     * @param source     - the object on which the event initially occurred or with which the event is
+     *                   associated (never {@code null})
+     * @param userEntity - user entity
+     * @param tfaCode    - tfa code
+     */
+    public TfaCodeNotificationEvent(Object source, UserEntity userEntity, String tfaCode) {
+        super(source);
+        this.userEntity = userEntity;
+        this.tfaCode = tfaCode;
+    }
+}
