c90eab9d2d2a104ff99d591b5d229642d5b03c92
Батыгин Роман Игоревич <batygin@self.team>
2021-02-25 17:56:47 +0700

    #ES-78 - some tests

--- eca-oauth/src/test/java/com/ecaservice/oauth/controller/ChangePasswordControllerTest.java
+++ eca-oauth/src/test/java/com/ecaservice/oauth/controller/ChangePasswordControllerTest.java
@@ -1,9 +1,11 @@
 package com.ecaservice.oauth.controller;
 
+import com.ecaservice.oauth.dto.ChangePasswordRequest;
 import com.ecaservice.oauth.entity.ChangePasswordRequestEntity;
 import com.ecaservice.oauth.service.ChangePasswordService;
 import com.ecaservice.oauth.service.Oauth2TokenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.inject.Inject;
@@ -40,6 +43,8 @@ class ChangePasswordControllerTest {
 
     private static final String BASE_URL = "/password/change";
     private static final String APPROVE_URL = BASE_URL + "/approve";
+    private static final String REQUEST_URL = BASE_URL + "/request";
+    private static final String PASSWORD = "pa66word!";
 
     @MockBean
     private ChangePasswordService changePasswordService;
@@ -52,6 +57,24 @@ class ChangePasswordControllerTest {
     private MockMvc mockMvc;
 
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void testCreateChangePasswordRequestWithEmptyOldPassword() throws Exception {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(StringUtils.EMPTY, PASSWORD);
+        mockMvc.perform(post(REQUEST_URL)
+                .content(objectMapper.writeValueAsString(changePasswordRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testCreateChangePasswordRequestWithEmptyNewPassword() throws Exception {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(PASSWORD, StringUtils.EMPTY);
+        mockMvc.perform(post(REQUEST_URL)
+                .content(objectMapper.writeValueAsString(changePasswordRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
     void testApproveChangePasswordRequest() throws Exception {
