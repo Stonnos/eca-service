31245f104d0741c0510a51a55c3b59562dccf5f6
Батыгин Роман Игоревич <batygin@self.team>
2021-03-12 12:42:26 +0700

    #ES-84 - sonar fix

--- eca-server/src/main/java/com/ecaservice/listener/support/ValidationErrorTranslator.java
+++ eca-server/src/main/java/com/ecaservice/listener/support/ValidationErrorTranslator.java
@@ -4,9 +4,13 @@ import com.ecaservice.base.model.EcaResponse;
 import com.ecaservice.base.model.MessageError;
 import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.ecaservice.util.Utils.buildValidationError;
@@ -23,7 +27,10 @@ public class ValidationErrorTranslator extends AbstractExceptionTranslator<Metho
 
     @Override
     public EcaResponse translate(MethodArgumentNotValidException exception) {
-        List<MessageError> errors = exception.getBindingResult().getAllErrors()
+        List<ObjectError> objectErrors = Optional.ofNullable(exception.getBindingResult())
+                .map(BindingResult::getAllErrors)
+                .orElse(Collections.emptyList());
+        List<MessageError> errors = objectErrors
                 .stream()
                 .map(FieldError.class::cast)
                 .map(fieldError -> new MessageError(fieldError.getCode(), fieldError.getField(),
