1cec4de88f7c9887a978386a2850bd9098de3a87
Батыгин Роман Игоревич <batygin@self.team>
2021-03-25 11:19:43 +0700

    #ES-89 - enable GlobalExceptionHandler in eca-server

--- eca-server/src/main/java/com/ecaservice/config/EcaServiceConfiguration.java
+++ eca-server/src/main/java/com/ecaservice/config/EcaServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.ecaservice.config;
 
 import com.ecaservice.classifier.options.config.ClassifiersOptionsConfiguration;
+import com.ecaservice.common.web.annotation.EnableGlobalExceptionHandler;
 import com.ecaservice.config.ers.ErsConfig;
 import com.ecaservice.oauth2.annotation.Oauth2ResourceServer;
 import eca.data.file.FileDataLoader;
@@ -27,6 +28,7 @@ import java.util.concurrent.Executors;
 @EnableScheduling
 @EnableCaching
 @EnableAsync
+@EnableGlobalExceptionHandler
 @Oauth2ResourceServer
 @EnableConfigurationProperties(
         {CommonConfig.class, CrossValidationConfig.class, ExperimentConfig.class, ErsConfig.class})
--- eca-server/src/main/java/com/ecaservice/controller/web/ErrorHandler.java
+++ eca-server/src/main/java/com/ecaservice/controller/web/ErrorHandler.java
@@ -1,17 +1,11 @@
 package com.ecaservice.controller.web;
 
-import com.ecaservice.common.web.ExceptionResponseHandler;
-import com.ecaservice.common.web.dto.ValidationErrorDto;
 import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.validation.ConstraintViolationException;
-import java.util.List;
 
 /**
  * Exception handler for controllers.
@@ -23,18 +17,6 @@ import java.util.List;
 public class ErrorHandler {
 
     /**
-     * Handles bad request error.
-     *
-     * @param ex -  exception
-     * @return response entity
-     */
-    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
-    public ResponseEntity<String> handleBadRequest(Exception ex) {
-        log.error(ex.getMessage());
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    /**
      * Handles feign client unauthorized error.
      *
      * @param ex -  exception
@@ -42,33 +24,7 @@ public class ErrorHandler {
      */
     @ExceptionHandler(FeignException.Unauthorized.class)
     public ResponseEntity<String> handleFeignUnauthorized(FeignException.Unauthorized ex) {
-        log.error(ex.getMessage());
+        log.error("Feign client unauthorized error: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    /**
-     * Handles validation error.
-     *
-     * @param ex -  method argument not valid exception
-     * @return response entity
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<ValidationErrorDto>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        var response = ExceptionResponseHandler.handleMethodArgumentNotValid(ex);
-        log.error("Request validation error: {}", response.getBody());
-        return response;
-    }
-
-    /**
-     * Handles constraint violation error.
-     *
-     * @param ex - constraint violation exception
-     * @return response entity
-     */
-    @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity<List<ValidationErrorDto>> handleConstraintViolation(ConstraintViolationException ex) {
-        var response = ExceptionResponseHandler.handleConstraintViolation(ex);
-        log.error("Request validation error: {}", response.getBody());
-        return response;
     }
 }
