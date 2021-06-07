06b52307c7d951d0c12b347a5260400dbecf1b05
Батыгин Роман Игоревич <batygin@self.team>
2021-06-07 19:04:40 +0700

    #ES-114 - CR fixes

--- eca-web/src/main/resources/src/app/classifiers-configuration-details/components/classifiers-configuration-details.component.ts
+++ eca-web/src/main/resources/src/app/classifiers-configuration-details/components/classifiers-configuration-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Injector, OnInit} from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import {
   ClassifierOptionsDto, ClassifiersConfigurationDto, PageDto,
   PageRequestDto
