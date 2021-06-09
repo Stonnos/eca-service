00df40d0c65f867eb74b9ac2440ca0f3bd09b947
Батыгин Роман Игоревич <batygin@self.team>
2021-06-09 18:38:00 +0700

    #ES-115 - added cache

--- core-audit/src/main/java/com/ecaservice/core/audit/service/store/DatabaseAuditCodeStore.java
+++ core-audit/src/main/java/com/ecaservice/core/audit/service/store/DatabaseAuditCodeStore.java
@@ -8,9 +8,16 @@ import com.ecaservice.core.audit.model.AuditEventTemplateModel;
 import com.ecaservice.core.audit.repository.AuditCodeRepository;
 import com.ecaservice.core.audit.repository.AuditEventTemplateRepository;
 import com.ecaservice.core.audit.service.AuditCodeStore;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Service to manage with audit codes.
@@ -26,15 +33,53 @@ public class DatabaseAuditCodeStore implements AuditCodeStore {
     private final AuditCodeRepository auditCodeRepository;
     private final AuditEventTemplateRepository auditEventTemplateRepository;
 
+    private LoadingCache<CacheKey, AuditEventTemplateModel> eventTemplatesCache;
+
+    /**
+     * Cache key model.
+     */
+    @Data
+    @AllArgsConstructor
+    private static class CacheKey {
+        /**
+         * Audit code
+         */
+        String auditCode;
+        /**
+         * Event type
+         */
+        EventType eventType;
+    }
+
+    /**
+     * Initialize cache.
+     */
+    @PostConstruct
+    public void initializeCache() {
+        log.info("Starting to initialize audit templates cache");
+        this.eventTemplatesCache = CacheBuilder.newBuilder()
+                .build(createCacheLoader());
+        log.info("Audit templates cache has been initialized");
+    }
+
     @Override
     public AuditEventTemplateModel getAuditEventTemplate(String auditCode, EventType eventType) {
         log.debug("Gets audit event with code [{}], type [{}]", auditCode, eventType);
-        var auditCodeEntity = getAuditCode(auditCode);
-        var auditCodeTemplate = auditEventTemplateRepository
-                .findByAuditCodeAndEventType(auditCodeEntity, eventType)
-                .orElseThrow(() -> new AuditDataNotFoundException(
-                        String.format("Audit event with code [%s], type [%s] not found", auditCode, eventType)));
-        return auditMapper.map(auditCodeTemplate);
+        return eventTemplatesCache.getUnchecked(new CacheKey(auditCode, eventType));
+    }
+
+    private CacheLoader<CacheKey, AuditEventTemplateModel> createCacheLoader() {
+        return new CacheLoader<>() {
+            @Override
+            public AuditEventTemplateModel load(CacheKey key) {
+                var auditCodeEntity = getAuditCode(key.getAuditCode());
+                var auditCodeTemplate = auditEventTemplateRepository
+                        .findByAuditCodeAndEventType(auditCodeEntity, key.getEventType())
+                        .orElseThrow(() -> new AuditDataNotFoundException(String.format("Audit event with code [%s], type [%s] not found", key.getAuditCode(), key.getEventType())));
+                log.debug("Fetched audit event template for key {}", key);
+                return auditMapper.map(auditCodeTemplate);
+            }
+        };
     }
 
     private AuditCodeEntity getAuditCode(String code) {
