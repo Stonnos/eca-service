5f8ce3975a214ad3cb02eaa6bc685ff003c0f863
Батыгин Роман Игоревич <batygin@self.team>
2021-06-16 13:30:31 +0700

    #ES-117 - unit tests for getNextPage method

--- eca-audit-log/src/test/java/com/ecaservice/audit/TestHelperUtils.java
+++ eca-audit-log/src/test/java/com/ecaservice/audit/TestHelperUtils.java
@@ -28,13 +28,24 @@ public class TestHelperUtils {
      * @return audit event request
      */
     public static AuditEventRequest createAuditEventRequest() {
+        return createAuditEventRequest(GROUP, CODE);
+    }
+
+    /**
+     * Creates audit event request.
+     *
+     * @param groupCode - group code
+     * @param code      - audit code
+     * @return audit event request
+     */
+    public static AuditEventRequest createAuditEventRequest(String groupCode, String code) {
         var auditEventRequest = new AuditEventRequest();
         auditEventRequest.setEventId(UUID.randomUUID().toString());
         auditEventRequest.setEventType(EventType.SUCCESS);
         auditEventRequest.setInitiator(INITIATOR);
-        auditEventRequest.setCode(CODE);
+        auditEventRequest.setCode(code);
         auditEventRequest.setCodeTitle(CODE_TITLE);
-        auditEventRequest.setGroupCode(GROUP);
+        auditEventRequest.setGroupCode(groupCode);
         auditEventRequest.setGroupTitle(GROUP_TITLE);
         auditEventRequest.setMessage(MESSAGE);
         auditEventRequest.setEventDate(LocalDateTime.now());
--- eca-audit-log/src/test/java/com/ecaservice/audit/service/AuditLogServiceTest.java
+++ eca-audit-log/src/test/java/com/ecaservice/audit/service/AuditLogServiceTest.java
@@ -2,31 +2,50 @@ package com.ecaservice.audit.service;
 
 import com.ecaservice.audit.AbstractJpaTest;
 import com.ecaservice.audit.config.EcaAuditLogConfig;
+import com.ecaservice.audit.entity.AuditLogEntity;
 import com.ecaservice.audit.mapping.AuditLogMapperImpl;
 import com.ecaservice.audit.repository.AuditLogRepository;
-import com.ecaservice.core.filter.annotation.EnableFilters;
+import com.ecaservice.core.filter.service.FilterService;
+import com.ecaservice.web.dto.model.FilterRequestDto;
+import com.ecaservice.web.dto.model.MatchMode;
+import com.ecaservice.web.dto.model.PageRequestDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.ecaservice.audit.TestHelperUtils.createAuditEventRequest;
+import static com.ecaservice.audit.dictionary.FilterDictionaries.AUDIT_LOG_TEMPLATE;
+import static com.ecaservice.audit.entity.AuditLogEntity_.CODE;
+import static com.ecaservice.audit.entity.AuditLogEntity_.EVENT_DATE;
+import static com.ecaservice.audit.entity.AuditLogEntity_.EVENT_ID;
+import static com.ecaservice.audit.entity.AuditLogEntity_.GROUP_CODE;
+import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for {@link AuditLogService} class.
  *
  * @author Roman Batygin
  */
-@EnableFilters
 @Import({AuditLogMapperImpl.class, AuditLogService.class, EcaAuditLogConfig.class})
 class AuditLogServiceTest extends AbstractJpaTest {
 
-    @Inject
-    private AuditLogService auditLogService;
+    private static final int PAGE_NUMBER = 0;
+    private static final int PAGE_SIZE = 10;
 
     @Inject
+    private AuditLogService auditLogService;
+    @Inject
     private AuditLogRepository auditLogRepository;
+
+    @MockBean
+    private FilterService filterService;
 
     @Override
     public void deleteAll() {
@@ -41,5 +60,43 @@ class AuditLogServiceTest extends AbstractJpaTest {
         var actual = auditLogRepository.findById(auditLog.getId()).orElse(null);
         assertThat(actual).isNotNull();
         assertThat(actual.getEventId()).isEqualTo(auditEventRequest.getEventId());
+    }
+
+    /**
+     * Tests global filtering by search query.
+     */
+    @Test
+    void testGlobalFilter() {
+        saveAuditLogs();
+        PageRequestDto pageRequestDto =
+                new PageRequestDto(PAGE_NUMBER, PAGE_SIZE, EVENT_DATE, false, "XGroup3", newArrayList());
+        when(filterService.getGlobalFilterFields(AUDIT_LOG_TEMPLATE)).thenReturn(
+                Arrays.asList(EVENT_ID, GROUP_CODE, CODE));
+        Page<AuditLogEntity> auditLogsPage = auditLogService.getNextPage(pageRequestDto);
+        assertThat(auditLogsPage).isNotNull();
+        assertThat(auditLogsPage.getTotalElements()).isOne();
+    }
+
+    /**
+     * Tests filtering by audit code.
+     */
+    @Test
+    void testFilterByAuditCode() {
+        saveAuditLogs();
+        PageRequestDto pageRequestDto =
+                new PageRequestDto(PAGE_NUMBER, PAGE_SIZE, EVENT_DATE, false, null, newArrayList());
+        when(filterService.getGlobalFilterFields(AUDIT_LOG_TEMPLATE)).thenReturn(Collections.emptyList());
+        pageRequestDto.getFilters().add(
+                new FilterRequestDto(CODE, Collections.singletonList("Code2"), MatchMode.EQUALS));
+        Page<AuditLogEntity> auditLogsPage = auditLogService.getNextPage(pageRequestDto);
+        assertThat(auditLogsPage).isNotNull();
+        assertThat(auditLogsPage.getTotalElements()).isOne();
+    }
+
+    private void saveAuditLogs() {
+        var first = createAuditEventRequest("Group1", "Code1");
+        var second = createAuditEventRequest("Group2", "Code2");
+        var third = createAuditEventRequest("XGroup3", "Code3");
+        Arrays.asList(first, second, third).forEach(auditLogService::save);
     }
 }
