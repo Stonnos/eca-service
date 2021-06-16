3f98662db73ba2f062ef658b841dce960d281909
Батыгин Роман Игоревич <batygin@self.team>
2021-06-16 17:14:54 +0700

    #ES-117 - added entity scan package

--- eca-audit-log/src/main/java/com/ecaservice/audit/config/EcaAuditLogConfiguration.java
+++ eca-audit-log/src/main/java/com/ecaservice/audit/config/EcaAuditLogConfiguration.java
@@ -1,10 +1,14 @@
 package com.ecaservice.audit.config;
 
+import com.ecaservice.audit.entity.AuditLogEntity;
+import com.ecaservice.audit.repository.AuditLogRepository;
 import com.ecaservice.common.web.annotation.EnableGlobalExceptionHandler;
 import com.ecaservice.core.filter.annotation.EnableFilters;
 import com.ecaservice.oauth2.annotation.Oauth2ResourceServer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Eca audit log configuration.
@@ -15,6 +19,8 @@ import org.springframework.context.annotation.Configuration;
 @Oauth2ResourceServer
 @EnableGlobalExceptionHandler
 @EnableFilters
+@EntityScan(basePackageClasses = AuditLogEntity.class)
+@EnableJpaRepositories(basePackageClasses = AuditLogRepository.class)
 @EnableConfigurationProperties(EcaAuditLogConfig.class)
 public class EcaAuditLogConfiguration {
 }
