aeb29c3fe9c0d2712608d5d66909a54a56bd1b57
Батыгин Роман Игоревич <batygin@self.team>
2021-06-18 17:14:35 +0700

    #ES-122 - added AuditLogBean

---
+++ eca-report/src/main/java/com/ecaservice/report/model/AuditLogBean.java
@@ -0,0 +1,52 @@
+package com.ecaservice.report.model;
+
+import lombok.Data;
+
+/**
+ * Audit log bean.
+ *
+ * @author Roman Batygin
+ */
+@Data
+public class AuditLogBean {
+
+    /**
+     * Audit event id.
+     */
+    private String eventId;
+
+    /**
+     * Audit message
+     */
+    private String message;
+
+    /**
+     * Event initiator
+     */
+    private String initiator;
+
+    /**
+     * Audit group
+     */
+    private String groupCode;
+
+    /**
+     * Audit group title
+     */
+    private String groupTitle;
+
+    /**
+     * Audit code
+     */
+    private String code;
+
+    /**
+     * Audit code title
+     */
+    private String codeTitle;
+
+    /**
+     * Event date
+     */
+    private String eventDate;
+}
