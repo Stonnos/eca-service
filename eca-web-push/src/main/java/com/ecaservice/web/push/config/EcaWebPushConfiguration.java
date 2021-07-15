abd1809551a37e88a709c766d14988d7646f3a27
Батыгин Роман Игоревич <batygin@self.team>
2021-07-15 17:23:27 +0700

    #ES-125 - configure open api for eca-web-push module

--- eca-web-push/src/main/java/com/ecaservice/web/push/config/EcaWebPushConfiguration.java
+++ eca-web-push/src/main/java/com/ecaservice/web/push/config/EcaWebPushConfiguration.java
@@ -1,6 +1,7 @@
 package com.ecaservice.web.push.config;
 
 import com.ecaservice.common.web.annotation.EnableGlobalExceptionHandler;
+import com.ecaservice.config.swagger.annotation.EnableOpenApi;
 import com.ecaservice.oauth2.annotation.Oauth2ResourceServer;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Roman Batygin
  */
 @Configuration
+@EnableOpenApi
 @Oauth2ResourceServer
 @EnableGlobalExceptionHandler
 public class EcaWebPushConfiguration {
