2a2b9195c0e51f674c41e68adf132fc64219da99
Батыгин Роман Игоревич <batygin@self.team>
2021-07-28 17:25:57 +0700

    #ES-127 - impl EmailMessageProcessor

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/service/EmailMessageProcessor.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/service/EmailMessageProcessor.java
@@ -5,9 +5,13 @@ import com.ecaservice.auto.test.model.EmailMessage;
 import com.ecaservice.auto.test.model.EmailTypeVisitor;
 import com.ecaservice.auto.test.repository.ExperimentRequestRepository;
 import com.ecaservice.common.web.exception.EntityNotFoundException;
+import com.ecaservice.test.common.model.ExecutionStatus;
+import com.ecaservice.test.common.model.TestResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * Email message processor service.
@@ -18,6 +22,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailMessageProcessor {
+
+    private static final String EXPERIMENT_FINISHED_WITH_TIMEOUT = "Experiment finished with timeout";
+    private static final String EXPERIMENT_FINISHED_WITH_ERROR = "Experiment finished with error";
 
     private final ExperimentRequestRepository experimentRequestRepository;
 
@@ -34,29 +41,41 @@ public class EmailMessageProcessor {
         emailMessage.getEmailType().handle(new EmailTypeVisitor() {
             @Override
             public void visitNewExperiment() {
-
+                experimentRequestEntity.setNewStatusEmailReceived(true);
             }
 
             @Override
             public void visitInProgressExperiment() {
-
+                experimentRequestEntity.setInProgressStatusEmailReceived(true);
             }
 
             @Override
             public void visitFinishedExperiment() {
-
+                experimentRequestEntity.setFinishedStatusEmailReceived(true);
+                experimentRequestEntity.setDownloadUrl(emailMessage.getDownloadUrl());
             }
 
             @Override
             public void visitErrorExperiment() {
-
+                experimentRequestEntity.setErrorStatusEmailReceived(true);
+                experimentRequestEntity.setDetails(EXPERIMENT_FINISHED_WITH_ERROR);
+                finishWithError();
             }
 
             @Override
             public void visitTimeoutExperiment() {
+                experimentRequestEntity.setTimeoutStatusEmailReceived(true);
+                experimentRequestEntity.setDetails(EXPERIMENT_FINISHED_WITH_TIMEOUT);
+                finishWithError();
+            }
 
+            void finishWithError() {
+                experimentRequestEntity.setExecutionStatus(ExecutionStatus.ERROR);
+                experimentRequestEntity.setTestResult(TestResult.ERROR);
+                experimentRequestEntity.setFinished(LocalDateTime.now());
             }
         });
+        experimentRequestRepository.save(experimentRequestEntity);
         log.info("Email message has been processed for experiment [{}]", emailMessage.getRequestId());
     }
 }
