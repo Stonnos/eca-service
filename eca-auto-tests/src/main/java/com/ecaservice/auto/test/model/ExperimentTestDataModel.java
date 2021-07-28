482fdc0a9b48c4f3a2d90097914927223b944cbd
Батыгин Роман Игоревич <batygin@self.team>
2021-07-28 11:22:43 +0700

    #ES-127 - added ExperimentTestDataModel

---
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/model/ExperimentTestDataModel.java
@@ -0,0 +1,29 @@
+package com.ecaservice.auto.test.model;
+
+import com.ecaservice.base.model.ExperimentType;
+import eca.core.evaluation.EvaluationMethod;
+import lombok.Data;
+
+/**
+ * Experiment test data model.
+ *
+ * @author Roman Batygin
+ */
+@Data
+public class ExperimentTestDataModel {
+
+    /**
+     * Experiment type
+     */
+    private ExperimentType experimentType;
+
+    /**
+     * Train data path in resources directory
+     */
+    private String trainDataPath;
+
+    /**
+     * Evaluation method
+     */
+    private EvaluationMethod evaluationMethod;
+}
