c606f6628d0d32168214ca3a5bf898146a75b387
Батыгин Роман Игоревич <batygin@self.team>
2021-07-28 14:00:16 +0700

    #ES-127 - refactored AbstractTestDataLoader

--- eca-tests-common/src/main/java/com/ecaservice/test/common/service/AbstractTestDataLoader.java
+++ eca-tests-common/src/main/java/com/ecaservice/test/common/service/AbstractTestDataLoader.java
@@ -45,12 +45,12 @@ public abstract class AbstractTestDataLoader<T> implements ConfigService<T> {
     public void readTestData() throws IOException {
         log.info("Starting to read test data [{}] from configs", testDataClass.getSimpleName());
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource[] classifiersOptionsFiles = resolver.getResources(getTestDataPath());
-        if (classifiersOptionsFiles.length == 0) {
+        Resource[] resources = resolver.getResources(getTestDataPath());
+        if (resources.length == 0) {
             log.error(TEST_DATA_DIRECTORY_IS_EMPTY);
             throw new ConfigException(TEST_DATA_DIRECTORY_IS_EMPTY);
         } else {
-            for (Resource resource : classifiersOptionsFiles) {
+            for (Resource resource : resources) {
                 try {
                     @Cleanup InputStream inputStream = resource.getInputStream();
                     var testDataModel = OBJECT_MAPPER.readValue(inputStream, testDataClass);
