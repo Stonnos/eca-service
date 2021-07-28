6dd6c0fcdbf7086f777759c20b2029d622ea74b1
Батыгин Роман Игоревич <batygin@self.team>
2021-07-28 13:27:07 +0700

    #ES-127 - impl AutoTestService

---
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/service/AutoTestService.java
@@ -0,0 +1,37 @@
+package com.ecaservice.auto.test.service;
+
+import com.ecaservice.auto.test.entity.AutoTestsJobEntity;
+import com.ecaservice.auto.test.repository.AutoTestsJobRepository;
+import com.ecaservice.test.common.model.ExecutionStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Auto test service.
+ *
+ * @author Roman Batygin
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AutoTestService {
+
+    private final AutoTestsJobRepository autoTestsJobRepository;
+
+    /**
+     * Creates experiment auto tests job.
+     *
+     * @return auto tests job entity
+     */
+    public AutoTestsJobEntity createExperimentsAutoTestJob() {
+        var autoTestsJobEntity = new AutoTestsJobEntity();
+        autoTestsJobEntity.setJobUuid(UUID.randomUUID().toString());
+        autoTestsJobEntity.setExecutionStatus(ExecutionStatus.NEW);
+        autoTestsJobEntity.setCreated(LocalDateTime.now());
+        return autoTestsJobRepository.save(autoTestsJobEntity);
+    }
+}
