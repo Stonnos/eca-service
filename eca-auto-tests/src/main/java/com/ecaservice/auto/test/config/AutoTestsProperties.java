e84756df97d53f1e1c108c7a4708ead4db089660
Батыгин Роман Игоревич <batygin@self.team>
2021-07-29 16:44:57 +0700

    #ES-127 - added custom ThreadPoolTaskScheduler

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/config/AutoTestsProperties.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/config/AutoTestsProperties.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -16,6 +18,9 @@ import javax.validation.constraints.NotNull;
 @Data
 @ConfigurationProperties("auto-tests")
 public class AutoTestsProperties {
+
+    private static final int MIN_POOL_SIZE = 1;
+    private static final int MAX_POOL_SIZE = 10;
 
     /**
      * Experiments test data path
@@ -34,6 +39,14 @@ public class AutoTestsProperties {
      */
     @NotNull
     private Integer delaySeconds;
+
+    /**
+     * Scheduler pool size
+     */
+    @NotNull
+    @Min(MIN_POOL_SIZE)
+    @Max(MAX_POOL_SIZE)
+    private Integer schedulerPoolSize;
 
     /**
      * Request processing timeout in seconds
--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/config/EcaAutoTestsConfiguration.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/config/EcaAutoTestsConfiguration.java
@@ -1,10 +1,15 @@
 package com.ecaservice.auto.test.config;
 
 import com.ecaservice.config.swagger.annotation.EnableOpenApi;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 /**
  * Auto test configuration class.
@@ -16,5 +21,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableConfigurationProperties(AutoTestsProperties.class)
-public class EcaAutoTestsConfiguration {
+@RequiredArgsConstructor
+public class EcaAutoTestsConfiguration implements SchedulingConfigurer {
+
+    private final AutoTestsProperties autoTestsProperties;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(threadPoolTaskScheduler());
+    }
+
+    /**
+     * Creates thread pool task scheduler bean.
+     *
+     * @return thread pool task scheduler bean
+     */
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(autoTestsProperties.getSchedulerPoolSize());
+        taskScheduler.initialize();
+        return taskScheduler;
+    }
 }
