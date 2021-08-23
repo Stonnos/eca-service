d17e18584cacfc67a38060bdfcbaffb31be4137a
Батыгин Роман Игоревич <batygin@self.team>
2021-08-23 15:19:16 +0700

    #ES-127 - added notnull constraints in EcaLoadTestsConfig

--- eca-load-tests/src/main/java/com/ecaservice/load/test/config/EcaLoadTestsConfig.java
+++ eca-load-tests/src/main/java/com/ecaservice/load/test/config/EcaLoadTestsConfig.java
@@ -4,8 +4,19 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import static com.ecaservice.load.test.dto.FieldConstraints.MAX_NUM_FOLDS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MAX_NUM_REQUESTS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MAX_NUM_TESTS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MAX_NUM_THREADS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MIN_NUM_FOLDS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MIN_NUM_REQUESTS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MIN_NUM_TESTS;
+import static com.ecaservice.load.test.dto.FieldConstraints.MIN_NUM_THREADS;
 
 /**
  * Eca load tests config model.
@@ -20,11 +31,17 @@ public class EcaLoadTestsConfig {
     /**
      * Requests number to eca - server
      */
+    @NotNull
+    @Min(MIN_NUM_REQUESTS)
+    @Max(MAX_NUM_REQUESTS)
     private Integer numRequests;
 
     /**
      * Threads number for requests sending
      */
+    @NotNull
+    @Min(MIN_NUM_THREADS)
+    @Max(MAX_NUM_THREADS)
     private Integer numThreads;
 
     /**
@@ -36,21 +53,29 @@ public class EcaLoadTestsConfig {
     /**
      * Request processing timeout in seconds
      */
+    @NotNull
     private Long requestTimeoutInSeconds;
 
     /**
      * Folds number for k * V cross - validation method
      */
+    @NotNull
+    @Min(MIN_NUM_FOLDS)
+    @Max(MAX_NUM_FOLDS)
     private Integer numFolds;
 
     /**
      * Tests number for k * V cross - validation method
      */
+    @NotNull
+    @Min(MIN_NUM_TESTS)
+    @Max(MAX_NUM_TESTS)
     private Integer numTests;
 
     /**
      * Seed value for random generator
      */
+    @NotNull
     private Integer seed;
 
     /**
@@ -68,10 +93,12 @@ public class EcaLoadTestsConfig {
     /**
      * Page size for paging processing
      */
+    @NotNull
     private Integer pageSize;
 
     /**
      * Delay in sec. for scheduler
      */
+    @NotNull
     private Integer delaySeconds;
 }
