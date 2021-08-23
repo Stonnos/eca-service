3de00a3424cd10fc7009a26fd21bd1dfc5711ab0
Батыгин Роман Игоревич <batygin@self.team>
2021-08-23 13:02:12 +0700

    #ES-127 - removed unused code in FilterUtils

--- core-filter/src/main/java/com/ecaservice/core/filter/util/FilterUtils.java
+++ core-filter/src/main/java/com/ecaservice/core/filter/util/FilterUtils.java
@@ -4,12 +4,6 @@ import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Sort;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.text.MessageFormat;
-
 /**
  * Sort utility class.
  *
@@ -17,8 +11,6 @@ import java.text.MessageFormat;
  */
 @UtilityClass
 public class FilterUtils {
-
-    private static final String LIKE_FORMAT = "%{0}%";
 
     /**
      * Builds sort object.
@@ -32,21 +24,5 @@ public class FilterUtils {
         String sortField = !StringUtils.isBlank(field) ? field : defaultField;
         Sort sort = Sort.by(sortField);
         return ascending ? sort.ascending() : sort.descending();
-    }
-
-    /**
-     * Builds like predicate.
-     *
-     * @param root            - root object
-     * @param criteriaBuilder - criteria builder object
-     * @param field           - field name
-     * @param value           - field value
-     * @param <T>             - entity type
-     * @return like predicate
-     */
-    public static <T> Predicate buildLikePredicate(Root<T> root, CriteriaBuilder criteriaBuilder, String field,
-                                                   String value) {
-        Expression<String> expression = root.get(field);
-        return criteriaBuilder.like(criteriaBuilder.lower(expression), MessageFormat.format(LIKE_FORMAT, value));
     }
 }
