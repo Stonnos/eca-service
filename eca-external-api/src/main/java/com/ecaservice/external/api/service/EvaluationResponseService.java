05846d2344a82344909cc3937fe5ac8deaff2d46
Батыгин Роман Игоревич <batygin@self.team>
2021-08-30 17:07:24 +0700

    #ES-133 - npe fix in EvaluationResponseService

--- eca-external-api/src/main/java/com/ecaservice/external/api/service/EvaluationResponseService.java
+++ eca-external-api/src/main/java/com/ecaservice/external/api/service/EvaluationResponseService.java
@@ -4,13 +4,9 @@ import com.ecaservice.external.api.config.ExternalApiConfig;
 import com.ecaservice.external.api.dto.EvaluationResponseDto;
 import com.ecaservice.external.api.dto.EvaluationStatus;
 import com.ecaservice.external.api.mapping.EvaluationStatusMapper;
-import eca.core.evaluation.Evaluation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
 
 /**
  * Class for response building.
@@ -40,13 +36,12 @@ public class EvaluationResponseService {
                 .requestId(ecaRequestEntity.getCorrelationId());
         var evaluationStatus = evaluationStatusMapper.map(ecaRequestEntity.getRequestStage());
         if (EvaluationStatus.FINISHED.equals(evaluationStatus)) {
-            Evaluation evaluation = null;
-            evaluationResponseDtoBuilder.numTestInstances(BigInteger.valueOf((long) evaluation.numInstances()))
-                    .numCorrect(BigInteger.valueOf((long) evaluation.correct()))
-                    .numIncorrect(BigInteger.valueOf((long) evaluation.incorrect()))
-                    .pctCorrect(BigDecimal.valueOf(evaluation.pctCorrect()))
-                    .pctIncorrect(BigDecimal.valueOf(evaluation.pctIncorrect()))
-                    .meanAbsoluteError(BigDecimal.valueOf(evaluation.meanAbsoluteError()))
+            evaluationResponseDtoBuilder.numTestInstances(ecaRequestEntity.getNumTestInstances())
+                    .numCorrect(ecaRequestEntity.getNumCorrect())
+                    .numIncorrect(ecaRequestEntity.getNumIncorrect())
+                    .pctCorrect(ecaRequestEntity.getPctCorrect())
+                    .pctIncorrect(ecaRequestEntity.getPctIncorrect())
+                    .meanAbsoluteError(ecaRequestEntity.getMeanAbsoluteError())
                     .modelUrl(String.format(MODEL_DOWNLOAD_URL_FORMAT, externalApiConfig.getDownloadBaseUrl(),
                             ecaRequestEntity.getCorrelationId()));
         }
