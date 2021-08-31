669197575182b951cc453bf20d8f292b6e94e55f
Батыгин Роман Игоревич <batygin@self.team>
2021-08-31 13:13:13 +0700

    #ES-133 - unit tests for EcaRequestService#deleteClassifierModel

--- eca-external-api/src/test/java/com/ecaservice/external/api/service/EcaRequestServiceTest.java
+++ eca-external-api/src/test/java/com/ecaservice/external/api/service/EcaRequestServiceTest.java
@@ -5,15 +5,21 @@ import com.ecaservice.external.api.dto.EvaluationRequestDto;
 import com.ecaservice.external.api.entity.EcaRequestEntity;
 import com.ecaservice.external.api.entity.EvaluationRequestEntity;
 import com.ecaservice.external.api.entity.RequestStageType;
+import com.ecaservice.external.api.exception.ProcessFileException;
 import com.ecaservice.external.api.mapping.EcaRequestMapperImpl;
 import com.ecaservice.external.api.repository.EvaluationRequestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 import javax.inject.Inject;
+import java.util.UUID;
 
 import static com.ecaservice.external.api.TestHelperUtils.createEvaluationRequestDto;
+import static com.ecaservice.external.api.TestHelperUtils.createEvaluationRequestEntity;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 
 /**
  * Unit tests for {@link EcaRequestService} class.
@@ -22,6 +28,11 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @Import({EcaRequestService.class, EcaRequestMapperImpl.class})
 class EcaRequestServiceTest extends AbstractJpaTest {
+
+    private static final String CLASSIFIER_ABSOLUTE_PATH = "/home/roman/classifier.model";
+
+    @MockBean
+    private FileDataService fileDataService;
 
     @Inject
     private EvaluationRequestRepository evaluationRequestRepository;
@@ -39,5 +50,31 @@ class EcaRequestServiceTest extends AbstractJpaTest {
         assertThat(actual.getRequestStage()).isEqualTo(RequestStageType.READY);
         assertThat(actual.getCreationDate()).isNotNull();
         assertThat(actual.getCorrelationId()).isNotNull();
+    }
+
+    @Test
+    void testDeleteClassifierModelSuccess() {
+        var evaluationRequestEntity = createEvaluationRequestEntity(UUID.randomUUID().toString());
+        evaluationRequestEntity.setClassifierAbsolutePath(CLASSIFIER_ABSOLUTE_PATH);
+        evaluationRequestRepository.save(evaluationRequestEntity);
+        ecaRequestService.deleteClassifierModel(evaluationRequestEntity);
+        var actual = evaluationRequestRepository.findById(evaluationRequestEntity.getId()).orElse(null);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getClassifierAbsolutePath()).isNull();
+        assertThat(actual.getDeletedDate()).isNotNull();
+    }
+
+    @Test
+    void testDeleteClassifierModelWithError() {
+        var evaluationRequestEntity = createEvaluationRequestEntity(UUID.randomUUID().toString());
+        evaluationRequestEntity.setClassifierAbsolutePath(CLASSIFIER_ABSOLUTE_PATH);
+        evaluationRequestRepository.save(evaluationRequestEntity);
+        doThrow(ProcessFileException.class).when(fileDataService).delete(CLASSIFIER_ABSOLUTE_PATH);
+        assertThrows(ProcessFileException.class,
+                () -> ecaRequestService.deleteClassifierModel(evaluationRequestEntity));
+        var actual = evaluationRequestRepository.findById(evaluationRequestEntity.getId()).orElse(null);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getClassifierAbsolutePath()).isNotNull();
+        assertThat(actual.getDeletedDate()).isNull();
     }
 }
