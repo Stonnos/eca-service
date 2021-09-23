7ad1f0245999740bf239d4f33fcd62415345d2dd
Батыгин Роман Игоревич <batygin@self.team>
2021-09-23 20:04:36 +0700

    #ES-144 - refactoring unit tests for InstancesReportService

--- eca-data-storage/src/test/java/com/ecaservice/data/storage/report/InstancesReportServiceTest.java
+++ eca-data-storage/src/test/java/com/ecaservice/data/storage/report/InstancesReportServiceTest.java
@@ -3,21 +3,23 @@ package com.ecaservice.data.storage.report;
 import com.ecaservice.data.storage.config.EcaDsConfig;
 import com.ecaservice.data.storage.model.report.ReportType;
 import com.ecaservice.data.storage.service.StorageService;
-import eca.data.file.xls.XLSSaver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import weka.core.Instances;
+
+import javax.inject.Inject;
 
 import static com.ecaservice.data.storage.TestHelperUtils.loadInstances;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,20 +27,19 @@ import static org.mockito.Mockito.when;
  *
  * @author Roman Batygin
  */
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@EnableConfigurationProperties
+@TestPropertySource("classpath:application.properties")
+@Import({InstancesReportService.class, ReportProvider.class, EcaDsConfig.class})
 class InstancesReportServiceTest {
 
     private static final long ID = 1L;
     private static final String ATTACHMENT_FORMAT = "attachment; filename=%s";
 
-    @Mock
+    @MockBean
     private StorageService storageService;
-    @Mock
-    private ReportProvider reportProvider;
-    @Mock
-    private EcaDsConfig ecaDsConfig;
 
-    @InjectMocks
+    @Inject
     private InstancesReportService instancesReportService;
 
     private Instances instances;
@@ -50,15 +51,15 @@ class InstancesReportServiceTest {
 
     @Test
     void testGenerateReport() throws Exception {
-        var xlsSaver = mock(XLSSaver.class);
-        var httpServletResponse = new MockHttpServletResponse();
-        when(storageService.getInstances(ID)).thenReturn(instances);
-        when(reportProvider.visitXls()).thenReturn(xlsSaver);
-        instancesReportService.generateInstancesReport(ID, ReportType.XLS, httpServletResponse);
-        assertThat(httpServletResponse.getContentType()).isEqualTo(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        String expectedFileName = String.format("%s.%s", instances.relationName(), ReportType.XLS.getExtension());
-        String expectedContentDisposition = String.format(ATTACHMENT_FORMAT, expectedFileName);
-        assertThat(httpServletResponse.getHeader(HttpHeaders.CONTENT_DISPOSITION)).isEqualTo(
-                expectedContentDisposition);
+        for (var reportType : ReportType.values()) {
+            var httpServletResponse = new MockHttpServletResponse();
+            when(storageService.getInstances(ID)).thenReturn(instances);
+            instancesReportService.generateInstancesReport(ID, reportType, httpServletResponse);
+            assertThat(httpServletResponse.getContentType()).isEqualTo(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            String expectedFileName = String.format("%s.%s", instances.relationName(), reportType.getExtension());
+            String expectedContentDisposition = String.format(ATTACHMENT_FORMAT, expectedFileName);
+            assertThat(httpServletResponse.getHeader(HttpHeaders.CONTENT_DISPOSITION)).isEqualTo(
+                    expectedContentDisposition);
+        }
     }
 }
