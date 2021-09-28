7af4be958d993ddbcc7caef1a0f4114cac87a7a6
Батыгин Роман Игоревич <batygin@self.team>
2021-09-28 17:31:30 +0700

    #ES-146 - removed unused code

--- eca-ers/src/main/java/com/ecaservice/ers/util/Utils.java
+++ eca-ers/src/main/java/com/ecaservice/ers/util/Utils.java
@@ -1,12 +1,8 @@
 package com.ecaservice.ers.util;
 
-import com.ecaservice.ers.dto.ClassifierOptionsResponse;
-import com.ecaservice.ers.dto.ClassifierReport;
-import com.ecaservice.ers.dto.EvaluationResultsResponse;
 import lombok.experimental.UtilityClass;
 
 import java.math.BigInteger;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,44 +12,6 @@ import java.util.Optional;
  */
 @UtilityClass
 public class Utils {
-
-    /**
-     * Creates evaluation results response.
-     *
-     * @param requestId      - request id
-     * @return evaluation results response
-     */
-    public static EvaluationResultsResponse buildResponse(String requestId) {
-        EvaluationResultsResponse evaluationResultsResponse = new EvaluationResultsResponse();
-        evaluationResultsResponse.setRequestId(requestId);
-        return evaluationResultsResponse;
-    }
-
-    /**
-     * Creates classifier options response.
-     *
-     * @param requestId      - request id
-     * @return classifier options response
-     */
-    public static ClassifierOptionsResponse buildClassifierOptionsResponse(String requestId) {
-        ClassifierOptionsResponse classifierOptionsResponse = new ClassifierOptionsResponse();
-        classifierOptionsResponse.setRequestId(requestId);
-        return classifierOptionsResponse;
-    }
-
-    /**
-     * Creates classifier options response.
-     *
-     * @param requestId         - request id
-     * @param classifierReports - classifier reports
-     * @return classifier options response
-     */
-    public static ClassifierOptionsResponse buildClassifierOptionsResponse(String requestId,
-                                                                           List<ClassifierReport> classifierReports) {
-        ClassifierOptionsResponse classifierOptionsResponse = buildClassifierOptionsResponse(requestId);
-        classifierOptionsResponse.setClassifierReports(classifierReports);
-        return classifierOptionsResponse;
-    }
 
     /**
      * Returns integer value if specified big integer value is not null, null otherwise.
