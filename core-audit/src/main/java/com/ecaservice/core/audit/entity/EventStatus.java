928b91a4772288443889c666fd1b03e5e9ebf869
Батыгин Роман Игоревич <batygin@self.team>
2021-10-01 12:05:52 +0700

    #ES-149 - added AuditEventRequestEntity

---
+++ core-audit/src/main/java/com/ecaservice/core/audit/entity/EventStatus.java
@@ -0,0 +1,19 @@
+package com.ecaservice.core.audit.entity;
+
+/**
+ * Event status enum.
+ *
+ * @author Roman Batygin
+ */
+public enum EventStatus {
+
+    /**
+     * Audit event successfully sent.
+     */
+    SENT,
+
+    /**
+     * Audit event not sent.
+     */
+    NOT_SENT
+}
