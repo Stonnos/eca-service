bdce90e484f8f5c2bdc934478f54aac7d8d8d818
Батыгин Роман Игоревич <batygin@self.team>
2021-10-01 12:49:35 +0700

    #ES-149 - unit tests for AuditMapper

--- core-audit/src/test/java/com/ecaservice/core/audit/TestHelperUtils.java
+++ core-audit/src/test/java/com/ecaservice/core/audit/TestHelperUtils.java
@@ -1,13 +1,18 @@
 package com.ecaservice.core.audit;
 
+import com.ecaservice.audit.dto.AuditEventRequest;
 import com.ecaservice.audit.dto.EventType;
 import com.ecaservice.core.audit.entity.AuditCodeEntity;
+import com.ecaservice.core.audit.entity.AuditEventRequestEntity;
 import com.ecaservice.core.audit.entity.AuditEventTemplateEntity;
 import com.ecaservice.core.audit.entity.AuditGroupEntity;
 import com.ecaservice.core.audit.model.AuditCodeModel;
 import com.ecaservice.core.audit.model.AuditEventTemplateModel;
 import com.ecaservice.core.audit.model.AuditGroupModel;
 import lombok.experimental.UtilityClass;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 /**
  * Test helper utility class.
@@ -23,6 +28,8 @@ public class TestHelperUtils {
     private static final String CODE = "code";
     private static final String MESSAGE_TEMPLATE = "Message";
     private static final long ID = 1L;
+    public static final String MESSAGE = "message";
+    public static final String INITIATOR = "initiator";
 
     /**
      * Creates audit group entity.
@@ -101,5 +108,43 @@ public class TestHelperUtils {
         auditGroupModel.setGroupCode(GROUP);
         auditGroupModel.setTitle(GROUP_TITLE);
         return auditGroupModel;
+    }
+
+    /**
+     * Creates audit event request dto.
+     *
+     * @return audit event request dto
+     */
+    public static AuditEventRequest createAuditEventRequest() {
+        var auditEventRequest = new AuditEventRequest();
+        auditEventRequest.setEventId(UUID.randomUUID().toString());
+        auditEventRequest.setMessage(MESSAGE);
+        auditEventRequest.setGroupCode(GROUP);
+        auditEventRequest.setGroupTitle(GROUP_TITLE);
+        auditEventRequest.setCode(CODE);
+        auditEventRequest.setGroupTitle(CODE_TITLE);
+        auditEventRequest.setEventType(EventType.SUCCESS);
+        auditEventRequest.setInitiator(INITIATOR);
+        auditEventRequest.setEventDate(LocalDateTime.now());
+        return auditEventRequest;
+    }
+
+    /**
+     * Creates audit event request entity.
+     *
+     * @return audit event request entity
+     */
+    public static AuditEventRequestEntity createAuditEventRequestEntity() {
+        var auditEventRequestEntity = new AuditEventRequestEntity();
+        auditEventRequestEntity.setEventId(UUID.randomUUID().toString());
+        auditEventRequestEntity.setMessage(MESSAGE);
+        auditEventRequestEntity.setGroupCode(GROUP);
+        auditEventRequestEntity.setGroupTitle(GROUP_TITLE);
+        auditEventRequestEntity.setCode(CODE);
+        auditEventRequestEntity.setGroupTitle(CODE_TITLE);
+        auditEventRequestEntity.setEventType(EventType.SUCCESS);
+        auditEventRequestEntity.setInitiator(INITIATOR);
+        auditEventRequestEntity.setEventDate(LocalDateTime.now());
+        return auditEventRequestEntity;
     }
 }
