377a40cd59b7e7732fc4753d5b8ccba1f4095a7f
Батыгин Роман Игоревич <batygin@self.team>
2021-10-05 12:38:18 +0700

    #ES-150 - added constraints

--- eca-oauth/src/main/java/com/ecaservice/oauth/config/AppProperties.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/config/AppProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -36,21 +37,25 @@ public class AppProperties {
     /**
      * Web application external base url
      */
+    @NotEmpty
     private String webExternalBaseUrl;
 
     /**
      * Reset password properties
      */
+    @NotNull
     private TokenValidityProperties resetPassword = new TokenValidityProperties();
 
     /**
      * Change password properties
      */
+    @NotNull
     private TokenValidityProperties changePassword = new TokenValidityProperties();
 
     /**
      * Change email properties
      */
+    @NotNull
     private TokenValidityProperties changeEmail = new TokenValidityProperties();
 
     /**
@@ -62,11 +67,13 @@ public class AppProperties {
         /**
          * Token validity in minutes
          */
+        @NotNull
         private Long validityMinutes;
 
         /**
          * Token url
          */
+        @NotEmpty
         private String url;
     }
 }
