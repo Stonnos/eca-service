16b17eedb167942028f4704c37791fa67ea5d22a
Батыгин Роман Игоревич <batygin@self.team>
2021-10-08 11:31:35 +0700

    #ES-152 - added examples in eca-data-storage

--- eca-web-transport/src/main/java/com/ecaservice/web/dto/model/InstancesReportInfoDto.java
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/model/InstancesReportInfoDto.java
@@ -15,18 +15,18 @@ public class InstancesReportInfoDto {
     /**
      * Report type
      */
-    @Schema(description = "Report type")
+    @Schema(description = "Report type", example = "XLS")
     private String reportType;
 
     /**
      * Report title
      */
-    @Schema(description = "Report title")
+    @Schema(description = "Report title", example = "Microsoft Excel (.xlsx)")
     private String title;
 
     /**
      * Report file extension
      */
-    @Schema(description = "Report file extension")
+    @Schema(description = "Report file extension", example = "xlsx")
     private String fileExtension;
 }
