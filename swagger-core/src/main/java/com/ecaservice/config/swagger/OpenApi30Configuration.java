ffe016fe4db4e8b5341aace3065a09a12b16ceaa
Батыгин Роман Игоревич <batygin@self.team>
2021-10-12 14:21:43 +0700

    #ES-152 - changed Bearer format

--- swagger-core/src/main/java/com/ecaservice/config/swagger/OpenApi30Configuration.java
+++ swagger-core/src/main/java/com/ecaservice/config/swagger/OpenApi30Configuration.java
@@ -33,7 +33,7 @@ public class OpenApi30Configuration {
     public static final String SCOPE_WEB = "web";
 
     private static final String ECA_AUTHENTICATION_SECURITY_DESCRIPTION = "Oauth2 authentication";
-    private static final String BEARER_ACCESS_TOKEN = "Bearer <access token>";
+    private static final String BEARER_ACCESS_TOKEN = "Authorization: Bearer <access token>";
 
     private static final String TOKEN_URL_FORMAT = "%s/oauth/token";
 
