345b95265c82c4f095cf1c7061e61c43ab68b64b
Батыгин Роман Игоревич <batygin@self.team>
2021-10-21 11:44:26 +0700

    #ES-156 - added feign-metrics module

---
+++ feign-metrics/src/main/java/com/ecaservice/feign/metrics/config/FeignClientMetricsAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.ecaservice.feign.metrics.config;
+
+import feign.Feign;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feign client metric auto configuration.
+ *
+ * @author Roman Batygin
+ */
+@Configuration
+public class FeignClientMetricsAutoConfiguration {
+
+    /**
+     * Adds micrometer capability for feign clients.
+     *
+     * @param meterRegistry - meter registry
+     * @return feign builder bean
+     */
+    @Bean
+    public Feign.Builder feignBuilder(MeterRegistry meterRegistry) {
+        return Feign.builder()
+                .addCapability(new MicrometerCapability(meterRegistry));
+    }
+}
