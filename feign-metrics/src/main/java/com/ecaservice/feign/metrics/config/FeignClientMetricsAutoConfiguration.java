065a48b681f6218efa720917a90b0a8726fde770
Батыгин Роман Игоревич <batygin@self.team>
2021-10-21 14:44:47 +0700

    #ES-156 - fix bean scope

--- feign-metrics/src/main/java/com/ecaservice/feign/metrics/config/FeignClientMetricsAutoConfiguration.java
+++ feign-metrics/src/main/java/com/ecaservice/feign/metrics/config/FeignClientMetricsAutoConfiguration.java
@@ -5,6 +5,7 @@ import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Feign client metric auto configuration.
@@ -21,6 +22,7 @@ public class FeignClientMetricsAutoConfiguration {
      * @return feign builder bean
      */
     @Bean
+    @Scope("prototype")
     public Feign.Builder feignBuilder(MeterRegistry meterRegistry) {
         return Feign.builder()
                 .addCapability(new MicrometerCapability(meterRegistry));
