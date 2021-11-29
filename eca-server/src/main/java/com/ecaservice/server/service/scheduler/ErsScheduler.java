cb8fed4c77e852fdcc1c3158bc4b9fb25ddeb75c
Батыгин Роман Игоревич <batygin@self.team>
2021-11-29 14:08:57 +0700

    #ES-176 - impl ErsRedeliveryService

--- eca-server/src/main/java/com/ecaservice/server/service/scheduler/ErsScheduler.java
+++ eca-server/src/main/java/com/ecaservice/server/service/scheduler/ErsScheduler.java
@@ -1,24 +1,11 @@
 package com.ecaservice.server.service.scheduler;
 
-import com.ecaservice.ers.dto.EvaluationResultsRequest;
-import com.ecaservice.server.config.ers.ErsConfig;
-import com.ecaservice.server.model.entity.ErsResponseStatus;
-import com.ecaservice.server.model.entity.ErsRetryRequest;
-import com.ecaservice.server.repository.ErsRetryRequestRepository;
-import com.ecaservice.server.service.ers.ErsErrorHandler;
-import com.ecaservice.server.service.ers.ErsRequestService;
-import com.ecaservice.server.service.ers.ErsRetryRequestCacheService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ecaservice.server.service.ers.ErsRedeliveryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
-
-import static com.ecaservice.server.util.PageHelper.processWithPagination;
 
 /**
  * Ers requests scheduler.
@@ -31,13 +18,7 @@ import static com.ecaservice.server.util.PageHelper.processWithPagination;
 @RequiredArgsConstructor
 public class ErsScheduler {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
-    private final ErsConfig ersConfig;
-    private final ErsRequestService ersRequestService;
-    private final ErsErrorHandler ersErrorHandler;
-    private final ErsRetryRequestCacheService ersRetryRequestCacheService;
-    private final ErsRetryRequestRepository ersRetryRequestRepository;
+    private final ErsRedeliveryService ersRedeliveryService;
 
     /**
      * Retry to send ers requests.
@@ -45,29 +26,7 @@ public class ErsScheduler {
     @Scheduled(fixedDelayString = "${ers.redeliveryIntervalSeconds}000")
     public void resendErsRequests() {
         log.info("Starting ers requests resending job");
-        var ids = ersRetryRequestRepository.getNotSentIds();
-        if (!CollectionUtils.isEmpty(ids)) {
-            log.info("Found [{}] not sent ers requests", ids.size());
-            processWithPagination(ids, ersRetryRequestRepository::findByIdIn, this::processPage,
-                    ersConfig.getPageSize());
-        }
+        ersRedeliveryService.resendErsRequests();
         log.info("Ers requests resending job has been finished");
-    }
-
-    private void processPage(List<ErsRetryRequest> ersRetryRequests) {
-        for (ErsRetryRequest ersRetryRequest : ersRetryRequests) {
-            try {
-                log.info("Resend ers request [{}]", ersRetryRequest.getErsRequest().getRequestId());
-                var evaluationResultsRequest = OBJECT_MAPPER.readValue(ersRetryRequest.getJsonRequest(),
-                        EvaluationResultsRequest.class);
-                ersRequestService.saveEvaluationResults(evaluationResultsRequest, ersRetryRequest.getErsRequest());
-            } catch (Exception ex) {
-                log.error("There was an error while resend ers request [{}]: {}",
-                        ersRetryRequest.getErsRequest().getRequestId(), ex.getMessage());
-                ersErrorHandler.handleErrorRequest(ersRetryRequest.getErsRequest(), ErsResponseStatus.ERROR,
-                        ex.getMessage());
-                ersRetryRequestCacheService.evict(ersRetryRequest.getErsRequest());
-            }
-        }
     }
 }
