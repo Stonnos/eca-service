69b5d771923023d7332891236b55d387bfaecead
Батыгин Роман Игоревич <batygin@self.team>
2022-02-02 12:14:59 +0700

    #ES-186 - updated ApiExamples

--- eca-audit-log/src/main/java/com/ecaservice/audit/controller/doc/ApiExamples.java
+++ eca-audit-log/src/main/java/com/ecaservice/audit/controller/doc/ApiExamples.java
@@ -14,11 +14,11 @@ public class ApiExamples {
      * Audit logs page request json
      */
     public static final String AUDIT_LOGS_PAGE_REQUEST_JSON =
-            "{\"content\": [{\"eventId\": \"1d2de514-3a87-4620-9b97-c260e24340de\", " +
-                    "\"correlationId\": \"202786\", \"message\": \"Some action\", " +
-                    "\"initiator\": \"user\", \"groupCode\": \"USER_ACTIONS\", \"groupTitle\": " +
-                    "\"User actions\", \"code\": \"LOGIN\", \"codeTitle\": \"User logged in\", " +
-                    "\"eventDate\": \"2021-07-01 14:00:00\"}], \"page\": 0, \"totalCount\": 1}";
+            "{\"page\":0,\"size\":25,\"sortField\":\"eventDate\",\"ascending\":false," +
+                    "\"searchQuery\":\"\",\"filters\":[{\"name\":\"groupCode\"," +
+                    "\"values\":[\"USER_PROFILE_ACTIONS\"],\"matchMode\":\"EQUALS\"}," +
+                    "{\"name\":\"initiator\",\"values\":[\"admin\"],\"matchMode\":\"LIKE\"}," +
+                    "{\"name\":\"eventDate\",\"values\":[\"2021-07-16\"],\"matchMode\":\"RANGE\"}]}";
 
     /**
      * Audit event request json
@@ -32,25 +32,30 @@ public class ApiExamples {
     /**
      * Audit filter response json
      */
-    public static final String AUDIT_LOG_FILTER_RESPONSE_JSON = "[{\"fieldName\": \"eventId\", \"description\": \"ID " +
-            "события\", \"fieldOrder\": 0, \"filterFieldType\": \"TEXT\", \"matchMode\": \"LIKE\", " +
-            "\"multiple\": false, \"dictionary\": null}, {\"fieldName\": \"groupCode\", \"description\": " +
-            "\"Группа событий\", \"fieldOrder\": 1, \"filterFieldType\": \"REFERENCE\", \"matchMode\": " +
-            "\"EQUALS\", \"multiple\": false, \"dictionary\": {\"name\": \"auditGroup\", \"values\": " +
-            "[{\"label\": \"Действия пользователя в личном кабинете\", \"value\": \"USER_PROFILE_ACTIONS\"}, " +
-            "{\"label\": \"Действия с конфигурациями классификаторов\", \"value\": " +
-            "\"CLASSIFIERS_CONFIGURATIONS_ACTIONS\"}, {\"label\": \"Действия с обучающими выборками\", " +
-            "\"value\": \"DATA_STORAGE_ACTIONS\"}]}}, {\"fieldName\": \"initiator\", \"description\": " +
-            "\"Инициатор события\", \"fieldOrder\": 2, \"filterFieldType\": \"TEXT\", \"matchMode\": " +
-            "\"LIKE\", \"multiple\": false, \"dictionary\": null}, {\"fieldName\": \"eventDate\", " +
-            "\"description\": \"Дата события\", \"fieldOrder\": 3, \"filterFieldType\": \"DATE\", " +
-            "\"matchMode\": \"RANGE\", \"multiple\": true, \"dictionary\": null}]";
+    public static final String AUDIT_LOG_FILTER_RESPONSE_JSON = "[{\"fieldName\": \"eventId\", " +
+            "\"description\": \"ID события\", \"fieldOrder\": 0, \"filterFieldType\": \"TEXT\", " +
+            "\"matchMode\": \"LIKE\", \"multiple\": false, \"dictionary\": null}, {\"fieldName\": " +
+            "\"correlationId\", \"description\": \"ID корреляции\", \"fieldOrder\": 1, " +
+            "\"filterFieldType\": \"TEXT\", \"matchMode\": \"LIKE\", \"multiple\": false, " +
+            "\"dictionary\": null}, {\"fieldName\": \"groupCode\", \"description\": " +
+            "\"Группа событий\", \"fieldOrder\": 2, \"filterFieldType\": \"REFERENCE\", " +
+            "\"matchMode\": \"EQUALS\", \"multiple\": false, \"dictionary\": {\"name\": " +
+            "\"auditGroup\", \"values\": [{\"label\": \"Действия пользователя в личном кабинете\", " +
+            "\"value\": \"USER_PROFILE_ACTIONS\"}, {\"label\": \"Действия с конфигурациями классификаторов\", " +
+            "\"value\": \"CLASSIFIERS_CONFIGURATIONS_ACTIONS\"}, {\"label\": " +
+            "\"Действия с обучающими выборками\", \"value\": \"DATA_STORAGE_ACTIONS\"}]}}, " +
+            "{\"fieldName\": \"initiator\", \"description\": \"Инициатор события\", \"fieldOrder\": 3, " +
+            "\"filterFieldType\": \"TEXT\", \"matchMode\": \"LIKE\", \"multiple\": false, " +
+            "\"dictionary\": null}, {\"fieldName\": \"eventDate\", \"description\": \"Дата события\", " +
+            "\"fieldOrder\": 4, \"filterFieldType\": \"DATE\", \"matchMode\": \"RANGE\", \"multiple\": true, " +
+            "\"dictionary\": null}]";
 
     /**
      * Audit logs page response json
      */
     public static final String AUDIT_LOGS_PAGE_RESPONSE_JSON = "{\"content\": [{\"eventId\": " +
-            "\"1d2de514-3a87-4620-9b97-c260e24340de\", \"message\": \"Some action\", \"initiator\": \"user\", " +
+            "\"1d2de514-3a87-4620-9b97-c260e24340de\", \"correlationId\": \"202786\", " +
+            "\"message\": \"Some action\", \"initiator\": \"user\", " +
             "\"groupCode\": \"USER_ACTIONS\", \"groupTitle\": \"User actions\", \"code\": \"LOGIN\", \"codeTitle\": " +
             "\"User logged in\", \"eventDate\": \"2021-07-01 14:00:00\"}], \"page\": 0, \"totalCount\": 1}";
 
