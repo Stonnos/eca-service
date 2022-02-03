b08d4c29c47912c21845a3e31f9f4fdc6e769eea
Батыгин Роман Игоревич <batygin@self.team>
2022-02-03 12:23:24 +0700

    #ES-186 - updated javadocs in Audit annotation

--- core-audit/src/main/java/com/ecaservice/core/audit/annotation/Audit.java
+++ core-audit/src/main/java/com/ecaservice/core/audit/annotation/Audit.java
@@ -34,9 +34,10 @@ public @interface Audit {
 
     /**
      * Spring Expression Language (SpEL) attribute for computing the correlation id key dynamically. Expression may
-     * contain method input parameters shortcuts or method returned value shortcuts.
+     * contain method input parameter shortcut or method returned value shortcut.
      * Method input parameters examples: {@code #id}, {@code #object.name} - for field in class object.
      * Method returned value examples: {@code #result}, {@code #result.value} - for field in class object.
+     * Note that method returned value shortcut should start with #result prefix.
      */
     String correlationIdKey() default "";
 }
