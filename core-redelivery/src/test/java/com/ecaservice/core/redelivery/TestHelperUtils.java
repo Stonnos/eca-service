e49c5c608d67a228195e12a64ad1d827f04ae22b
Батыгин Роман Игоревич <batygin@self.team>
2022-02-15 17:01:03 +0700

    #ES-189 - unit test for RequestRedeliveryService

--- core-redelivery/src/test/java/com/ecaservice/core/redelivery/TestHelperUtils.java
+++ core-redelivery/src/test/java/com/ecaservice/core/redelivery/TestHelperUtils.java
@@ -1,6 +1,9 @@
 package com.ecaservice.core.redelivery;
 
+import com.ecaservice.core.redelivery.entity.RetryRequest;
 import lombok.experimental.UtilityClass;
+
+import java.time.LocalDateTime;
 
 /**
  * Test helper utility class.
@@ -9,4 +12,21 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class TestHelperUtils {
+
+    private static final String REQUEST_TYPE = "REQUEST_TYPE";
+    private static final String REQUEST = "request";
+
+    /**
+     * Creates retry request.
+     *
+     * @return retry request
+     */
+    public static RetryRequest createRetryRequest() {
+        var retryRequest = new RetryRequest();
+        retryRequest.setRequestType(REQUEST_TYPE);
+        retryRequest.setRequest(REQUEST);
+        retryRequest.setMaxRetries(0);
+        retryRequest.setCreatedAt(LocalDateTime.now());
+        return retryRequest;
+    }
 }
