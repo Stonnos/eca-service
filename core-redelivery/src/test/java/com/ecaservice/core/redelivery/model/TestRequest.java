bf217aa4af5e497d5431b9ce66103811aa12b996
Батыгин Роман Игоревич <batygin@self.team>
2022-02-15 16:35:25 +0700

    #ES-189 - unit test for JsonRequestMessageConverter class

---
+++ core-redelivery/src/test/java/com/ecaservice/core/redelivery/model/TestRequest.java
@@ -0,0 +1,20 @@
+package com.ecaservice.core.redelivery.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Test request model.
+ *
+ * @author Roman Batygin
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestRequest {
+
+    private int x;
+
+    private int y;
+}
