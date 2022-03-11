fe00d0e4bd4923824b34ad4b3e04b08a18ec0c4d
Батыгин Роман Игоревич <batygin@self.team>
2022-03-11 13:29:32 +0700

    #ES-194 - populate classifier in request

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/service/runner/ClassifierAutoTestRunner.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/service/runner/ClassifierAutoTestRunner.java
@@ -8,12 +8,14 @@ import com.ecaservice.auto.test.service.AutoTestJobService;
 import com.ecaservice.auto.test.service.AutoTestWorkerService;
 import com.ecaservice.auto.test.service.ClassifierTestDataProvider;
 import com.ecaservice.base.model.EvaluationRequest;
+import com.ecaservice.classifier.options.adapter.ClassifierOptionsAdapter;
 import com.ecaservice.test.common.service.InstancesLoader;
 import eca.core.evaluation.EvaluationMethod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
+import weka.classifiers.AbstractClassifier;
 import weka.core.Instances;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class ClassifierAutoTestRunner extends AbstractAutoTestRunner<EvaluationR
 
     private final InstancesLoader instancesLoader;
     private final ClassifierTestDataProvider classifierTestDataProvider;
+    private final ClassifierOptionsAdapter classifierOptionsAdapter;
 
     private final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
@@ -41,16 +44,19 @@ public class ClassifierAutoTestRunner extends AbstractAutoTestRunner<EvaluationR
      * @param baseEvaluationRequestRepository - base evaluation request repository
      * @param instancesLoader                 - instances loader
      * @param classifierTestDataProvider      - classifier test data provider
+     * @param classifierOptionsAdapter        - classifier options adapter
      */
     public ClassifierAutoTestRunner(AutoTestJobService autoTestJobService,
                                     AutoTestWorkerService autoTestWorkerService,
                                     BaseEvaluationRequestRepository baseEvaluationRequestRepository,
                                     InstancesLoader instancesLoader,
-                                    ClassifierTestDataProvider classifierTestDataProvider) {
+                                    ClassifierTestDataProvider classifierTestDataProvider,
+                                    ClassifierOptionsAdapter classifierOptionsAdapter) {
         super(AutoTestType.EVALUATION_REQUEST_PROCESS, autoTestJobService, autoTestWorkerService,
                 baseEvaluationRequestRepository);
         this.instancesLoader = instancesLoader;
         this.classifierTestDataProvider = classifierTestDataProvider;
+        this.classifierOptionsAdapter = classifierOptionsAdapter;
     }
 
     @Override
@@ -73,6 +79,9 @@ public class ClassifierAutoTestRunner extends AbstractAutoTestRunner<EvaluationR
         Instances instances = instancesLoader.loadInstances(instancesResource);
         EvaluationRequest evaluationRequest = new EvaluationRequest();
         evaluationRequest.setData(instances);
+        AbstractClassifier classifier =
+                classifierOptionsAdapter.convert(classifierTestDataModel.getClassifierOptions());
+        evaluationRequest.setClassifier(classifier);
         evaluationRequest.setEvaluationMethod(classifierTestDataModel.getEvaluationMethod());
         if (EvaluationMethod.CROSS_VALIDATION.equals(classifierTestDataModel.getEvaluationMethod())) {
             evaluationRequest.setNumFolds(classifierTestDataModel.getNumFolds());
