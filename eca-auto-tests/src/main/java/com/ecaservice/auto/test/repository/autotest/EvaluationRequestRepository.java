4d10295807a39265bc0cc71daa7d46cfc3601832
Батыгин Роман Игоревич <batygin@self.team>
2022-03-14 18:35:51 +0700

    #ES-194 - added JpaBaseEvaluationRequestRepository

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/repository/autotest/EvaluationRequestRepository.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/repository/autotest/EvaluationRequestRepository.java
@@ -1,34 +1,11 @@
 package com.ecaservice.auto.test.repository.autotest;
 
-import com.ecaservice.auto.test.entity.autotest.AutoTestsJobEntity;
 import com.ecaservice.auto.test.entity.autotest.EvaluationRequestEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 /**
  * Repository to manage with {@link EvaluationRequestEntity} persistence entity.
  *
  * @author Roman Batygin
  */
-public interface EvaluationRequestRepository extends JpaRepository<EvaluationRequestEntity, Long> {
-
-    /**
-     * Finds evaluation request with correlation id.
-     *
-     * @param correlationId - correlation id
-     * @return evaluation request entity
-     */
-    Optional<EvaluationRequestEntity> findByCorrelationId(String correlationId);
-
-    /**
-     * Gets evaluation requests page for specified job.
-     *
-     * @param autoTestsJobEntity - auto tests job
-     * @param pageable           - pageable object
-     * @return evaluation requests page
-     */
-    Page<EvaluationRequestEntity> findAllByJob(AutoTestsJobEntity autoTestsJobEntity, Pageable pageable);
+public interface EvaluationRequestRepository extends JpaBaseEvaluationRequestRepository<EvaluationRequestEntity> {
 }
--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/repository/autotest/ExperimentRequestRepository.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/repository/autotest/ExperimentRequestRepository.java
@@ -1,47 +1,16 @@
 package com.ecaservice.auto.test.repository.autotest;
 
-import com.ecaservice.auto.test.entity.autotest.AutoTestsJobEntity;
 import com.ecaservice.auto.test.entity.autotest.ExperimentRequestEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Repository to manage with {@link ExperimentRequestEntity} persistence entity.
  *
  * @author Roman Batygin
  */
-public interface ExperimentRequestRepository extends JpaRepository<ExperimentRequestEntity, Long> {
-
-    /**
-     * Finds experiment request with correlation id and stage.
-     *
-     * @param correlationId - correlation id
-     * @return experiment request entity
-     */
-    Optional<ExperimentRequestEntity> findByCorrelationId(String correlationId);
-
-    /**
-     * Finds experiment request by request id
-     *
-     * @param requestId - request id
-     * @return experiment request entity
-     */
-    Optional<ExperimentRequestEntity> findByRequestId(String requestId);
-
-    /**
-     * Gets experiment requests page for specified job.
-     *
-     * @param autoTestsJobEntity - auto tests job
-     * @param pageable           - pageable object
-     * @return experiment requests page
-     */
-    Page<ExperimentRequestEntity> findAllByJob(AutoTestsJobEntity autoTestsJobEntity, Pageable pageable);
+public interface ExperimentRequestRepository extends JpaBaseEvaluationRequestRepository<ExperimentRequestEntity> {
 
     /**
      * Finds finished requests ids.
@@ -50,12 +19,4 @@ public interface ExperimentRequestRepository extends JpaRepository<ExperimentReq
      */
     @Query("select er.id from ExperimentRequestEntity er where er.stageType = 'REQUEST_FINISHED' order by er.started")
     List<Long> findFinishedRequests();
-
-    /**
-     * Finds experiment requests page with specified ids.
-     *
-     * @param ids - ids list
-     * @return experiment requests page
-     */
-    List<ExperimentRequestEntity> findByIdInOrderByCreated(Collection<Long> ids);
 }
