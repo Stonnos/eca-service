96699c61d0b91ce4ea4236eb1a7fb65256314d0b
Батыгин Роман Игоревич <batygin@self.team>
2022-03-29 14:38:38 +0700

    #ES-197 - impl evaluation results event publishing

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/mq/listener/EvaluationMessageListener.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/mq/listener/EvaluationMessageListener.java
@@ -2,15 +2,17 @@ package com.ecaservice.auto.test.mq.listener;
 
 import com.ecaservice.auto.test.entity.autotest.EvaluationRequestEntity;
 import com.ecaservice.auto.test.entity.autotest.RequestStageType;
+import com.ecaservice.auto.test.event.model.EvaluationResultsTestStepEvent;
 import com.ecaservice.auto.test.repository.autotest.EvaluationRequestRepository;
+import com.ecaservice.auto.test.repository.autotest.EvaluationResultsTestStepRepository;
 import com.ecaservice.auto.test.service.EvaluationRequestService;
-import com.ecaservice.auto.test.service.EvaluationResultsProcessor;
 import com.ecaservice.base.model.EvaluationResponse;
 import com.ecaservice.base.model.TechnicalStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import static com.ecaservice.auto.test.util.Utils.getFirstErrorMessage;
@@ -25,9 +27,10 @@ import static com.ecaservice.auto.test.util.Utils.getFirstErrorMessage;
 @RequiredArgsConstructor
 public class EvaluationMessageListener {
 
+    private final ApplicationEventPublisher applicationEventPublisher;
     private final EvaluationRequestService evaluationRequestService;
-    private final EvaluationResultsProcessor evaluationResultsProcessor;
     private final EvaluationRequestRepository evaluationRequestRepository;
+    private final EvaluationResultsTestStepRepository evaluationResultsTestStepRepository;
 
     /**
      * Handles evaluation response message from eca - server.
@@ -55,8 +58,7 @@ public class EvaluationMessageListener {
                                         String correlationId) {
         evaluationRequestEntity.setRequestId(evaluationResponse.getRequestId());
         if (TechnicalStatus.SUCCESS.equals(evaluationResponse.getStatus())) {
-            evaluationResultsProcessor.compareAndMatchEvaluationResults(evaluationRequestEntity,
-                    evaluationResponse.getEvaluationResults());
+            handleSuccessResponse(evaluationRequestEntity, evaluationResponse);
         } else {
             log.info("Got error response [{}] for evaluation request [{}], correlation id [{}]",
                     evaluationResponse.getStatus(),
@@ -65,5 +67,18 @@ public class EvaluationMessageListener {
             evaluationRequestService.finishWithError(evaluationRequestEntity, errorMessage);
         }
         log.info("Message [{}] response has been processed", evaluationRequestEntity.getCorrelationId());
+    }
+
+    private void handleSuccessResponse(EvaluationRequestEntity evaluationRequestEntity,
+                                       EvaluationResponse evaluationResponse) {
+        var evaluationResultsStep =
+                evaluationResultsTestStepRepository.findByEvaluationRequestEntity(evaluationRequestEntity);
+        if (evaluationResultsStep == null) {
+            log.warn("Evaluation results step not found for evaluation request with id [{}]",
+                    evaluationRequestEntity.getRequestId());
+        } else {
+            applicationEventPublisher.publishEvent(new EvaluationResultsTestStepEvent(this, evaluationResultsStep,
+                    evaluationResponse.getEvaluationResults()));
+        }
     }
 }
