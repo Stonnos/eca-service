df6d8a7ed8326ee29b13e6f6033e5d5e320681fa
Батыгин Роман Игоревич <batygin@self.team>
2022-03-30 11:56:21 +0700

    #ES-197 - added testSteps collection

--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/dto/EvaluationRequestExtendedDto.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/dto/EvaluationRequestExtendedDto.java
@@ -1,8 +1,12 @@
 package com.ecaservice.auto.test.dto;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+import java.util.List;
 
 /**
  * Evaluation request extended dto.
@@ -13,4 +17,10 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class EvaluationRequestExtendedDto extends EvaluationRequestDto {
+
+    /**
+     * Test steps
+     */
+    @ArraySchema(schema = @Schema(description = "Test steps"))
+    private List<BaseTestStepDto> testSteps;
 }
--- eca-auto-tests/src/main/java/com/ecaservice/auto/test/dto/ExperimentResultsTestStepDto.java
+++ eca-auto-tests/src/main/java/com/ecaservice/auto/test/dto/ExperimentResultsTestStepDto.java
@@ -1,6 +1,7 @@
 package com.ecaservice.auto.test.dto;
 
 import com.ecaservice.auto.test.model.evaluation.EvaluationResultsDetailsMatch;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,6 +22,6 @@ public class ExperimentResultsTestStepDto extends BaseTestStepDto {
     /**
      * Experiment results details matches
      */
-    @Schema(description = "Experiment results details matches")
+    @ArraySchema(schema = @Schema(description = "Experiment results details matches"))
     private List<EvaluationResultsDetailsMatch> experimentResultDetails;
 }
