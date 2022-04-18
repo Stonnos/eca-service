2692428ecee2a442de19ed48e73a2f4e10891211
Батыгин Роман Игоревич <batygin@self.team>
2022-04-18 15:00:44 +0700

    #ES-199 - added FormTemplateGroupEntity and changeset

---
+++ core-form-template/src/main/java/com/ecaservice/core/form/template/entity/FormTemplateGroupEntity.java
@@ -0,0 +1,48 @@
+package com.ecaservice.core.form.template.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Form template group persistence entity.
+ *
+ * @author Roman Batygin
+ */
+@Data
+@Entity
+@Table(name = "form_template_group")
+public class FormTemplateGroupEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * Creation date
+     */
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+    /**
+     * Group name
+     */
+    @Column(name = "group_name", nullable = false, unique = true)
+    private String groupName;
+
+    /**
+     * Form templates list
+     */
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "group_id", nullable = false)
+    private List<FormTemplateEntity> templates;
+}
