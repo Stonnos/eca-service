359fa8b475f24de52d6af0cd30d9b4c53c88363b
Батыгин Роман Игоревич <batygin@self.team>
2022-04-19 12:19:36 +0700

    #ES-199 - impl ClassifiersTemplatesController

---
+++ eca-server/src/main/java/com/ecaservice/server/controller/web/ClassifiersTemplatesController.java
@@ -0,0 +1,82 @@
+package com.ecaservice.server.controller.web;
+
+import com.ecaservice.core.form.template.service.FormTemplateProvider;
+import com.ecaservice.web.dto.model.FormTemplateDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.ecaservice.config.swagger.OpenApi30Configuration.ECA_AUTHENTICATION_SECURITY_SCHEME;
+import static com.ecaservice.config.swagger.OpenApi30Configuration.SCOPE_WEB;
+import static com.ecaservice.web.dto.doc.CommonApiExamples.UNAUTHORIZED_RESPONSE_JSON;
+
+/**
+ * Implements API to manage classifiers templates.
+ *
+ * @author Roman Batygin
+ */
+@Slf4j
+@Tag(name = "API to manage classifiers templates")
+@Validated
+@RestController
+@RequestMapping("/classifiers/templates")
+@RequiredArgsConstructor
+public class ClassifiersTemplatesController {
+
+    private static final String CLASSIFIERS_GROUP = "classifiers";
+
+    private final FormTemplateProvider formTemplateProvider;
+
+    /**
+     * Gets classifiers form templates.
+     *
+     * @return form templates list
+     */
+    @PreAuthorize("#oauth2.hasScope('web')")
+    @Operation(
+            description = "Gets classifiers form templates",
+            summary = "Gets classifiers form templates",
+            security = @SecurityRequirement(name = ECA_AUTHENTICATION_SECURITY_SCHEME, scopes = SCOPE_WEB),
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = {
+                                            @ExampleObject(value = ""),
+                                    },
+                                    array = @ArraySchema(schema = @Schema(implementation = FormTemplateDto.class))
+                            )
+                    ),
+                    @ApiResponse(description = "Not authorized", responseCode = "401",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = {
+                                            @ExampleObject(value = UNAUTHORIZED_RESPONSE_JSON),
+                                    }
+                            )
+                    )
+            }
+    )
+    @GetMapping(value = "/list")
+    public List<FormTemplateDto> getClassifiersTemplates() {
+        log.info("Request classifiers templates");
+        var classifierTemplates = formTemplateProvider.getTemplates(CLASSIFIERS_GROUP);
+        log.info("[{}] classifiers form templates has been fetched", classifierTemplates.size());
+        return classifierTemplates;
+    }
+}
