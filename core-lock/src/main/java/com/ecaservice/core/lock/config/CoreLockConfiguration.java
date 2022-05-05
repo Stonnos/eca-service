1aa2d9ce20e0ca597281601468bf21736c607cc3
Батыгин Роман Игоревич <batygin@self.team>
2022-05-05 18:14:06 +0700

    #ES-202 - fix core-lock configuration

--- core-lock/src/main/java/com/ecaservice/core/lock/config/CoreLockConfiguration.java
+++ core-lock/src/main/java/com/ecaservice/core/lock/config/CoreLockConfiguration.java
@@ -3,6 +3,7 @@ package com.ecaservice.core.lock.config;
 import com.ecaservice.core.lock.fallback.DefaultFallbackHandler;
 import com.ecaservice.core.lock.fallback.FallbackHandler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Roman Batygin
  */
 @Configuration
+@ComponentScan({"com.ecaservice.core.lock"})
 public class CoreLockConfiguration {
 
     /**
