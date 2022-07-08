74f9534f33c2c84626e40f66c7c896ca79940aa7
Роман Батыгин <roman.batygin@mail.ru>
2022-07-08 10:29:35 +0700

    #ES-209 - impl MessageTemplateProvider

--- eca-server/src/main/java/com/ecaservice/server/config/cache/CacheNames.java
+++ eca-server/src/main/java/com/ecaservice/server/config/cache/CacheNames.java
@@ -16,17 +16,7 @@ public class CacheNames {
     public static final String EVALUATION_RESULTS_CACHE_NAME = "evaluation-results";
 
     /**
-     * Global filter cache name
+     * Message templates cache name
      */
-    public static final String GLOBAL_FILTERS_CACHE_NAME = "global-filters";
-
-    /**
-     * Filter templates cache name
-     */
-    public static final String FILTER_TEMPLATES_CACHE_NAME = "filter-templates";
-
-    /**
-     * Filter dictionaries cache name
-     */
-    public static final String FILTER_DICTIONARIES_CACHE_NAME = "filter-dictionaries";
+    public static final String MESSAGE_TEMPLATES_CACHE_NAME = "message-templates";
 }
---
+++ eca-server/src/main/java/com/ecaservice/server/service/message/template/MessageTemplateProvider.java
@@ -0,0 +1,39 @@
+package com.ecaservice.server.service.message.template;
+
+import com.ecaservice.common.web.exception.EntityNotFoundException;
+import com.ecaservice.server.model.entity.MessageTemplateEntity;
+import com.ecaservice.server.repository.MessageTemplateRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import static com.ecaservice.server.config.cache.CacheNames.MESSAGE_TEMPLATES_CACHE_NAME;
+
+/**
+ * Message templates provider.
+ *
+ * @author Roman Batygin
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MessageTemplateProvider {
+
+    private final MessageTemplateRepository messageTemplateRepository;
+
+    /**
+     * Gets message template by id.
+     *
+     * @param id - template id
+     * @return message template entity
+     */
+    @Cacheable(MESSAGE_TEMPLATES_CACHE_NAME)
+    public MessageTemplateEntity getTemplate(String id) {
+        log.info("Starting to load message template [{}]", id);
+        var messageTemplate = messageTemplateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MessageTemplateEntity.class, id));
+        log.info("Message template [{}] has been loaded", id);
+        return messageTemplate;
+    }
+}
