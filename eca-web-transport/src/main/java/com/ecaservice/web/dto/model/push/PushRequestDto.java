66763ce6de7c0bd83d5b0e45a689a9be1850c46c
Роман Батыгин <roman.batygin@mail.ru>
2022-07-29 12:12:12 +0700

    #ES-221 - added PushRequestDto

---
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/model/push/PushRequestDto.java
@@ -0,0 +1,55 @@
+package com.ecaservice.web.dto.model.push;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Map;
+
+import static com.ecaservice.web.dto.util.FieldConstraints.MAX_ADDITIONAL_PROPERTIES_SIZE;
+import static com.ecaservice.web.dto.util.FieldConstraints.MAX_LENGTH_255;
+import static com.ecaservice.web.dto.util.FieldConstraints.UUID_MAX_LENGTH;
+import static com.ecaservice.web.dto.util.FieldConstraints.UUID_PATTERN;
+import static com.ecaservice.web.dto.util.FieldConstraints.VALUE_1;
+
+/**
+ * Push request dto.
+ */
+@Data
+@Schema(description = "Push request")
+public class PushRequestDto {
+
+    /**
+     * Request id
+     */
+    @NotBlank
+    @Pattern(regexp = UUID_PATTERN)
+    @Size(min = VALUE_1, max = UUID_MAX_LENGTH)
+    @Schema(description = "Request id", example = "1d2de514-3a87-4620-9b97-c260e24340de", required = true)
+    private String requestId;
+
+    /**
+     * Message type
+     */
+    @NotBlank
+    @Size(min = VALUE_1, max = MAX_LENGTH_255)
+    @Schema(description = "Message type", example = "EXPERIMENT_STATUS", required = true)
+    private String messageType;
+
+    /**
+     * Message text
+     */
+    @Size(max = MAX_LENGTH_255)
+    @Schema(description = "Message text", example = "Message text")
+    private String messageText;
+
+    /**
+     * Additional properties
+     */
+    @ArraySchema(schema = @Schema(description = "Additional properties"), maxItems = MAX_ADDITIONAL_PROPERTIES_SIZE)
+    @Size(max = MAX_ADDITIONAL_PROPERTIES_SIZE)
+    private Map<@NotBlank String, @NotBlank String> additionalProperties;
+}
--- eca-web-transport/src/main/java/com/ecaservice/web/dto/util/FieldConstraints.java
+++ eca-web-transport/src/main/java/com/ecaservice/web/dto/util/FieldConstraints.java
@@ -144,4 +144,15 @@ public class FieldConstraints {
      * Experiment results max items
      */
     public static final int EXPERIMENT_RESULTS_MAX_ITEMS = 5;
+
+    /**
+     * UUID pattern.
+     */
+    public static final String UUID_PATTERN =
+            "^[0-9a-f]{8}-[0-9a-f]{4}-[34][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$";
+
+    /**
+     * Max. additional properties size
+     */
+    public static final int MAX_ADDITIONAL_PROPERTIES_SIZE = 50;
 }
