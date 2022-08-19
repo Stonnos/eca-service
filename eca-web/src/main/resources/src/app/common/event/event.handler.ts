0efb6376b6edbc55d1407ffaff361cebdd7a2732
Роман Батыгин <roman.batygin@mail.ru>
2022-08-19 16:26:55 +0700

    #ES-230 - fix push service connection for refresh token event

--- eca-web/src/main/resources/src/app/common/event/event.handler.ts
+++ eca-web/src/main/resources/src/app/common/event/event.handler.ts
@@ -4,12 +4,15 @@ import { PushService } from "../push/push.service";
 import { MessageService } from "primeng/api";
 import { EventType } from "./event.type";
 import { Logger } from "../util/logging";
+import { UsersService } from "../../users/services/users.service";
+import { UserDto } from "../../../../../../../target/generated-sources/typescript/eca-web-dto";
 
 @Injectable()
 export class EventHandler {
 
   public constructor(private eventService: EventService,
                      private pushService: PushService,
+                     private usersService: UsersService,
                      private messageService: MessageService) {
   }
 
@@ -17,11 +20,13 @@ export class EventHandler {
     return this.eventService.eventSubscribe()
       .subscribe({
         next: (eventType: EventType) => {
-          Logger.debug(`Received event ${eventType}`);
+          Logger.debug(`Handle event ${eventType}`);
           switch (eventType) {
             case EventType.INIT_PUSH:
-            case EventType.TOKEN_REFRESHED:
               this.pushService.init();
+              break;
+            case EventType.TOKEN_REFRESHED:
+              this.handleTokenRefreshedEvent();
               break;
             case EventType.CLOSE_PUSH:
             case EventType.TOKEN_EXPIRED:
@@ -36,5 +41,21 @@ export class EventHandler {
           this.messageService.add({ severity: 'error', summary: 'Ошибка', detail: error.message });
         }
       });
+  }
+
+  private handleTokenRefreshedEvent(): void {
+    this.usersService.getCurrentUser().subscribe({
+      next: (user: UserDto) => {
+        if (!user.pushEnabled) {
+          Logger.debug('Pushes are disabled for user. Skipped push reconnection after token has been refreshed');
+        } else {
+          Logger.debug('Starting push reconnection after token has been refreshed');
+          this.pushService.init();
+        }
+      },
+      error: (error) => {
+        this.messageService.add({ severity: 'error', summary: 'Ошибка', detail: error.message });
+      }
+    });
   }
 }
