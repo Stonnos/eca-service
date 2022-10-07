04ab912af24239b1c01f6dd316a0f661a8a2d7fd
Роман Батыгин <roman.batygin@mail.ru>
2022-10-07 11:26:19 +0700

    #ES-232 - removed UserPushNotificationRequest#created field

--- eca-web-push-transport/src/main/java/com/ecaservice/web/push/dto/UserPushNotificationRequest.java
+++ eca-web-push-transport/src/main/java/com/ecaservice/web/push/dto/UserPushNotificationRequest.java
@@ -1,23 +1,14 @@
 package com.ecaservice.web.push.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 import java.util.List;
 
-import static com.ecaservice.web.push.dto.FieldConstraints.DATE_TIME_PATTERN;
-import static com.ecaservice.web.push.dto.FieldConstraints.LOCAL_DATE_TIME_MAX_LENGTH;
 import static com.ecaservice.web.push.dto.FieldConstraints.MAX_LENGTH_255;
 import static com.ecaservice.web.push.dto.FieldConstraints.RECEIVERS_MAX_SIZE;
 import static com.ecaservice.web.push.dto.FieldConstraints.VALUE_1;
@@ -47,14 +38,4 @@ public class UserPushNotificationRequest extends AbstractPushRequest {
     @Size(max = RECEIVERS_MAX_SIZE)
     @Schema(description = "Receivers list")
     private List<String> receivers;
-
-    /**
-     * Created date
-     */
-    @NotNull
-    @JsonFormat(pattern = DATE_TIME_PATTERN)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @Schema(description = "Created date", pattern = DATE_TIME_PATTERN, maxLength = LOCAL_DATE_TIME_MAX_LENGTH)
-    private LocalDateTime created;
 }
