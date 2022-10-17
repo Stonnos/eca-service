f4ffa4299e9e2ba1fcb9c7f945e517122296d45f
Роман Батыгин <roman.batygin@mail.ru>
2022-10-17 17:40:38 +0700

    #ES-237 - added PushTokenEntity and change set

---
+++ eca-web-push/src/main/java/com/ecaservice/web/push/entity/PushTokenEntity.java
@@ -0,0 +1,43 @@
+package com.ecaservice.web.push.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+/**
+ * Push token persistence entity.
+ *
+ * @author Roman Batygin
+ */
+@Data
+@Entity
+@Table(name = "push_token")
+public class PushTokenEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * User
+     */
+    @Column(nullable = false, unique = true)
+    private String user;
+
+    /**
+     * Token value
+     */
+    @Column(name = "token_id", nullable = false)
+    private String tokenId;
+
+    /**
+     * Token expiration date
+     */
+    @Column(name = "expire_at", nullable = false)
+    private LocalDateTime expireAt;
+}
