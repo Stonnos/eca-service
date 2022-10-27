bcff50867841101684cc6c14b47c96a2e6104ab0
Роман Батыгин <roman.batygin@mail.ru>
2022-10-27 11:21:22 +0700

    #ES-243 - added java docs to ExperimentStepService

--- eca-server/src/main/java/com/ecaservice/server/service/experiment/ExperimentStepService.java
+++ eca-server/src/main/java/com/ecaservice/server/service/experiment/ExperimentStepService.java
@@ -7,6 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+/**
+ * Experiment step service.
+ *
+ * @author Roman Batygin
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -14,20 +21,52 @@ public class ExperimentStepService {
 
     private final ExperimentStepRepository experimentStepRepository;
 
+    /**
+     * Completes experiment step.
+     *
+     * @param experimentStepEntity - experiment step entity
+     */
     public void complete(ExperimentStepEntity experimentStepEntity) {
+        log.info("Starting to complete experiment [{}] step [{}]",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
         experimentStepEntity.setStatus(ExperimentStepStatus.COMPLETED);
+        experimentStepEntity.setCompleted(LocalDateTime.now());
         experimentStepRepository.save(experimentStepEntity);
+        log.info("Experiment [{}] step [{}] has been completed",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
     }
 
+    /**
+     * Completes experiment step with error.
+     *
+     * @param experimentStepEntity - experiment step entity
+     * @param errorMessage         - error message
+     */
     public void completeWithError(ExperimentStepEntity experimentStepEntity, String errorMessage) {
+        log.info("Starting to complete experiment [{}] step [{}] with error",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
         experimentStepEntity.setStatus(ExperimentStepStatus.ERROR);
         experimentStepEntity.setErrorMessage(errorMessage);
+        experimentStepEntity.setCompleted(LocalDateTime.now());
         experimentStepRepository.save(experimentStepEntity);
+        log.info("Experiment [{}] step [{}] has been completed with error",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
     }
 
+    /**
+     * Fails experiment step.
+     *
+     * @param experimentStepEntity - experiment step entity
+     * @param errorMessage         - error message
+     */
     public void failed(ExperimentStepEntity experimentStepEntity, String errorMessage) {
+        log.info("Starting to failure experiment [{}] step [{}] with error",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
         experimentStepEntity.setStatus(ExperimentStepStatus.FAILED);
         experimentStepEntity.setErrorMessage(errorMessage);
+        experimentStepEntity.setCompleted(LocalDateTime.now());
         experimentStepRepository.save(experimentStepEntity);
+        log.info("Experiment [{}] step [{}] has been failed",
+                experimentStepEntity.getExperiment().getRequestId(), experimentStepEntity.getStep());
     }
 }
