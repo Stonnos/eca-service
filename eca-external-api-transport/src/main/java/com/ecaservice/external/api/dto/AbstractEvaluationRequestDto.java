a1d03f07d90f2e30becabffd7864290ad6f1c6c4
Роман Батыгин <roman.batygin@mail.ru>
2022-11-07 11:48:19 +0700

    #ES-247 - added AbstractEvaluationRequestDto in eca-external-api-transport

---
+++ eca-external-api-transport/src/main/java/com/ecaservice/external/api/dto/AbstractEvaluationRequestDto.java
@@ -0,0 +1,30 @@
+package com.ecaservice.external.api.dto;
+
+import com.ecaservice.external.api.dto.annotations.DataURL;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+import static com.ecaservice.external.api.dto.Constraints.MAX_LENGTH_255;
+import static com.ecaservice.external.api.dto.Constraints.MIN_LENGTH_1;
+
+/**
+ * Abstract evaluation request dto model.
+ *
+ * @author Roman Batygin
+ */
+@Data
+@Schema(description = "Base evaluation request model")
+public class AbstractEvaluationRequestDto implements Serializable {
+
+    /**
+     * Training data url
+     */
+    @DataURL
+    @Size(min = MIN_LENGTH_1, max = MAX_LENGTH_255)
+    @Schema(description = "Train data url", example = "http://kt.ijs.si/Branax/Repository/WEKA/Iris.xls",
+            required = true)
+    private String trainDataUrl;
+}
