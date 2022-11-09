49b1e4a3534e2126331da70fb1ae7201d45bc0ef
Роман Батыгин <roman.batygin@mail.ru>
2022-11-09 11:31:38 +0700

    #ES-248 - delete fields from ClassifierOptionsInfo

--- eca-ers/src/main/java/com/ecaservice/ers/model/ClassifierOptionsInfo.java
+++ eca-ers/src/main/java/com/ecaservice/ers/model/ClassifierOptionsInfo.java
@@ -2,20 +2,11 @@ package com.ecaservice.ers.model;
 
 import lombok.Data;
 
-import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapKeyColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Classifier options info persistence entity.
@@ -48,26 +39,4 @@ public class ClassifierOptionsInfo {
      */
     @Column(name = "classifier_description")
     private String classifierDescription;
-
-    /**
-     * Is meta classifier (used for stacking algorithms)?
-     */
-    @Column(name = "meta_classifier")
-    private boolean metaClassifier;
-
-    /**
-     * Classifier input options map
-     */
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "input_options")
-    @MapKeyColumn(name = "option_name")
-    @Column(name = "option_value")
-    private Map<String, String> inputOptionsMap;
-
-    /**
-     * Individual classifiers options for ensemble algorithms
-     */
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "parent_id")
-    private List<ClassifierOptionsInfo> individualClassifiers;
 }
