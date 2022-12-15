336467f80b866a986500ab4d478d3e8d8653385b
Роман Батыгин <roman.batygin@mail.ru>
2022-12-15 12:23:40 +0700

    #ES-259 - added open api

--- discovery-server/src/main/java/com/ecaservice/discovery/prometheus/controller/PrometheusSdApiController.java
+++ discovery-server/src/main/java/com/ecaservice/discovery/prometheus/controller/PrometheusSdApiController.java
@@ -2,8 +2,16 @@ package com.ecaservice.discovery.prometheus.controller;
 
 import com.ecaservice.discovery.prometheus.dto.PrometheusSdConfig;
 import com.ecaservice.discovery.prometheus.service.PrometheusSdConfigService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +24,7 @@ import java.util.List;
  * @author Roman Batygin
  */
 @Slf4j
+@Tag(name = "Service discovery api for prometheus")
 @RestController
 @RequestMapping("/api/prometheus")
 @RequiredArgsConstructor
@@ -28,6 +37,24 @@ public class PrometheusSdApiController {
      *
      * @return sd configs list
      */
+    @Operation(
+            description = "Gets service discovery configs for prometheus",
+            summary = "Gets service discovery configs for prometheus",
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "PrometheusSdConfigsResponse",
+                                                    ref = "#/components/examples/PrometheusSdConfigsResponse"
+                                            )
+                                    },
+                                    array = @ArraySchema(schema = @Schema(implementation = PrometheusSdConfig.class))
+                            )
+                    )
+            }
+    )
     @GetMapping(value = "/sd-configs")
     public List<PrometheusSdConfig> getSdConfigs() {
         log.debug("Request get prometheus sd target configs");
