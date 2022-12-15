6d62d1fb02f672df7f65b9dcd534c4b8ef454ff8
Роман Батыгин <roman.batygin@mail.ru>
2022-12-15 12:29:40 +0700

    #ES-259 - added DiscoveryServerConfiguration

---
+++ discovery-server/src/main/java/com/ecaservice/discovery/config/DiscoveryServerConfiguration.java
@@ -0,0 +1,14 @@
+package com.ecaservice.discovery.config;
+
+import com.ecaservice.config.swagger.annotation.EnableOpenApi;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Discovery server configuration.
+ *
+ * @author Roman Batygin
+ */
+@EnableOpenApi
+@Configuration
+public class DiscoveryServerConfiguration {
+}
