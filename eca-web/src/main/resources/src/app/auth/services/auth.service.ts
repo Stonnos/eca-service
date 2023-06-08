f21f6c4401800a1e742bbba3d52808d703d3df75
Roman <bat1238@yandex.ru>
2023-06-08 21:56:55 +0700

    #ES-270 - pass token value from web app

--- eca-web/src/main/resources/src/app/auth/services/auth.service.ts
+++ eca-web/src/main/resources/src/app/auth/services/auth.service.ts
@@ -23,8 +23,9 @@ export class AuthService {
     return this.performTokenRequest(params);
   }
 
-  public verifyTfaCode(code: string): Observable<any> {
+  public verifyTfaCode(token: string, code: string): Observable<any> {
     const params = new URLSearchParams();
+    params.append('token', token);
     params.append('tfa_code', code);
     params.append('grant_type', 'tfa_code');
     return this.performTokenRequest(params);
