820956965df190d0d571435790e585dc26e19bab
Roman <bat1238@yandex.ru>
2023-06-14 12:59:14 +0700

    #ES-272 - drop NotificationEntity#initiator not null constraint

--- eca-web-push-transport/src/main/java/com/ecaservice/web/push/dto/UserPushNotificationRequest.java
+++ eca-web-push-transport/src/main/java/com/ecaservice/web/push/dto/UserPushNotificationRequest.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -42,8 +41,7 @@ public class UserPushNotificationRequest extends AbstractPushRequest {
     /**
      * Initiator user
      */
-    @NotBlank
-    @Size(min = VALUE_1, max = MAX_LENGTH_255)
+    @Size(max = MAX_LENGTH_255)
     @Schema(description = "Initiator user")
     private String initiator;
 
