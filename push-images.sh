89ca90ae67c75e7c1fa6fcc67548ec1402e5d775
Roman <bat1238@yandex.ru>
2023-06-16 01:57:00 +0700

    #ES-275 - added push-images.sh script

---
+++ push-images.sh
@@ -0,0 +1,20 @@
+docker tag eca-oauth dockerbatygin/eca:eca-oauth
+docker push dockerbatygin/eca:eca-oauth
+docker tag eca-server dockerbatygin/eca:eca-server
+docker push dockerbatygin/eca:eca-server
+docker tag eca-mail dockerbatygin/eca:eca-mail
+docker push dockerbatygin/eca:eca-mail
+docker tag eca-data-storage dockerbatygin/eca:eca-data-storage
+docker push dockerbatygin/eca:eca-data-storage
+docker tag eca-ers dockerbatygin/eca:eca-ers
+docker push dockerbatygin/eca:eca-ers
+docker tag eca-web dockerbatygin/eca:eca-web
+docker push dockerbatygin/eca:eca-web
+docker tag zuul-gate dockerbatygin/eca:zuul-gate
+docker push dockerbatygin/eca:zuul-gate
+docker tag discovery-server dockerbatygin/eca:discovery-server
+docker push dockerbatygin/eca:discovery-server
+docker tag eca-web-push dockerbatygin/eca:eca-web-push
+docker push dockerbatygin/eca:eca-web-push
+docker tag eca-audit-log dockerbatygin/eca:eca-audit-log
+docker push dockerbatygin/eca:eca-audit-log
