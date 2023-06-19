93b4c400cf6c818ed662e25b2f25e6707eb8263a
Roman <bat1238@yandex.ru>
2023-06-19 15:15:08 +0700

    #ES-276 - removed EvaluationResponse#evaluationResults field

--- eca-model/src/main/java/com/ecaservice/base/model/EvaluationResponse.java
+++ eca-model/src/main/java/com/ecaservice/base/model/EvaluationResponse.java
@@ -1,10 +1,5 @@
 package com.ecaservice.base.model;
 
-import com.ecaservice.base.model.databind.EvaluationResultsDeserializer;
-import com.ecaservice.base.model.databind.EvaluationResultsSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import eca.core.evaluation.EvaluationResults;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -53,11 +48,4 @@ public class EvaluationResponse extends EcaResponse {
      * Mean absolute error
      */
     private BigDecimal meanAbsoluteError;
-
-    /**
-     * Evaluation results
-     */
-    @JsonSerialize(using = EvaluationResultsSerializer.class)
-    @JsonDeserialize(using = EvaluationResultsDeserializer.class)
-    private EvaluationResults evaluationResults;
 }
