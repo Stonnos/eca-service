b01387a2b9675f029d3e7a14dbd179799eb856f4
Roman <bat1238@yandex.ru>
2023-08-13 14:31:58 +0700

    #ES-288 - fix eca-mail startup

--- eca-mail/src/main/java/com/ecaservice/mail/config/EcaMailConfiguration.java
+++ eca-mail/src/main/java/com/ecaservice/mail/config/EcaMailConfiguration.java
@@ -4,11 +4,15 @@ import com.ecaservice.common.web.annotation.EnableGlobalExceptionHandler;
 import com.ecaservice.config.swagger.annotation.EnableOpenApi;
 import com.ecaservice.core.filter.annotation.EnableFilters;
 import com.ecaservice.core.filter.error.FilterExceptionHandler;
+import com.ecaservice.mail.model.Email;
+import com.ecaservice.mail.repository.EmailRepository;
 import com.ecaservice.oauth2.annotation.Oauth2ResourceServer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -23,6 +27,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @Oauth2ResourceServer
 @EnableGlobalExceptionHandler
+@EntityScan(basePackageClasses = Email.class)
+@EnableJpaRepositories(basePackageClasses = EmailRepository.class)
 @EnableConfigurationProperties(MailConfig.class)
 @Import(FilterExceptionHandler.class)
 public class EcaMailConfiguration {
