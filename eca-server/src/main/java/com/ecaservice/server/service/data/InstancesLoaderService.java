6607b2ee69baa732236b0f8730bb0617c776fcb1
Roman <bat1238@yandex.ru>
2023-08-30 21:44:13 +0700

    #ES-291 - impl InstancesLoaderService

--- eca-server/src/main/java/com/ecaservice/server/service/data/InstancesLoaderService.java
+++ eca-server/src/main/java/com/ecaservice/server/service/data/InstancesLoaderService.java
@@ -1,20 +1,51 @@
 package com.ecaservice.server.service.data;
 
+import com.ecaservice.s3.client.minio.databind.JsonDeserializer;
+import com.ecaservice.s3.client.minio.service.ObjectStorageService;
+import eca.data.file.converter.InstancesConverter;
+import eca.data.file.model.InstancesModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import weka.core.Instances;
 
+import java.io.IOException;
+
+/**
+ * Service for loading instances from storage.
+ *
+ * @author Roman Batygin
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class InstancesLoaderService {
 
+    private final ObjectStorageService objectStorageService;
     private final InstancesMetaDataService instancesMetaDataService;
+    private final InstancesConverter instancesConverter = new InstancesConverter();
+    private final JsonDeserializer<InstancesModel> instancesModelJsonDeserializer = new JsonDeserializer<>();
 
-
+    /**
+     * Loads instances with specified uuid.
+     *
+     * @param uuid - instances uuid
+     * @return instances
+     */
     public Instances loadInstances(String uuid) {
-        //TODO impl method
-        return null;
+        log.info("Starting to load instances [{}] from data storage", uuid);
+        var instancesMetaData = instancesMetaDataService.getInstancesMetaData(uuid);
+        var instancesModel = getInstancesModel(instancesMetaData.getObjectPath());
+        var instances = instancesConverter.convert(instancesModel);
+        log.info("Instances [{}] has been loaded from data storage", uuid);
+        return instances;
+    }
+
+    private InstancesModel getInstancesModel(String objectPath) {
+        try {
+            return objectStorageService.getObject(objectPath, InstancesModel.class, instancesModelJsonDeserializer);
+        } catch (IOException | ClassNotFoundException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 }
