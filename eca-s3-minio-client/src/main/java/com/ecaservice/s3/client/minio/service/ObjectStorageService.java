9e0eeaca6acf73cd98d84c32017f1f1b5c2b07f5
Roman <bat1238@yandex.ru>
2023-08-31 17:40:07 +0700

    #ES-291 - impl FstSerializer. Refactoring ObjectStorageService

--- eca-s3-minio-client/src/main/java/com/ecaservice/s3/client/minio/service/ObjectStorageService.java
+++ eca-s3-minio-client/src/main/java/com/ecaservice/s3/client/minio/service/ObjectStorageService.java
@@ -1,14 +1,15 @@
 package com.ecaservice.s3.client.minio.service;
 
 import com.ecaservice.s3.client.minio.databind.FstDeserializer;
+import com.ecaservice.s3.client.minio.databind.FstSerializer;
 import com.ecaservice.s3.client.minio.databind.ObjectDeserializer;
+import com.ecaservice.s3.client.minio.databind.ObjectSerializer;
 import com.ecaservice.s3.client.minio.model.GetPresignedUrlObject;
 import com.ecaservice.s3.client.minio.model.UploadObject;
 import io.micrometer.core.annotation.Timed;
 import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.nustaq.serialization.FSTObjectOutput;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
@@ -32,33 +33,43 @@ public class ObjectStorageService {
     private final MinioStorageService minioStorageService;
 
     /**
-     * Uploads serialized object to S3 storage.
+     * Uploads serialized object to S3 storage. Default {@link FstSerializer} used by default.
      *
      * @param object     - object
      * @param objectPath - object path in S3
      * @throws IOException in case of I/O error
      */
     public void uploadObject(Serializable object, String objectPath) throws IOException {
+        uploadObject(object, objectPath, new FstSerializer());
+    }
+
+    /**
+     * Uploads serialized object to S3 storage.
+     *
+     * @param object     - object
+     * @param objectPath - object path in S3
+     * @param serializer - object serializer
+     * @throws IOException in case of I/O error
+     */
+    public void uploadObject(Serializable object, String objectPath, ObjectSerializer serializer) throws IOException {
         log.info("Starting to upload object [{}] to storage", objectPath);
         log.info("Starting to serialize object [{}]", objectPath);
         var stopWatch = new StopWatch();
         stopWatch.start();
-        try (var fstObjectOutput = new FSTObjectOutput()) {
-            fstObjectOutput.writeObject(object);
-            stopWatch.stop();
-            log.info("Object [{}] has been serialized for {} s.", objectPath, stopWatch.getTotalTimeSeconds());
-            minioStorageService.uploadObject(
-                    UploadObject.builder()
-                            .objectPath(objectPath)
-                            .inputStream(() -> new ByteArrayInputStream(fstObjectOutput.getBuffer()))
-                            .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE)
-                            .build()
-            );
-        }
+        byte[] bytes = serializer.serialize(object);
+        stopWatch.stop();
+        log.info("Object [{}] has been serialized for {} s.", objectPath, stopWatch.getTotalTimeSeconds());
+        minioStorageService.uploadObject(
+                UploadObject.builder()
+                        .objectPath(objectPath)
+                        .inputStream(() -> new ByteArrayInputStream(bytes))
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE)
+                        .build()
+        );
     }
 
     /**
-     * Gets object from S3 storage.
+     * Gets object from S3 storage. Default {@link FstDeserializer} used by default.
      *
      * @param objectPath  - object path
      * @param targetClazz - target class
