3ec76939eeb8e7a05d8d924ffe27452c46540322
Roman <bat1238@yandex.ru>
2023-09-06 23:51:52 +0700

    #ES-290 - bugfix class attribute validation

--- eca-data-loader/src/main/java/com/ecaservice/data/loader/util/Utils.java
+++ eca-data-loader/src/main/java/com/ecaservice/data/loader/util/Utils.java
@@ -1,6 +1,8 @@
 package com.ecaservice.data.loader.util;
 
 import com.ecaservice.data.loader.exception.InvalidTrainDataFormatException;
+import eca.data.file.model.AttributeModel;
+import eca.data.file.model.AttributeType;
 import eca.data.file.model.InstancesModel;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
@@ -32,11 +34,20 @@ public class Utils {
         if (StringUtils.isEmpty(instancesModel.getClassName())) {
             throw new InvalidTrainDataFormatException("Got empty class name");
         }
-        if (instancesModel.getAttributes().stream().noneMatch(
-                attribute -> attribute.getName().equals(instancesModel.getClassName()))) {
-            throw new InvalidTrainDataFormatException(
-                    String.format("Class attribute [%s] not found in instances [%s] attributes set",
-                            instancesModel.getClassName(), instancesModel.getRelationName()));
+        var classAttribute = instancesModel.getAttributes()
+                .stream()
+                .filter(attributeModel -> attributeModel.getName().equals(instancesModel.getClassName()))
+                .findFirst()
+                .orElseThrow(() -> new InvalidTrainDataFormatException(
+                        String.format("Class attribute [%s] not found in instances [%s] attributes set",
+                                instancesModel.getClassName(), instancesModel.getRelationName())));
+        if (!AttributeType.NOMINAL.equals(classAttribute.getType())) {
+            throw new InvalidTrainDataFormatException(String.format("Class attribute [%s] must be nominal",
+                    classAttribute.getName()));
+        }
+        if (CollectionUtils.isEmpty(classAttribute.getValues()) || classAttribute.getValues().size() == 1) {
+            throw new InvalidTrainDataFormatException(String.format("Class attribute [%s] must have at least 2 values",
+                    classAttribute.getName()));
         }
     }
 }
