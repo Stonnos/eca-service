be83bfd2d9a958ec88861d8643889a7da673a960
Roman <bat1238@yandex.ru>
2023-09-08 15:09:46 +0700

    #ES-290 - removed InstancesMetaInfoDto#expireAt field

--- eca-data-loader-transport/src/main/java/com/ecaservice/data/loader/dto/InstancesMetaInfoDto.java
+++ eca-data-loader-transport/src/main/java/com/ecaservice/data/loader/dto/InstancesMetaInfoDto.java
@@ -1,18 +1,8 @@
 package com.ecaservice.data.loader.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDateTime;
-
-import static com.ecaservice.data.loader.dto.FieldConstraints.DATE_TIME_PATTERN;
-import static com.ecaservice.data.loader.dto.FieldConstraints.LOCAL_DATE_TIME_MAX_LENGTH;
 
 /**
  * Instances meta info dto.
@@ -27,7 +17,8 @@ public class InstancesMetaInfoDto {
     /**
      * Instances uuid
      */
-    @Schema(description = "Instances uuid", example = "f8cecbf7-405b-403b-9a94-f51e8fb73ed8", maxLength = FieldConstraints.UUID_MAX_SIZE)
+    @Schema(description = "Instances uuid", example = "f8cecbf7-405b-403b-9a94-f51e8fb73ed8",
+            maxLength = FieldConstraints.UUID_MAX_SIZE)
     private String uuid;
 
     /**
@@ -74,16 +65,7 @@ public class InstancesMetaInfoDto {
      * Instances object path in storage
      */
     @Schema(description = "Instances object path in storage",
-            example = "instances-f8cecbf7-405b-403b-9a94-f51e8fb73ed8.json", maxLength = FieldConstraints.MAX_LENGTH_255)
+            example = "instances-f8cecbf7-405b-403b-9a94-f51e8fb73ed8.json",
+            maxLength = FieldConstraints.MAX_LENGTH_255)
     private String objectPath;
-
-    /**
-     * Object expiration date
-     */
-    @JsonFormat(pattern = DATE_TIME_PATTERN)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @Schema(description = "Object expiration date", type = "string", example = "2021-07-01 14:00:00",
-            maxLength = LOCAL_DATE_TIME_MAX_LENGTH)
-    private LocalDateTime expireAt;
 }
