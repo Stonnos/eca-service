e392b54b55ac478457fbf477a5441b06eb35fe60
Roman <bat1238@yandex.ru>
2023-11-15 15:00:31 +0700

    #ES-306 - revert removed @Audit#initiatorkey

--- core-audit/src/main/java/com/ecaservice/core/audit/annotation/Audit.java
+++ core-audit/src/main/java/com/ecaservice/core/audit/annotation/Audit.java
@@ -24,6 +24,15 @@ public @interface Audit {
     String value() default "";
 
     /**
+     * Spring Expression Language (SpEL) attribute for computing the initiator key dynamically. Expression may contain
+     * method input parameter shortcut or method returned value shortcut.
+     * Method input parameters examples: {@code #id}, {@code #object.name} - for field in class object.
+     * Method returned value examples: {@code #result}, {@code #result.value} - for field in class object.
+     * Note that method returned value shortcut should start with #result prefix.
+     */
+    String initiatorKey() default "";
+
+    /**
      * Spring Expression Language (SpEL) attribute for computing the correlation id key dynamically. Expression may
      * contain method input parameter shortcut or method returned value shortcut.
      * Method input parameters examples: {@code #id}, {@code #object.name} - for field in class object.
