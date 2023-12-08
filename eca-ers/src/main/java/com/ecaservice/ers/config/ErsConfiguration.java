868f10f41686c2f6bc9c50304d9d62c894e78e85
Roman <bat1238@yandex.ru>
2023-12-08 18:22:10 +0700

    ECS-6 - enabled filters in ers

--- eca-ers/src/main/java/com/ecaservice/ers/config/ErsConfiguration.java
+++ eca-ers/src/main/java/com/ecaservice/ers/config/ErsConfiguration.java
@@ -2,19 +2,27 @@ package com.ecaservice.ers.config;
 
 import com.ecaservice.common.web.annotation.EnableGlobalExceptionHandler;
 import com.ecaservice.config.swagger.annotation.EnableOpenApi;
+import com.ecaservice.core.filter.annotation.EnableFilters;
+import com.ecaservice.ers.model.EvaluationResultsInfo;
+import com.ecaservice.ers.repository.EvaluationResultsInfoRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Ers configuration class.
  *
  * @author Roman Batygin
  */
+@Configuration
 @EnableOpenApi
 @EnableGlobalExceptionHandler
+@EnableFilters
 @EnableCaching
-@Configuration
+@EntityScan(basePackageClasses = EvaluationResultsInfo.class)
+@EnableJpaRepositories(basePackageClasses = EvaluationResultsInfoRepository.class)
 @EnableConfigurationProperties(ErsConfig.class)
 public class ErsConfiguration {
 }
