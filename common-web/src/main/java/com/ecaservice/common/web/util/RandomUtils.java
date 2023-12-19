5ac5b04606eaad5edb1ea4977b8c00b8378a04eb
Roman <bat1238@yandex.ru>
2023-12-19 18:19:11 +0700

    changed RandomUtils#generateToken algorithm

--- common-web/src/main/java/com/ecaservice/common/web/util/RandomUtils.java
+++ common-web/src/main/java/com/ecaservice/common/web/util/RandomUtils.java
@@ -2,10 +2,8 @@ package com.ecaservice.common.web.util;
 
 import lombok.experimental.UtilityClass;
 import org.springframework.util.Base64Utils;
-import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
 import java.util.UUID;
 
 /**
@@ -16,34 +14,19 @@ import java.util.UUID;
 @UtilityClass
 public class RandomUtils {
 
-    private static final String SALT_FORMAT = "%s:%d";
+    private static final String TOKEN_FORMAT = "%s:%d";
 
     /**
      * Generates unique token by algorithm:
-     * 1. Creates salt in format uuid:first_random_number
-     * 2. Gets md5_salt = MD5(salt)
-     * 3. Creates string in format md5_salt:second_random_number
-     * 4. Gets results = base64(md5_salt:second_random_number)
+     * 1. Creates string in format uuid:current_timestamp_millis
+     * 2. Calculate token = base64(uuid:current_timestamp_millis)
      *
      * @return token value
      */
     public static String generateToken() {
-        long first = secureRandomNumber();
-        long second = secureRandomNumber();
+        long currentTimestampMillis = System.currentTimeMillis();
         String uuid = UUID.randomUUID().toString();
-        String salt = String.format(SALT_FORMAT, uuid, first);
-        String md5Salt = DigestUtils.md5DigestAsHex(salt.getBytes(StandardCharsets.UTF_8));
-        String stringToEncode = String.format(SALT_FORMAT, md5Salt, second);
+        String stringToEncode = String.format(TOKEN_FORMAT, uuid, currentTimestampMillis);
         return Base64Utils.encodeToString(stringToEncode.getBytes(StandardCharsets.UTF_8));
-    }
-
-    /**
-     * Generates secure random number.
-     *
-     * @return random number
-     */
-    public static long secureRandomNumber() {
-        SecureRandom secureRandom = new SecureRandom();
-        return secureRandom.nextLong();
     }
 }
