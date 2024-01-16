6aafb58dee8d06876466f13a3825c56e12d2d6d2
Roman <bat1238@yandex.ru>
2024-01-16 17:12:40 +0700

    ECS-9 impl /report/download api

---
+++ eca-ers/src/main/java/com/ecaservice/ers/report/ReportTemplates.java
@@ -0,0 +1,19 @@
+package com.ecaservice.ers.report;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Report templates.
+ *
+ * @author Roman Batygin
+ */
+@Slf4j
+@UtilityClass
+public class ReportTemplates {
+
+    /**
+     * Audit logs template
+     */
+    public static final String EVALUATION_RESULTS_HISTORY_TEMPLATE = "evaluation-results-history-template.xlsx";
+}
