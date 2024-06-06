1b8a08840df7bbe1f369e8099b11c7550b0e5003
Roman <bat1238@yandex.ru>
2024-06-06 18:41:25 +0700

    ECS-40 added ResourceServerSecurityConfiguration for eca-oauth

--- eca-oauth/src/main/java/com/ecaservice/oauth/config/security/AuthorizationServerSecurityConfiguration.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/config/security/AuthorizationServerSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.ecaservice.oauth.config.security;
 
-import com.ecaservice.oauth.config.AppProperties;
 import com.ecaservice.oauth.config.TfaConfig;
 import com.ecaservice.oauth.repository.UserEntityRepository;
 import com.ecaservice.oauth.service.tfa.TfaCodeService;
@@ -9,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -34,16 +32,10 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2AccessTokenGenerator;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2RefreshTokenGenerator;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
-import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.CollectionUtils;
 
 import java.time.Duration;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 /**
  * Authorization server security configuration.
@@ -58,18 +50,6 @@ import static com.google.common.collect.Lists.newArrayList;
 public class AuthorizationServerSecurityConfiguration {
 
     private static final int AUTHORIZATION_SERVER_SECURITY_FILTER_ORDER = 0;
-
-    private static final int PUBLIC_ENDPOINTS_SECURITY_FILTER_ORDER = 1;
-    private static final int RESOURCE_SERVER_SECURITY_FILTER_ORDER = 2;
-
-    private static final String[] COMMON_WHITELIST_URLS = {
-            "/v3/api-docs/**",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
-            "/actuator/**"
-    };
-
-    private final AppProperties appProperties;
 
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
@@ -104,17 +84,6 @@ public class AuthorizationServerSecurityConfiguration {
     }
 
     /**
-     * Creates bearer token resolver bean.
-     *
-     * @return bearer token resolver
-     */
-    @Bean
-    @Primary
-    public BearerTokenResolver bearerTokenResolver() {
-        return new SimpleBearerTokenResolver(new DefaultBearerTokenResolver());
-    }
-
-    /**
      * Creates authorization server security filter chain
      *
      * @param http                   - http security
@@ -144,56 +113,6 @@ public class AuthorizationServerSecurityConfiguration {
                 )
                 .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
                 .with(authorizationServerConfigurer, Customizer.withDefaults());
-        return http.build();
-    }
-
-    /**
-     * Creates whitelist urls security filter chain.
-     *
-     * @param http - http security
-     * @return security filter chain
-     * @throws Exception in case of error
-     */
-    @Bean
-    @Order(PUBLIC_ENDPOINTS_SECURITY_FILTER_ORDER)
-    public SecurityFilterChain whitelistSecurityUrlsFilterChain(HttpSecurity http) throws Exception {
-        List<String> whitelistUrls = newArrayList(COMMON_WHITELIST_URLS);
-        if (!CollectionUtils.isEmpty(appProperties.getSecurity().getWhitelistUrls())) {
-            whitelistUrls.addAll(appProperties.getSecurity().getWhitelistUrls());
-        }
-        log.info("Resource server security whitelist urls: {}", whitelistUrls);
-        http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                // Disabled bearer token check for specified urls
-                .securityMatcher(whitelistUrls.toArray(new String[0]))
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll());
-        return http.build();
-    }
-
-    /**
-     * Creates resource server security filter chain.
-     *
-     * @param http                       - http security
-     * @param oAuth2AuthorizationService - oauth2 authorization service
-     * @return security filter chain
-     * @throws Exception in case of error
-     */
-    @Bean
-    @Order(RESOURCE_SERVER_SECURITY_FILTER_ORDER)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
-                                                          OAuth2AuthorizationService oAuth2AuthorizationService)
-            throws Exception {
-        http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(c ->
-                        c.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                                .opaqueToken(opaqueTokenConfigurer -> opaqueTokenConfigurer
-                                        .introspector(new JdbcTokenIntrospector(oAuth2AuthorizationService))));
         return http.build();
     }
 
