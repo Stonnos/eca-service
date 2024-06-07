b0340f95abecaa53757dc824f70346a6f6a677c1
Roman <bat1238@yandex.ru>
2024-06-08 02:17:17 +0700

    ECS-40 added migrate-user-photo-ids.sql script

---
+++ eca-oauth/src/main/resources/db/changelog/sql/migrate-user-photo-ids.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR REPLACE FUNCTION migrate_user_photo_ids() RETURNS VOID AS $$
+DECLARE
+  u_photo user_photo%rowtype;
+BEGIN
+  FOR u_photo IN select * from user_photo
+  LOOP
+     update user_entity set photo_id = u_photo.id where id = u_photo.user_id;
+  END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+DO $$
+BEGIN
+  PERFORM migrate_user_photo_ids();
+END $$;
+
+DROP function migrate_user_photo_ids();
