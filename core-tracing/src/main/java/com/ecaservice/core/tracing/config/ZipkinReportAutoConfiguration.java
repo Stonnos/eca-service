8e09e9933079894b409420980f1a6814f862b5b4
Roman <bat1238@yandex.ru>
2024-06-09 18:17:07 +0700

    ECS-40 removed zipkin asyncreporter bean from ZipkinReportAutoConfiguration

--- core-tracing/src/main/java/com/ecaservice/core/tracing/config/ZipkinReportAutoConfiguration.java
+++ core-tracing/src/main/java/com/ecaservice/core/tracing/config/ZipkinReportAutoConfiguration.java
@@ -1,15 +1,12 @@
 package com.ecaservice.core.tracing.config;
 
-import com.ecaservice.core.tracing.report.NoopReporter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.Span;
-import zipkin2.codec.BytesEncoder;
-import zipkin2.reporter.AsyncReporter;
-import zipkin2.reporter.Sender;
+import zipkin2.reporter.Reporter;
 
 /**
  * Zipkin report configuration.
@@ -22,27 +19,13 @@ import zipkin2.reporter.Sender;
 public class ZipkinReportAutoConfiguration {
 
     /**
-     * Creates async span reporter bean.
-     *
-     * @param sender  - trace sender
-     * @param encoder - span encoder
-     * @return span reporter bean
-     */
-    @Bean
-    @ConditionalOnProperty(value = "management.zipkin.tracing.enabled", havingValue = "true")
-    public AsyncReporter<Span> spanReporter(Sender sender, BytesEncoder<Span> encoder) {
-        log.info("Zipkin async reporter has been configured");
-        return AsyncReporter.builder(sender).build(encoder);
-    }
-
-    /**
      * Creates span noop reporter bean.
      *
      * @return span noop reporter bean
      */
     @Bean
     @ConditionalOnProperty(value = "management.zipkin.tracing.enabled", havingValue = "false", matchIfMissing = true)
-    public NoopReporter noopReporter() {
-        return new NoopReporter();
+    public Reporter<Span> noopReporter() {
+        return Reporter.NOOP;
     }
 }
