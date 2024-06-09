7492ad154c0e06e0540433e6a42621a31df3d9d1
Roman <bat1238@yandex.ru>
2024-06-09 18:39:39 +0700

    ECS-40 returned zipkin async reporter in configuration

--- core-tracing/src/main/java/com/ecaservice/core/tracing/config/ZipkinReportAutoConfiguration.java
+++ core-tracing/src/main/java/com/ecaservice/core/tracing/config/ZipkinReportAutoConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.Span;
+import zipkin2.codec.BytesEncoder;
+import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.Reporter;
+import zipkin2.reporter.Sender;
 
 /**
  * Zipkin report configuration.
@@ -17,6 +20,20 @@ import zipkin2.reporter.Reporter;
 @Configuration
 @EnableConfigurationProperties(ZipkinReportProperties.class)
 public class ZipkinReportAutoConfiguration {
+
+    /**
+     * Creates async span reporter bean.
+     *
+     * @param sender  - trace sender
+     * @param encoder - span encoder
+     * @return span reporter bean
+     */
+    @Bean
+    @ConditionalOnProperty(value = "management.zipkin.tracing.enabled", havingValue = "true")
+    public AsyncReporter<Span> spanReporter(Sender sender, BytesEncoder<Span> encoder) {
+        log.info("Zipkin async reporter has been configured");
+        return AsyncReporter.builder(sender).build(encoder);
+    }
 
     /**
      * Creates span noop reporter bean.
