69bc7c7895e53404101d16fc72e2b34b273633aa
Roman <bat1238@yandex.ru>
2024-06-10 22:04:58 +0700

    ECS-40 fix oauth2 client configuration

--- feign-oauth/src/main/java/com/ecaservice/feign/oauth/config/FeignClientOauth2Configuration.java
+++ feign-oauth/src/main/java/com/ecaservice/feign/oauth/config/FeignClientOauth2Configuration.java
@@ -2,6 +2,7 @@ package com.ecaservice.feign.oauth.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.cloud.openfeign.security.OAuth2AccessTokenInterceptor;
@@ -14,8 +15,6 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.util.Assert;
 
-import java.util.Optional;
-
 /**
  * Feign configuration for oauth2 secured endpoints.
  *
@@ -25,6 +24,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class FeignClientOauth2Configuration {
 
+    @Value("${spring.cloud.openfeign.oauth2.clientRegistrationId:}")
+    private String clientRegistrationId;
+
     /**
      * Creates oauth2 authorized client manager bean.
      *
@@ -33,12 +35,12 @@ public class FeignClientOauth2Configuration {
     @Bean
     public OAuth2AuthorizedClientManager feignOauthAuthorizedClientManager(
             OAuth2ClientProperties oAuth2ClientProperties) {
-        var clientRegistrations = oAuth2ClientProperties.getRegistration().entrySet()
-                .stream()
-                .map(entry -> clientRegistration(oAuth2ClientProperties, entry.getKey(), entry.getValue()))
-                .toList();
-        log.info("Feign client registrations has been configured");
-        var clientRegistrationRepository = new InMemoryClientRegistrationRepository(clientRegistrations);
+        var registration = oAuth2ClientProperties.getRegistration().get(clientRegistrationId);
+        Assert.notNull(registration,
+                String.format("Client [%s] registration must be configured", clientRegistrationId));
+        var clientRegistration = clientRegistration(oAuth2ClientProperties, registration);
+        log.info("Feign client [{}] registration has been configured", clientRegistrationId);
+        var clientRegistrationRepository = new InMemoryClientRegistrationRepository(clientRegistration);
         var oAuth2AuthorizedClientService = new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
         return new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository,
                 oAuth2AuthorizedClientService);
@@ -56,24 +58,32 @@ public class FeignClientOauth2Configuration {
     public OAuth2AccessTokenInterceptor oAuth2AccessTokenInterceptor(
             OAuth2ClientProperties oAuth2ClientProperties,
             OAuth2AuthorizedClientManager feignOauthAuthorizedClientManager) {
-        String clientRegistrationId = oAuth2ClientProperties.getRegistration().keySet().iterator().next();
-        Assert.notNull(clientRegistrationId, "Client registration must be specified for oauth2 client!");
         return new OAuth2AccessTokenInterceptor(clientRegistrationId, feignOauthAuthorizedClientManager);
     }
 
     private ClientRegistration clientRegistration(OAuth2ClientProperties oAuth2ClientProperties,
-                                                  String id,
                                                   OAuth2ClientProperties.Registration registration) {
-        OAuth2ClientProperties.Provider provider =
-                oAuth2ClientProperties.getProvider().get(registration.getProvider());
-        String tokenUri = Optional.ofNullable(provider)
-                .map(OAuth2ClientProperties.Provider::getTokenUri)
-                .orElse(null);
-        return ClientRegistration.withRegistrationId(id)
-                .clientId(registration.getClientId())
-                .clientSecret(registration.getClientSecret())
-                .authorizationGrantType(
-                        new AuthorizationGrantType(registration.getAuthorizationGrantType()))
+        String clientId = registration.getClientId();
+        Assert.notNull(clientId,
+                String.format("Client [%s] id must be configured", clientRegistrationId));
+        String clientSecret = registration.getClientSecret();
+        Assert.notNull(clientSecret,
+                String.format("Client [%s] secret must be configured", clientRegistrationId));
+        String grantType = registration.getAuthorizationGrantType();
+        Assert.notNull(grantType,
+                String.format("Client [%s] grant type must be configured", clientRegistrationId));
+        Assert.notNull(registration.getProvider(),
+                String.format("Client [%s] registration provider must be configured", clientRegistrationId));
+        OAuth2ClientProperties.Provider provider = oAuth2ClientProperties.getProvider().get(registration.getProvider());
+        Assert.notNull(provider,
+                String.format("Client [%s] provider must be configured", clientRegistrationId));
+        String tokenUri = provider.getTokenUri();
+        Assert.notNull(tokenUri,
+                String.format("Client [%s] provider tokenUri must be configured", clientRegistrationId));
+        return ClientRegistration.withRegistrationId(clientRegistrationId)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .authorizationGrantType(new AuthorizationGrantType(grantType))
                 .scope(registration.getScope())
                 .tokenUri(tokenUri)
                 .build();
