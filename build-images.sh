8717c813cb804997dffdc50407d9936e922bcc73
Roman <bat1238@yandex.ru>
2024-06-17 17:50:00 +0700

    ECS-41 migrate project to java 21

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,11 @@
-docker build eca-oauth eca-oauth
-docker build eca-server eca-server
-docker build eca-mail eca-mail
-docker build eca-data-storage eca-data-storage
-docker build eca-ers eca-ers
-docker build eca-web eca-web
-docker build api-gateway api-gateway
-docker build discovery-server discovery-server
-docker build eca-web-push eca-web-push
-docker build eca-audit-log eca-audit-log
-docker build eca-data-loader eca-data-loader
+docker build -t eca-oauth eca-oauth
+docker build -t eca-server eca-server
+docker build -t eca-mail eca-mail
+docker build -t eca-data-storage eca-data-storage
+docker build -t eca-ers eca-ers
+docker build -t eca-web eca-web
+docker build -t api-gateway api-gateway
+docker build -t discovery-server discovery-server
+docker build -t eca-web-push eca-web-push
+docker build -t eca-audit-log eca-audit-log
+docker build -t eca-data-loader eca-data-loader
--- eca-external-api/README.md
+++ eca-external-api/README.md
@@ -9,7 +9,7 @@ ECA external API
 
 Необходимый софт
 ----------------------------------------
-* Openjdk 17
+* Openjdk 21
 * maven => 3.9.2
 * Rabbit MQ => 3.8.14
 * eca-core 6.9.9
