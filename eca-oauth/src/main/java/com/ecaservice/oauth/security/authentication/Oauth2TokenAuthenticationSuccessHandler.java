1bd38c2b2822117448ac961aef9887c8afd37811
Roman <bat1238@yandex.ru>
2024-12-10 12:53:22 +0700

    ECS-56 fix get token parsing error in swagger, returned reduced response

--- eca-oauth/src/main/java/com/ecaservice/oauth/security/authentication/Oauth2TokenAuthenticationSuccessHandler.java
+++ eca-oauth/src/main/java/com/ecaservice/oauth/security/authentication/Oauth2TokenAuthenticationSuccessHandler.java
@@ -1,13 +1,15 @@
 package com.ecaservice.oauth.security.authentication;
 
 import com.ecaservice.oauth.config.AppProperties;
+import com.ecaservice.oauth.security.model.Oauth2TokenResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
@@ -18,6 +20,7 @@ import org.springframework.security.oauth2.core.http.converter.OAuth2AccessToken
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AccessTokenAuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.time.temporal.ChronoUnit;
@@ -38,8 +41,11 @@ import static com.ecaservice.oauth.util.CookiesUtils.tokenCookie;
 public class Oauth2TokenAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private final AppProperties appProperties;
+
     private final HttpMessageConverter<OAuth2AccessTokenResponse> accessTokenHttpResponseConverter =
             new OAuth2AccessTokenResponseHttpMessageConverter();
+    private final MappingJackson2HttpMessageConverter jackson2HttpMessageConverter =
+            new MappingJackson2HttpMessageConverter();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -56,15 +62,21 @@ public class Oauth2TokenAuthenticationSuccessHandler implements AuthenticationSu
         }
     }
 
-    private void sendAccessTokenResponseInCookie(HttpServletResponse response, Authentication authentication) {
+    private void sendAccessTokenResponseInCookie(HttpServletResponse response, Authentication authentication)
+            throws IOException {
         var accessTokenAuthentication = (OAuth2AccessTokenAuthenticationToken) authentication;
+        OAuth2AccessToken accessToken = accessTokenAuthentication.getAccessToken();
         Cookie accessTokenCookie =
-                tokenCookie(ACCESS_TOKEN_COOKIE, accessTokenAuthentication.getAccessToken(), ALL_PATH);
+                tokenCookie(ACCESS_TOKEN_COOKIE, accessToken, ALL_PATH);
         Cookie refreshTokenCookie = tokenCookie(REFRESH_TOKEN_COOKIE, accessTokenAuthentication.getRefreshToken(),
                 appProperties.getSecurity().getRefreshTokenCookiePath());
         response.addCookie(accessTokenCookie);
         response.addCookie(refreshTokenCookie);
-        response.setStatus(HttpStatus.OK.value());
+        var oauth2TokenResponse =
+                new Oauth2TokenResponse(StringUtils.collectionToDelimitedString(accessToken.getScopes(), " "),
+                        accessToken.getTokenType().getValue());
+        ServletServerHttpResponse httpResponse = new ServletServerHttpResponse(response);
+        jackson2HttpMessageConverter.write(oauth2TokenResponse, MediaType.APPLICATION_JSON, httpResponse);
         log.info("Access/refresh token cookies has been write in response");
     }
 
