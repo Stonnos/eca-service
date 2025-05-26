c34f73585a0713376d640265056c34dedd968607
Roman Batygin <rbatygin@igatec.com>
2025-05-26 16:24:54 +0700

    ECS-74 impl EvaluationResultsReportDataFetcher

--- eca-server/src/main/java/com/ecaservice/server/report/EvaluationResultsReportDataProcessor.java
+++ eca-server/src/main/java/com/ecaservice/server/report/EvaluationResultsReportDataProcessor.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
- * Evaluation results report data fetcher.
+ * Evaluation results report data processor.
  *
  * @author Roman Batygin
  */
