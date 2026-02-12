d7402d5aec4ba078b417b9171df5dc8c73c5c15b
Roman Batygin <rbatygin@igatec.com>
2026-02-13 01:06:36 +0700

    ECS-96 impl contingency-table-dialog.module.ts

--- eca-web/src/main/resources/src/app/common/model/column.model.ts
+++ eca-web/src/main/resources/src/app/common/model/column.model.ts
@@ -2,4 +2,9 @@ export class ColumnModel {
   name: string;
   label: string;
   sortBy?: string;
+
+  public constructor(name?: string, label?: string) {
+    this.name = name;
+    this.label = label;
+  }
 }
