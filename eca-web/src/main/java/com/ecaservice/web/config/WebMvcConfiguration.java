3567665d7234455a84beaec3e4deb0c70bbd5d5e
Roman Batygin <rbatygin@igatec.com>
2026-07-27 23:53:24 +0700

    added /revoke-change-email url in forwards

--- eca-web/src/main/java/com/ecaservice/web/config/WebMvcConfiguration.java
+++ eca-web/src/main/java/com/ecaservice/web/config/WebMvcConfiguration.java
@@ -18,7 +18,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private static final List<String> URL_PATHS =
             List.of("/login", "/dashboard/**", "/create-reset-password-request", "/reset-password/**",
-                    "/change-password/**", "/access-denied", "/change-email/**", "/evaluation-results-request-path/**");
+                    "/revoke-change-email/**", "/change-password/**", "/access-denied", "/change-email/**",
+                    "/evaluation-results-request-path/**");
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
