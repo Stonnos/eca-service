a5a550771f822fdb5338a847ee8884ad01174911
Roman Batygin <rbatygin@igatec.com>
2026-08-11 15:50:11 +0700

    removed eca-mail from deploy

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,10 @@
 docker build -t eca-oauth eca-oauth
 docker build -t eca-server eca-server
-docker build -t eca-mail eca-mail
 docker build -t eca-data-storage eca-data-storage
 docker build -t eca-ers eca-ers
 docker build -t eca-web eca-web
 docker build -t api-gateway api-gateway
 docker build -t discovery-server discovery-server
-docker build -t eca-web-push eca-web-push
+docker build -t eca-notification-service eca-notification-service
 docker build -t eca-audit-log eca-audit-log
 docker build -t eca-data-loader eca-data-loader
--- deploy-images.sh
+++ deploy-images.sh
@@ -2,8 +2,6 @@ docker tag eca-oauth dockerbatygin/eca:eca-oauth
 docker push dockerbatygin/eca:eca-oauth
 docker tag eca-server dockerbatygin/eca:eca-server
 docker push dockerbatygin/eca:eca-server
-docker tag eca-mail dockerbatygin/eca:eca-mail
-docker push dockerbatygin/eca:eca-mail
 docker tag eca-data-storage dockerbatygin/eca:eca-data-storage
 docker push dockerbatygin/eca:eca-data-storage
 docker tag eca-ers dockerbatygin/eca:eca-ers
@@ -14,8 +12,8 @@ docker tag api-gateway dockerbatygin/eca:api-gateway
 docker push dockerbatygin/eca:api-gateway
 docker tag discovery-server dockerbatygin/eca:discovery-server
 docker push dockerbatygin/eca:discovery-server
-docker tag eca-web-push dockerbatygin/eca:eca-web-push
-docker push dockerbatygin/eca:eca-web-push
+docker tag eca-notification-service dockerbatygin/eca:eca-notification-service
+docker push dockerbatygin/eca:eca-notification-service
 docker tag eca-audit-log dockerbatygin/eca:eca-audit-log
 docker push dockerbatygin/eca:eca-audit-log
 docker tag eca-data-loader dockerbatygin/eca:eca-data-loader
